7a879095dfb4c966a226bb7e34df7cffbaf92887
Ivan Judson <judson@mcs.anl.gov>
2003-01-09 19:03:29 +0000

    Added top level INSTALL, README and setup.py in preparation for release engineering.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        setup.py
+# Purpose:     This is the setup.py used by distutils to generate the install
+#              files for the Access Grid Software.
+#
+# Author:      Ivan R. Judson
+#
+# Created:     2003/01/09
+# RCS-ID:      $Id: setup.py,v 1.1 2003-01-09 19:03:29 judson Exp $
+# Copyright:   (c) 2003
+# Licence:     See COPYING.TXT
+#-----------------------------------------------------------------------------
