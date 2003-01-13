edc01ba08a0a57bab35138b87c26cc53e6b3f207
Tom Uram <turam@mcs.anl.gov>
2003-01-13 18:27:16 +0000

    Defaults from None to zero

--- AccessGrid/NetworkLocation.py
+++ AccessGrid/NetworkLocation.py
@@ -5,13 +5,13 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/13/12
-# RCS-ID:      $Id: NetworkLocation.py,v 1.2 2003-01-06 21:26:39 turam Exp $
+# RCS-ID:      $Id: NetworkLocation.py,v 1.3 2003-01-13 18:27:16 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
 class NetworkLocation:
-    __doc__ = """
+    """
     The Network Locations are simply python objects to manage network
     information. We derive two types, unicast and multicast. It's probably more
     correct to derive IP4Uni and IP4Multi and IP6Uni and IP6Multi, but that's
@@ -38,7 +38,7 @@ class NetworkLocation:
         return self.port
         
 class UnicastNetworkLocation(NetworkLocation):
-    __doc__ = """
+    """
     Unicast network location encapsulates the configuration information about
     a unicast network connection.
     """
@@ -46,14 +46,14 @@ class UnicastNetworkLocation(NetworkLocation):
     pass
 
 class MulticastNetworkLocation(NetworkLocation):
-    __doc__ = """
+    """
     Multicast network location encapsulates the configuration information about
     a multicsat network connection.
     """
     TYPE = 'multicast'
     ttl = 0
     
-    def __init__(self, host=None, port=None, ttl=None):
+    def __init__(self, host=None, port=0, ttl=0):
         self.ttl = ttl
         NetworkLocation.__init__(self, host, port)
     
