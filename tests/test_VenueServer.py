76921303b4529c0024b0d65d0549a44ef15233ba
Ivan Judson <judson@mcs.anl.gov>
2003-01-15 20:32:23 +0000

    Updated to use new default arguments in the VenueDescription constructor. Shutdown is now working.

--- tests/test_VenueServer.py
+++ tests/test_VenueServer.py
@@ -5,7 +5,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/17/12
-# RCS-ID:      $Id: test_VenueServer.py,v 1.1 2003-01-09 18:59:24 judson Exp $
+# RCS-ID:      $Id: test_VenueServer.py,v 1.2 2003-01-15 20:32:23 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -13,14 +13,18 @@
 from AccessGrid.hosting.pyGlobus import Client
 from AccessGrid.Descriptions import VenueDescription
 
-vd1 = VenueDescription("First Venue", "First Venue Description", "", None)
-vd2 = VenueDescription("Second Venue", "Second Venue Description", "", None)
-vd3 = VenueDescription("Third Venue", "Third Venue Description", "", None)
-vd4 = VenueDescription("Fourth Venue", "Fourth Venue Description", "", None)
+vd1 = VenueDescription("First Venue", "First Venue Description")
+vd2 = VenueDescription("Second Venue", "Second Venue Description")
+vd3 = VenueDescription("Third Venue", "Third Venue Description")
+vd4 = VenueDescription("Fourth Venue", "Fourth Venue Description")
 
 vms = Client.Handle('https://localhost:8000/VenueServer').get_proxy()
 
-print "\nNew Venue URL: " + vms.AddVenue(vd1)
+try:
+    print "\nNew Venue URL: " + vms.AddVenue(vd1)
+except:
+    print "Add Venue Failed!"
+    
 print "\nNew Venue URL: " + vms.AddVenue(vd2)
 print "\nNew Venue URL: " + vms.AddVenue(vd3)
 print "\nNew Venue URL: " + vms.AddVenue(vd4)
@@ -36,3 +40,6 @@ for v in vl:
 
 print "\nCheckpointing"
 vms.Checkpoint()
+
+print "\nShutting down"
+vms.Shutdown(0)
