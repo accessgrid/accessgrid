86731338d8574d00dcbf46b72131805731bad4dd
Ivan Judson <judson@mcs.anl.gov>
2003-01-17 17:54:28 +0000

    Merged conflicts, documented and updated.

--- AccessGrid/Venue.py
+++ AccessGrid/Venue.py
@@ -6,7 +6,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: Venue.py,v 1.8 2003-01-16 20:25:26 turam Exp $
+# RCS-ID:      $Id: Venue.py,v 1.9 2003-01-17 17:54:28 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -48,11 +48,12 @@ class Venue(ServiceBase.ServiceBase):
 
         self.defaultTtl = 127
 
-
     # Management methods
     def AddNetworkService(self, connectionInfo, networkServiceDescription):
-        """ """
-
+        """
+        AddNetworkService allows an administrator to add functionality to
+        the Venue. Network services are described in the design documents.
+        """
         try:
             self.networkServices[ networkServiceDescription.uri ] = networkServiceDescription
         except:
@@ -62,13 +63,20 @@ class Venue(ServiceBase.ServiceBase):
     AddNetworkService.soap_export_as = "AddNetworkService"
     
     def GetNetworkServices(self, connectionInfo):
-        """ """
+        """
+        GetNetworkServices retreives the list of network service descriptions
+        from the venue.
+        """
         return self.networkServices
+    
     GetNetworkServices.pass_connection_info = 1
     GetNetworkServices.soap_export_as = "GetNetworkServices"
     
     def RemoveNetworkService(self, connectionInfo, networkServiceDescription):
-        """ """
+        """
+        RemoveNetworkService removes a network service from a venue, making
+        it unavailable to users of the venue.
+        """
         try:
             del self.networkServices[ networkServiceDescription.uri ]
         except:
@@ -78,35 +86,52 @@ class Venue(ServiceBase.ServiceBase):
     RemoveNetworkService.soap_export_as = "RemoveNetworkService"
         
     def AddConnection(self, connectionInfo, connectionDescription):
-        """ """
+        """
+        AddConnection allows an administrator to add a connection to a
+        virtual venue to this virtual venue.
+        """
         try:
 
             self.connections[connectionDescription.uri] = connectionDescription
             self.coherenceService.distribute( Event( Event.ADD_CONNECTION, connectionDescription ) )
         except:
             print "Connection already exists ", connectionDescription.uri
+
     AddConnection.pass_connection_info = 1
     AddConnection.soap_export_as = "AddConnection"
 
     def RemoveConnection(self, connectionInfo, connectionDescription):
-        """ """
+        """
+        RemoveConnection removes a connection to another virtual venue
+        from this virtual venue. This is an administrative operation.
+        """
         try:
             del self.connections[ connectionDescription.uri ]
             self.coherenceService.distribute( Event( Event.REMOVE_CONNECTION, connectionDescription ) )
         except:
             print "Exception in RemoveConnection", sys.exc_type, sys.exc_value
             print "Connection does not exist ", connectionDescription.uri
+
     RemoveConnection.pass_connection_info = 1
     RemoveConnection.soap_export_as = "RemoveConnection"
 
     def GetConnections(self, connectionInfo ):
-        """ """
+        """
+        GetConnections returns a list of all the connections to other venues
+        that are found within this venue.
+        """
         return self.connections.values()
+
     GetConnections.pass_connection_info = 1
     GetConnections.soap_export_as = "GetConnections"
 
     def SetConnections(self, connectionInfo, connectionList ):
-        """ """
+        """
+        SetConnections is a convenient aggregate accessor for the list of
+        connections for this venue. Alternatively the user could iterate over
+        a list of connections adding them one by one, but this is more
+        desirable.
+        """
         try:
 
             self.connections = dict()
@@ -116,44 +141,51 @@ class Venue(ServiceBase.ServiceBase):
         except:
             print "Exception in SetConnections", sys.exc_type, sys.exc_value
             print "Connection does not exist ", connectionDescription.uri
+
     SetConnections.pass_connection_info = 1
     SetConnections.soap_export_as = "SetConnections"
 
-
-        
-
     def SetDescription(self, connectionInfo, description):
-        """ """
+        """
+        SetDescription allows an administrator to set the venues description
+        to something new.
+        """
         self.description = description
+
     SetDescription.pass_connection_info = 1
     SetDescription.soap_export_as = "SetDescription"
 
     def GetDescription(self, connectionInfo):
-        """ """
+        """
+        GetDescription returns the description for the virtual venue.
+        """
         return self.description
+
     GetDescription.pass_connection_info = 1
     GetDescription.soap_export_as = "GetDescription"
 
     def GetStreams(self, connectionInfo):
-        """ """
+        """
+        GetStreams returns a list of stream descriptions to the caller.
+        """
         return self.streams
+
     GetStreams.pass_connection_info = 1
     GetStreams.soap_export_as = "GetStreams"
 
     # Client Methods
     def Enter(self, connectionInfo, clientProfile):
-        """ """
+        """
+        The Enter method is used by a VenueClient to gain access to the
+        services, users, and content found within a Virtual Venue.
+        """
         privateId = None
         streamDescriptions = None
         state = self.GetState( connectionInfo )
 
-
         try:
-
             print "Called Venue Enter for: "
-
             print dir(clientProfile)
-
 
             userInVenue = False
             for key in self.users.keys():
@@ -183,7 +215,11 @@ class Venue(ServiceBase.ServiceBase):
     Enter.soap_export_as = "Enter"
     
     def Exit(self, connectionInfo, privateId ):
-        """ """
+        """
+        The Exit method is used by a VenueClient to cleanly leave a Virtual
+        Venue. Cleanly leaving a Virtual Venue allows the Venue to cleanup
+        any state associated (or caused by) the VenueClients presence.
+        """
         try:
             print "Called Venue Exit on " + str(connectionInfo)
 
@@ -202,7 +238,10 @@ class Venue(ServiceBase.ServiceBase):
     Exit.soap_export_as = "Exit"
 
     def GetState(self, connectionInfo):
-        """ """
+        """
+        GetState enables a VenueClient to poll the Virtual Venue for the
+        current state of the Virtual Venue.
+        """
 
         try:
            print "Called GetState on ", self.uniqueId
@@ -224,7 +263,11 @@ class Venue(ServiceBase.ServiceBase):
     GetState.soap_export_as = "GetState"
 
     def UpdateClientProfile(self, connectionInfo, clientProfile):
-        """ """
+        """
+        UpdateClientProfile allows a VenueClient to update/modify the client
+        profile that is stored by the Virtual Venue that they gave to the Venue
+        when they called the Enter method.
+        """
         for user in self.users.values():
             if user.publicId == clientProfile.publicId:
                 self.users[user.privateId] = clientProfile
@@ -234,18 +277,29 @@ class Venue(ServiceBase.ServiceBase):
     UpdateClientProfile.soap_export_as = "UpdateClientProfile"
 
     def AddData(self, connectionInfo, dataDescription ):
-        """ """
+        """
+        The AddData method enables VenuesClients to put data in the Virtual
+        Venue. Data put in the Virtual Venue through AddData is persistently
+        stored.
+        """
         try:
+            # We also have to actually add real data, but not yet
             self.data[dataDescription.name] = dataDescription
-            self.coherenceService.distribute( Event( Event.ADD_DATA, dataDescription ) )
+            self.coherenceService.distribute( Event( Event.ADD_DATA,
+                                                     dataDescription ) )
         except:
             print "Exception in AddData ", sys.exc_type, sys.exc_value
             print "Data already exists ", dataDescription.name
+
     AddData.pass_connection_info = 1
     AddData.soap_export_as = "AddData"
 
     def GetData(self, connectionInfo, dataDescription):
-        """ """
+        """
+        GetData is the method called by a VenueClient to retrieve information
+        about the data. This might also be the operation that the VenueClient
+        uses to actually retrieve the real data.
+        """
 
         # Do something with the data store
 
@@ -253,36 +307,48 @@ class Venue(ServiceBase.ServiceBase):
     GetData.soap_export_as = "GetData"
 
     def RemoveData(self, connectionInfo, dataDescription):
-        """ """
+        """
+        RemoveData removes persistent data from the Virtual Venue.
+        """
         try:
+            # We actually have to remove the real data, not just the
+            # data description -- I guess that's later :-)
             del self.data[ dataDescription.name ]
-            self.coherenceService.distribute( Event( Event.REMOVE_DATA, dataDescription ) )
+            self.coherenceService.distribute( Event( Event.REMOVE_DATA,
+                                                     dataDescription ) )
         except:
             print "Exception in RemoveData", sys.exc_type, sys.exc_value
             print "Data does not exist", dataDescription.name
+
     RemoveData.pass_connection_info = 1
     RemoveData.soap_export_as = "RemoveData"
 
     def AddService(self, connectionInfo, serviceDescription):
         """
-        This method should add a service description to the Venue.
+        This method adds a service description to the Venue.
         """
         print "Adding service ", serviceDescription.name, serviceDescription.uri
         try:
             self.services[serviceDescription.uri] = serviceDescription
-            self.coherenceService.distribute( Event( Event.ADD_SERVICE, serviceDescription ) )
+            self.coherenceService.distribute( Event( Event.ADD_SERVICE,
+                                                     serviceDescription ) )
         except:
             print "Exception in AddService ", sys.exc_type, sys.exc_value
             print "Service already exists ", serviceDescription.name
+
     AddService.pass_connection_info = 1
     AddService.soap_export_as = "AddService"
 
     def RemoveService(self, connectionInfo, serviceDescription):
-        """ """
+        """
+        This method removes a service description from the list of services
+        in the Virtual Venue.
+        """
         
         try:
             del self.services[serviceDescription.uri]
-            self.coherenceService.distribute( Event( Event.REMOVE_SERVICE, serviceDescription ) )
+            self.coherenceService.distribute( Event( Event.REMOVE_SERVICE,
+                                                     serviceDescription ) )
         except:
             print "Exception in RemoveService", sys.exc_type, sys.exc_value
             print "Service does not exist ", serviceDescription.name
@@ -291,30 +357,40 @@ class Venue(ServiceBase.ServiceBase):
     RemoveService.soap_export_as = "RemoveService"
 
     def Ping( self ):
-      print "Ping!"
-      return 1
+        """ I don't know, ask Tom """
+        print "Ping!"
+        return 1
+
     Ping.pass_connection_info = 0
     Ping.soap_export_as = "Ping"
 
-
-
-
-
     # Internal Methods
     def Shutdown(self):
-        """ """
+        """
+        This method cleanly shuts down all active threads associated with the
+        Virtual Venue. Currently there are a few threads in the Coherence
+        Service.
+        """
         self.coherenceService.stop()
 
     def NegotiateCapabilities(self, clientProfile):
-        """ """
+        """
+        This method takes a client profile and finds a set of streams that
+        matches the client profile. Later this method could use network
+        services to find The Best Match of all the network services,
+        the existing streams, and all the client capabilities.
+        """
         streamDescriptions = []
 
         #
-        # Compare user's producer capabilities with existing stream description capabilities.
-        # New producer capabilities are added to the stream descriptions, and an event
-        # is emitted to alert current participants about the new stream
+        # Compare user's producer capabilities with existing stream
+        # description capabilities. New producer capabilities are added
+        # to the stream descriptions, and an event is emitted to alert
+        # current participants about the new stream
         # 
-        streamCapTypes = map( lambda streamDesc: streamDesc.capability.type, self.streams )
+        streamCapTypes = map( lambda streamDesc: streamDesc.capability.type,
+                              self.streams )
+
         for capability in clientProfile.capabilities:
             if capability.role == Capability.PRODUCER:
                 if capability.type not in streamCapTypes:
@@ -322,15 +398,16 @@ class Venue(ServiceBase.ServiceBase):
 
                     print "* * Adding new producer of type ", capability.type
                     encryptionKey = "venue didn't assign an encryption key"
-                    streamDesc = StreamDescription( "noName", "noDesc", self.AllocateMulticastLocation(), 
-                                                    capability, encryptionKey )
+                    streamDesc = StreamDescription( "noName", "noDesc",
+                                             self.AllocateMulticastLocation(), 
+                                             capability, encryptionKey )
                     self.streams.append( streamDesc )
                     streamDescriptions.append( streamDesc )
-
         
         #
-        # Compare user's consumer capabilities with existing stream description capabilities.
-        # The user will receive a list of compatible stream descriptions
+        # Compare user's consumer capabilities with existing stream
+        # description capabilities. The user will receive a list of
+        # compatible stream descriptions
         # 
         clientConsumerCapTypes = []
         for capability in clientProfile.capabilities:
@@ -342,18 +419,23 @@ class Venue(ServiceBase.ServiceBase):
 
         return streamDescriptions
 
-
     def AllocateMulticastLocation(self):
-        return NetworkLocation.MulticastNetworkLocation( self.multicastAddressAllocator.AllocateAddress(),
-                                                         self.multicastAddressAllocator.AllocatePort(), 
-                                                         self.defaultTtl )
+        """
+        This method creates a new Multicast Network Location.
+        """
+        return NetworkLocation.MulticastNetworkLocation(
+            self.multicastAddressAllocator.AllocateAddress(),
+            self.multicastAddressAllocator.AllocatePort(), 
+            self.defaultTtl )
        
     def GetNextPrivateId( self ):
+        """This method creates the next Private Id."""
         privateId = self.nextPrivateId
         self.nextPrivateId = self.nextPrivateId + 1
         return privateId
 
     def IsValidPrivateId( self, privateId ):
+        """This method verifies the private Id is valid."""
         if privateId in self.users.keys():
             return True 
         return False
