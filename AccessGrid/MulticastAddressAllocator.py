ba00bc3737ca7eb31c46bbe1f9f044dc702610c3
Ivan Judson <judson@mcs.anl.gov>
2003-01-21 17:19:36 +0000

    Changed the setBaseAddress method to take a string, unpack it and do the right thing internally.

--- AccessGrid/MulticastAddressAllocator.py
+++ AccessGrid/MulticastAddressAllocator.py
@@ -5,7 +5,7 @@
 # Author:      Robert Olson, Ivan R. Judson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: MulticastAddressAllocator.py,v 1.4 2003-01-21 16:57:12 turam Exp $
+# RCS-ID:      $Id: MulticastAddressAllocator.py,v 1.5 2003-01-21 17:19:36 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -13,31 +13,22 @@
 import sys
 import socket
 import struct
-
 from random import Random
 import time
+
 import NetworkLocation
 
 class MulticastAddressAllocator:
-    __doc__ = """
-
+    """
+    This class provides a clean API for allocating multicast addresses. It can 
+    provide addresses either randomly from the predefined SDP pool or randomly
+    from a pool provided by the user.
     """
 
     RANDOM = "random"
     INTERVAL = "interval"
 
-    baseAddress = ''
-    addressMask = ''
-
-    allocatedAddresses = []
-    portBase = 0
-    portMask = 0
-    allocatedPorts = []
-    random = None
-    mask = ''
-    
-
-    def __init__(self, baseAddress = None, addressMask = None, 
+    def __init__(self, baseAddress = None, addressMask = None,
                  portBase = 49152, portMask = 0x3fff):
         """
         We initialize this class with the information needed to allocate
@@ -45,33 +36,33 @@ class MulticastAddressAllocator:
         from a GLOP address space. If a base address and mask are not supplied
         the allocator allocates from the SDR address space.
         """
+        self.allocatedAddresses = []
+        self.allocatedPorts = []
+        
         if baseAddress == None:
             # 224.2.128.0
-
-            self.baseAddress = socket.htonl(0xe0028000) 
+            self.baseAddress = socket.htonl(0xe0028000)
         else:
-            self.baseAddress = struct.unpack("L", 
+            self.baseAddress = struct.unpack("L",
                                         socket.inet_aton(baseAddress))[0]
-        
+
         if addressMask == None:
             # 255.255.128.0
             self.addressMask = socket.htonl(0xffff8000)
         else:
             self.addressMask = socket.htonl(~((1 << (32 - addressMask)) - 1))
-            
+
         self.portBase = portBase
         self.portMask = portMask
         self.random = Random(time.clock())
 
         self.addressAllocationMethod = MulticastAddressAllocator.INTERVAL
 
-
-    def SetBaseAddress(self, baseAddress ):
-        #self.lowerAddress = socket.htonl(baseAddress)
-        self.lowerAddress = baseAddress
-
+    def SetBaseAddress(self, baseAddress):
+        self.baseAddress = struct.unpack("L", socket.inet_aton(baseAddress))[0]
+                                        
     def GetBaseAddress(self):
-        return self.lowerAddress
+        return self.baseAddress
 
     def SetAddressMask(self, addressMask = None):
         self.addressMask = addressMask
@@ -84,7 +75,7 @@ class MulticastAddressAllocator:
 
     def GetAddressAllocationMethod( self ):
         return self.addressAllocationMethod
-    
+
     def AllocatePort(self):
         """
         We allocate only even numbered ports since that is the requirement
@@ -92,32 +83,32 @@ class MulticastAddressAllocator:
         """
         randomNumber = self.random.randrange(self.portBase, 65535, 2)
         port = (randomNumber & self.portMask) + self.portBase
-        self.allocatedPorts.append(port)    
+        self.allocatedPorts.append(port)
         return port
-    
+
     def RecyclePort(self, port):
         self.allocatedPorts.remove(port)
-        
+
     def AllocateAddress(self):
-        randomNumber = self.random.randrange(sys.maxint)        
+        randomNumber = self.random.randrange(sys.maxint)
         newAddress = self.baseAddress & self.addressMask
         newAddress = newAddress | (randomNumber & ~self.addressMask)
-    
+
         addressString = socket.inet_ntoa(struct.pack("L", newAddress))
         self.allocatedAddresses.append(addressString)
-        
+
         return addressString
 
     def RecycleAddress(self, address):
         self.addressesInUse.remove(address)
-        
+
 if __name__ == "__main__":
     iter = 10
     multicastAddressAllocator = MulticastAddressAllocator()
     print "%d random addresses: " % (iter)
     for i in range(0, iter):
         print multicastAddressAllocator.AllocateAddress()
-        
+
     multicastAddressAllocator = MulticastAddressAllocator("233.2.171.1", 24)
     print "%d random ANL GLOP addresses: " % (iter)
     for i in range(0, iter):
@@ -125,4 +116,4 @@ if __name__ == "__main__":
 
     print "%d random ports:" % (iter)
     for i in range(0, iter):
-        print multicastAddressAllocator.AllocatePort()    
+        print multicastAddressAllocator.AllocatePort()
