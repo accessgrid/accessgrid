1c8ce769ddc9aade82ac50164d7af1567f0228c7
Tom Uram <turam@mcs.anl.gov>
2003-01-21 18:27:33 +0000

    Add web service methods (prefixed with 'ws'); Implement updates to local venue state based on coherence events; Implement lead/follow methods

--- AccessGrid/VenueClient.py
+++ AccessGrid/VenueClient.py
@@ -2,20 +2,22 @@
 # Name:        VenueClient.py
 # Purpose:     This is the client side object of the Virtual Venues Services.
 #
-# Author:      Ivan R. Judson
+# Author:      Ivan R. Judson, Thomas D. Uram
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: VenueClient.py,v 1.7 2003-01-17 23:10:37 judson Exp $
+# RCS-ID:      $Id: VenueClient.py,v 1.8 2003-01-21 18:27:33 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
+import sys
+from AccessGrid.hosting.pyGlobus.ServiceBase import ServiceBase
 from AccessGrid.hosting.pyGlobus import Client
 from AccessGrid.CoherenceClient import CoherenceClient
 from AccessGrid.ClientProfile import ClientProfile
-from AccessGrid.Types import Event, VenueState
+from AccessGrid.Types import *
 
-class VenueClient:
+class VenueClient( ServiceBase ):
     """
     This is the client side object that maintains a stateful
     relationship with a Virtual Venue. This object provides the
@@ -23,6 +25,7 @@ class VenueClient:
     Interface.  The VenueClient can only be in one venue at a
     time.
     """
+
     def __init__(self, profile=None):
         """
         This client class is used on shared and personal nodes.
@@ -30,182 +33,234 @@ class VenueClient:
         self.profile = profile
         self.nodeServiceUri = None
         self.workspaceDock = None
+        self.homeVenue = None
+        self.followerProfiles = dict()
         self.__InitVenueData__()
+
 
     def __InitVenueData__( self ):
         self.coherenceClient = None
         self.venueState = None
-        self.currentVenue = None
+        self.venueUri = None
+        self.venueProxy = None
         self.privateId = None
         self.streamDescList = []
 
-    def SetProfile(self, profile):
-        """
-        This method sets the profile for the VenueClient, there are currently
-        two types of client profiles -- one for users, and one for nodes.
-        """
-        self.profile = profile
 
-    def coherenceCallback(self, event):
-        """ """
-        print "Got coherence event: " + event.eventType, str(event.data)
+    def SetProfile(self, profile):
+        self.profile = profile
+        
+
+    def CoherenceCallback(self, event):
+
+        coherenceCallbacks = {
+           Event.ENTER: self.venueState.AddUser ,
+           Event.EXIT: self.venueState.RemoveUser ,
+           Event.ADD_DATA: self.venueState.AddData ,
+           Event.REMOVE_DATA: self.venueState.RemoveData ,
+           Event.ADD_SERVICE: self.venueState.AddService ,
+           Event.REMOVE_SERVICE: self.venueState.RemoveService ,
+           Event.ADD_CONNECTION: self.venueState.AddConnection ,
+           Event.REMOVE_CONNECTION: self.venueState.RemoveConnection }
+
+        callback = coherenceCallbacks[event.eventType]
+        callback( event.data )
+
 
     def EnterVenue(self, URL):
         """
         EnterVenue puts this client into the specified venue.
         """
 
-        if self.currentVenue != None:
-            self.ExitVenue()
+        try:
 
-        #
-        # Retrieve list of node capabilities
-        #
-        if self.nodeServiceUri:
-            print "using node service uri ", self.nodeServiceUri
-            self.profile.capabilities = Client.Handle( self.nodeServiceUri ).get_proxy().GetCapabilities()
-            print "Got capabilities "
-            for cap in self.profile.capabilities:
-                print "  ", cap.role, cap.type
+            if self.venueUri != None:
+                self.ExitVenue()
 
-        #
-        # Enter the venue
-        #
-        (venueState, self.privateId, self.streamDescList ) = Client.Handle( URL ).get_proxy().Enter( self.profile )
-        self.venueState = VenueState( venueState.description,
-                                      venueState.connections,
-                                      venueState.users,
-                                      venueState.nodes,
-                                      venueState.data,
-                                      venueState.services,
-                                      venueState.coherenceLocation )
+            #
+            # Retrieve list of node capabilities
+            #
+            if self.nodeServiceUri != None:
+                self.profile.capabilities = Client.Handle( self.nodeServiceUri ).get_proxy().GetCapabilities()
 
-        if self.streamDescList != None:
-            print "streams ------"
-            for stream in self.streamDescList:
-                print "  ", stream.capability.type, stream.capability.role,
-                stream.location.host, stream.location.port
+            #
+            # Enter the venue
+            #
+            (venueState, self.privateId, self.streamDescList ) = Client.Handle( URL ).get_proxy().Enter( self.profile )
+            self.venueState = VenueState( venueState.description, venueState.connections, 
+                                            venueState.users, venueState.nodes, 
+                                            venueState.data, venueState.services, 
+                                            venueState.coherenceLocation )
+            self.venueUri = URL
+            self.venueProxy = Client.Handle( self.venueUri ).get_proxy()
 
-        print dir(self.venueState)
-        print self.venueState.users
-        print dir(self.venueState.users)
-
-        self.currentVenue = URL
-
-        #
-        # Start the coherence client
-        #
-#FIXME - should create coherence client at instantiation, and with each enter
-#        update its host/port (setting the callbacks at instantiation, too)
-        self.coherenceClient = CoherenceClient( self.venueState.coherenceLocation.host,
-                                                self.venueState.coherenceLocation.port,
-                                                self.coherenceCallback )
-        self.coherenceClient.add_callback( Event.ENTER,
-                                           self.venueState.AddUser )
-        self.coherenceClient.add_callback( Event.EXIT,
-                                           self.venueState.RemoveUser )
-        self.coherenceClient.add_callback( Event.ADD_DATA,
-                                           self.venueState.AddData )
-        self.coherenceClient.add_callback( Event.REMOVE_DATA,
-                                           self.venueState.RemoveData )
-        self.coherenceClient.add_callback( Event.ADD_SERVICE,
-                                           self.venueState.AddService )
-        self.coherenceClient.add_callback( Event.REMOVE_SERVICE,
-                                           self.venueState.RemoveService )
-        self.coherenceClient.add_callback( Event.ADD_CONNECTION,
-                                           self.venueState.AddConnection )
-        self.coherenceClient.add_callback( Event.REMOVE_CONNECTION,
-                                           self.venueState.RemoveConnection )
-        self.coherenceClient.start()
+            #
+            # Create the coherence client
+            #
+            self.coherenceClient = CoherenceClient( self.venueState.coherenceLocation.host,
+                                                    self.venueState.coherenceLocation.port,
+                                                    self.CoherenceCallback )
+            self.coherenceClient.start()
 
 
-        #
-        # Update the node service with stream descriptions
-        #
-        if self.nodeServiceUri:
-            Client.Handle( self.nodeServiceUri ).get_proxy().ConfigureStreams( streamDescList )
+            # 
+            # Update the node service with stream descriptions
+            #
+            if self.nodeServiceUri != None:
+                Client.Handle( self.nodeServiceUri ).get_proxy().ConfigureStreams( self.streamDescList )
 
-    def ExitVenue(self):
+            #
+            # Update venue clients being led with stream descriptions
+            #
+            for profile in self.followerProfiles.values():
+                Client.Handle( profile.venueClientUri ).get_proxy().wsEnterVenue( URL )
+
+        except:
+            print "Exception in EnterVenue : ", sys.exc_type, sys.exc_value
+        
+
+    def ExitVenue( self ):
         """
         ExitVenue removes this client from the specified venue.
         """
-        Client.Handle( self.currentVenue ).get_proxy().Exit( self.privateId )
+        self.venueProxy.Exit( self.privateId )
+        self.coherenceClient.stop()
         self.__InitVenueData__()
 
-    def GetVenue(self):
+
+    def GetVenue( self ):
         """
         GetVenue gets the venue the client is currently in.
         """
-        return self.currentVenue
+        return self.venueUri
+        
 
-    def AddService(self, serviceDescription):
-        """
-        AddService adds a service to the client. The expected
-        minimum set of services always present include:
-        Workspace Docking Service
-        Node Service
-        """
-
-    def RemoveService(self, serviceDescription):
-        """
-        RemoveService removes a service from the client.
-        """
-
-    def ModifyService(self, serviceDescription):
-        """
-        ModifyService modifies a service description for a
-        service in the Venue.
-        """
-
-    def GetServices(self):
-        """
-        GetServices returns the list of services provided by this client.
-        """
-
-    def GetHomeVenue(self):
+    def GetHomeVenue( self ):
         """
         GetHomeVenue returns the default venue for this venue client.
         """
+        return self.homeVenue
+        
 
-    def SetHomeVenue(self, venueURL):
+    def SetHomeVenue( self, venueURL):
         """
         SetHomeVenue sets the default venue for this venue client.
         """
+        self.homeVenue = venueURL
+        
 
-    def FollowNode(self, clientProfile):
+    def Follow( self, venueClientUri ):
         """
-        FollowNode tells this venue client to follow the node
+        Follow tells this venue client to follow the node
         specified.
         """
+        Client.Handle( venueClientUri ).get_proxy().wsLead( self.profile )
 
-    def UnfollowNode(self, clientProfile):
+
+    def Unfollow( self, venueClientUri ):
         """
-        UnfollowNode tells this venue client to stop following
+        Unfollow tells this venue client to stop following
         the node specified.
         """
+        Client.Handle( venueClientUri ).get_proxy().wsUnlead( self.profile )
 
-    def LeadNode(self, clientProfile):
+
+    def Lead( self, clientProfile):
         """
-        LeadNode tells this venue client to drag the specified
+        Lead tells this venue client to drag the specified
         node with it.
         """
+        self.followerProfiles[clientProfile.publicId] = clientProfile
+
+
+    def Unlead( self, clientProfile):
+        """
+        Unlead tells this venue client to stop dragging the specified
+        node with it.
+        """
+        for profile in self.followerProfiles.values():
+            if profile.publicId == clientProfile.publicId:
+                del self.followerProfiles[clientProfile.publicId]
+
 
     def SetNodeServiceUri( self, nodeServiceUri ):
+        """
+        Bind the given node service to this venue client
+        """
         self.nodeServiceUri = nodeServiceUri
 
-if __name__ == "__main__":
-    import sys
-    from AccessGrid.hosting.pyGlobus import Client
-    from AccessGrid.hosting.pyGlobus.Server import Server
-    profile = ClientProfile('userProfileExample')
 
-    vc = VenueClient(profile)
-    vc.EnterVenue(sys.argv[1])
 
-    port = 10100
-    if len(sys.argv) > 1:
-        port = int(sys.argv[1])
-    server = Server( port )
-    service = server.create_service_object()
+    #
+    # Web Service methods
+    #
 
-    serviceManager._bind_to_service( service )
+    def wsSetProfile(self, connectionInfo, profile):
+        self.SetProfile( profile )
+    wsSetProfile.pass_connection_info = 1
+    wsSetProfile.soap_export_as = "wsSetProfile"
+
+
+    def wsEnterVenue(self, connectionInfo, venueUri ):
+        try:
+            self.EnterVenue( venueUri )
+        except:
+            print "EXception in wsEnterVenue ", sys.exc_type, sys.exc_value
+    wsEnterVenue.pass_connection_info = 1
+    wsEnterVenue.soap_export_as = "wsEnterVenue"
+        
+
+    def wsExitVenue(self, connectionInfo ):
+        self.ExitVenue()
+    wsExitVenue.pass_connection_info = 1
+    wsExitVenue.soap_export_as = "wsExitVenue"
+        
+
+    def wsGetVenue(self, connectionInfo):
+        return self.GetVenue()
+    wsGetVenue.pass_connection_info = 1
+    wsGetVenue.soap_export_as = "wsGetVenue"
+        
+
+    def wsGetHomeVenue(self, connectionInfo ):
+        return self.GetHomeVenue()
+    wsGetHomeVenue.pass_connection_info = 1
+    wsGetHomeVenue.soap_export_as = "wsGetHomeVenue"
+        
+
+    def wsSetHomeVenue(self, connectionInfo, venueUri ):
+        self.SetHomeVenue( venueUri )
+    wsSetHomeVenue.pass_connection_info = 1
+    wsSetHomeVenue.soap_export_as = "wsSetHomeVenue"
+        
+
+    def wsLead( self, connectionInfo, clientProfile):
+        self.Lead( clientProfile )
+    wsLead.pass_connection_info = 1
+    wsLead.soap_export_as = "wsLead"
+
+
+    def wsUnlead( self, connectionInfo, clientProfile):
+        self.Unlead( clientProfile )
+    wsUnlead.pass_connection_info = 1
+    wsUnlead.soap_export_as = "wsUnlead"
+
+
+    def wsFollow(self, connectionInfo, venueClientUri ):
+        self.Follow( venueClientUri )
+    wsFollow.pass_connection_info = 1
+    wsFollow.soap_export_as = "wsFollow"
+
+
+    def wsUnfollow(self, connectionInfo, venueClientUri ):
+        self.Unfollow( venueClientUri )
+    wsUnfollow.pass_connection_info = 1
+    wsUnfollow.soap_export_as = "wsUnfollow"
+
+
+    def wsSetNodeServiceUri( self, connectionInfo, nodeServiceUri ):
+        self.SetNodeServiceUri( nodeServiceUri )
+    wsSetNodeServiceUri.pass_connection_info = 1
+    wsSetNodeServiceUri.soap_export_as = "wsSetNodeServiceUri"
+
+
