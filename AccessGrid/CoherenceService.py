653fc1727a914d3239d6de47189441ccf07eef62
Ivan Judson <judson@mcs.anl.gov>
2003-01-22 20:28:52 +0000

    Fixed a couple of minor issues related to integration with the VenueServer/Venues.

--- AccessGrid/CoherenceService.py
+++ AccessGrid/CoherenceService.py
@@ -6,15 +6,16 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: CoherenceService.py,v 1.11 2003-01-21 18:37:38 judson Exp $
+# RCS-ID:      $Id: CoherenceService.py,v 1.12 2003-01-22 20:28:52 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
 import socket
+from threading import Thread
+import sys
 from SocketServer import ThreadingTCPServer
 from SocketServer import ThreadingMixIn, StreamRequestHandler
-import sys
 
 from pyGlobus.io import GSITCPSocketServer
 
@@ -26,30 +27,13 @@ def auth_callback(arg, handle, identity, context):
 
 # This really should be defined in pyGlobus.io
 class ThreadingGSITCPSocketServer(ThreadingMixIn, GSITCPSocketServer): pass
- 
-#class CoherenceService(ThreadingTCPServer):
-class CoherenceService(ThreadingGSITCPSocketServer):
-    """
-    The CoherenceService provides a secure event layer. This might be more 
-    scalable as a secure RTP or other UDP solution, but for now we use TCP.
-    In the TCP case the CoherenceService is the Server.
-    """
-    def __init__(self, server_address, RequestHandlerClass):
-        self.connections = []
-        ThreadingGSITCPSocketServer.__init__(self, server_address, RequestHandlerClass)
-#        ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass)
 
-    def distribute(self, data):
-        for c in self.connections:
-            c.wfile.write(data)
-            
 class CoherenceRequestHandler(StreamRequestHandler):
     """
     The CoherenceRequestHandler is the object than handles a single coherence
     connection. This is an attempt to use the ThreadedTCPServer so that I can
     easily change to a GSI TCP solution.
     """
-    
     def stop(self):
         self.running = 0
     
@@ -73,6 +57,34 @@ class CoherenceRequestHandler(StreamRequestHandler):
                 print "Caught exception trying to read data"
                 self.running = 0
                 self.server.connections.remove(self)
+                
+#class CoherenceService(ThreadingTCPServer):
+class CoherenceService(ThreadingGSITCPSocketServer, Thread):
+    """
+    The CoherenceService provides a secure event layer. This might be more 
+    scalable as a secure RTP or other UDP solution, but for now we use TCP.
+    In the TCP case the CoherenceService is the Server.
+    """
+    def __init__(self, server_address, 
+                 RequestHandlerClass=CoherenceRequestHandler):
+        Thread.__init__(self)
+        self.connections = []
+        ThreadingGSITCPSocketServer.__init__(self, server_address, RequestHandlerClass)
+#        ThreadingTCPServer.__init__(self, server_address, RequestHandlerClass)
+
+    def run(self):
+        self.running = 1
+        while(self.running):
+            self.handle_request()
+
+    def stop(self):
+        self.running = 0
+        
+    def distribute(self, data):
+        for c in self.connections:
+            c.wfile.write(data)
+            
+
             
 if __name__ == "__main__":
   import string
@@ -80,5 +92,5 @@ if __name__ == "__main__":
   port = 6500
   print "Creating new CoherenceService at %s %d." % (host, port)
   coherence = CoherenceService((host, port), CoherenceRequestHandler)
-  while(1):
-      coherence.handle_request()
+  coherenceThread = threading.Thread(target=coherence.run)
+  coherenceThread.start()
