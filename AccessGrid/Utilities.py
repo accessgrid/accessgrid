0c550f2f0ee81b0a31a31f76770d065b75d79bcc
Ivan Judson <judson@mcs.anl.gov>
2003-01-22 20:26:32 +0000

    Added exception formatting function. Nice for debugging -- probably not something that should be returned to a SOAP call.

--- AccessGrid/Utilities.py
+++ AccessGrid/Utilities.py
@@ -1,7 +1,17 @@
 import os
 import string
 import sys
+import traceback
 
+def formatExceptionInfo(maxTBlevel=5):
+    cla, exc, trbk = sys.exc_info()
+    excName = cla.__name__
+    try:
+        excArgs = exc.__dict__["args"]
+    except KeyError:
+        excArgs = "<no args>"
+    excTb = traceback.format_tb(trbk, maxTBlevel)
+    return (excName, excArgs, excTb)
 
 def Which( file ):
    paths = string.split( os.environ['PATH'], os.pathsep )
