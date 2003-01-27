e88afc40581187d4956719d312aaa74bfa1ef5e3
Ivan Judson <judson@mcs.anl.gov>
2003-01-27 20:59:03 +0000

    Fixed some odd formatting and added config file stuff to utilities.

--- AccessGrid/Utilities.py
+++ AccessGrid/Utilities.py
@@ -1,11 +1,11 @@
 #-----------------------------------------------------------------------------
 # Name:        Utilities.py
-# Purpose:     
+# Purpose:
 #
 # Author:      Everyone
 #
 # Created:     2003/23/01
-# RCS-ID:      $Id: Utilities.py,v 1.4 2003-01-23 14:28:42 judson Exp $
+# RCS-ID:      $Id: Utilities.py,v 1.5 2003-01-27 20:59:03 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -13,6 +13,34 @@ import os
 import string
 import sys
 import traceback
+import ConfigParser
+
+def LoadConfig(fileName, config={}):
+    """
+    Returns a dictionary with keys of the form <section>.<option>
+    and the corresponding values.
+    This is from the python cookbook credit: Dirk Holtwick.
+    """
+    cp = ConfigParser.ConfigParser()
+    cp.read(fileName)
+    for sec in cp.sections():
+        name = string.lower(sec)
+        for opt in cp.options(sec):
+            config[name + "."
+                   + string.lower(opt)] = string.strip(
+                    cp.get(sec, opt))
+    return config
+
+def SaveConfig(fileName, config):
+    """
+    This method saves the current configuration out to the specified file.
+    """
+    cp = ConfigParser.ConfigParser()
+    for k in config.keys():
+        (section, option) = split(k, '.')
+        cp.set(section, option, config[k])
+        
+    cp.write(file(fileName, 'w+'))
 
 def formatExceptionInfo(maxTBlevel=5):
     cla, exc, trbk = sys.exc_info()
@@ -25,32 +53,32 @@ def formatExceptionInfo(maxTBlevel=5):
     return (excName, excArgs, excTb)
 
 def Which( file ):
-   paths = string.split( os.environ['PATH'], os.pathsep )
-   if sys.platform == "win32" and string.find( file, ".exe" ) == -1:
-      file = file + ".exe"
-   for path in paths:
-      testfile = os.path.join( path, file )
-      if os.path.exists( testfile ):
-         return testfile
+    paths = string.split( os.environ['PATH'], os.pathsep )
+    if sys.platform == "win32" and string.find( file, ".exe" ) == -1:
+        file = file + ".exe"
+    for path in paths:
+        testfile = os.path.join( path, file )
+        if os.path.exists( testfile ):
+            return testfile
 
-   return None
+    return None
 
 from AccessGrid.Types import Capability, AGResource
 
 def GetResourceList():
-   """
-   -- not yet implemented --
-   Placeholder method for resource interrogation.  
-   """
+    """
+    -- not yet implemented --
+    Placeholder method for resource interrogation.
+    """
 
-   print "GetResourceList() : not yet implemented"
-   
-   resources = []
+    print "GetResourceList() : not yet implemented"
 
-   resources.append( AGResource( Capability.VIDEO, "/dev/video0" ) )
-   resources.append( AGResource( Capability.VIDEO, "/dev/video1" ) )
-   resources.append( AGResource( Capability.VIDEO, "/dev/video2" ) )
-   resources.append( AGResource( Capability.VIDEO, "/dev/video3" ) )
-   resources.append( AGResource( Capability.AUDIO, "/dev/audio" ) )
+    resources = []
 
-   return resources
+    resources.append( AGResource( Capability.VIDEO, "/dev/video0" ) )
+    resources.append( AGResource( Capability.VIDEO, "/dev/video1" ) )
+    resources.append( AGResource( Capability.VIDEO, "/dev/video2" ) )
+    resources.append( AGResource( Capability.VIDEO, "/dev/video3" ) )
+    resources.append( AGResource( Capability.AUDIO, "/dev/audio" ) )
+
+    return resources
