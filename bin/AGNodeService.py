7dbe2484c1c26635dee317572085cbcbf8e1b093
Tom Uram <turam@mcs.anl.gov>
2003-01-30 23:41:44 +0000

    Add trivial authorization callback to allow one user to connect to a service running as a different user

--- bin/AGNodeService.py
+++ bin/AGNodeService.py
@@ -2,10 +2,14 @@ import sys
 from AccessGrid.AGNodeService import AGNodeService
 from AccessGrid.hosting.pyGlobus.Server import Server
 
+
+def AuthCallback(server, g_handle, remote_user, context):
+    return 1
+
 nodeService = AGNodeService()
 
 # start the service
-server = Server( int( sys.argv[1] ) )
+server = Server( int( sys.argv[1] ) , auth_callback=AuthCallback )
 service = server.create_service_object("NodeService")
 nodeService._bind_to_service( service )
 print "Starting service; URI: ", nodeService.get_handle()
--- bin/AGServiceManager.py
+++ bin/AGServiceManager.py
@@ -2,8 +2,11 @@ import sys
 from AccessGrid.AGServiceManager import AGServiceManager
 from AccessGrid.hosting.pyGlobus.Server import Server
 
+def AuthCallback(server, g_handle, remote_user, context):
+    return 1
+
 serviceManager = AGServiceManager()
-server = Server( int(sys.argv[1]) )
+server = Server( int(sys.argv[1]), auth_callback=AuthCallback )
 service = server.create_service_object("ServiceManager")
 serviceManager._bind_to_service( service )
 print "Starting service; URI: ", serviceManager.get_handle()
