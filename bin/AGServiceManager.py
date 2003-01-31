c852a8f4b016b6718dd078afc974365a7be982b0
Tom Uram <turam@mcs.anl.gov>
2003-01-31 22:46:40 +0000

    Make AGServiceManager use default port=12000

--- bin/AGServiceManager.py
+++ bin/AGServiceManager.py
@@ -6,7 +6,11 @@ def AuthCallback(server, g_handle, remote_user, context):
     return 1
 
 serviceManager = AGServiceManager()
-server = Server( int(sys.argv[1]), auth_callback=AuthCallback )
+
+port = 12000
+if len(sys.argv) > 1:
+    port = sys.argv[1]
+server = Server( port, auth_callback=AuthCallback )
 service = server.create_service_object("ServiceManager")
 serviceManager._bind_to_service( service )
 print "Starting service; URI: ", serviceManager.get_handle()
