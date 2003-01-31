09be83762436f5cbda894c7ab31b7be078035668
Eric Olson <eolson@mcs.anl.gov>
2003-01-31 20:16:19 +0000

    updated test code

--- AccessGrid/hosting/pyGlobus/tclient.py
+++ AccessGrid/hosting/pyGlobus/tclient.py
@@ -1,7 +1,34 @@
 import Client
+import os
+
+def cb(server, g_handle, remote_user, context):
+    print "got callback"
+    return 1
+
+
+import time
+gt = time.time
 
 #h = Client.Handle('https://localhost:8000/Foobar/Baz')
-h = Client.Handle('https://localhost:8000/100')
+h = Client.Handle('https://localhost:8000/100', authCallback = cb)
 
-ret = h.get_proxy().method(3)
-print "Got '%s' from method"  % (map(lambda x: str(x), ret))
+n = 1
+
+t1 = gt()
+
+ret = None
+
+try:
+    ret = h.get_proxy().method(3)
+except Client.FaultType, f:
+    print "call raised fault ", f, dir(f)
+    print "faultcode='%s'" % (f.faultcode)
+    print "string='%s'" % ( f.faultstring)
+
+else:
+    t2 = gt()
+    print "t1=%s t2=%s" % (t1, t2)
+    print "dur =", t2 - t1
+    print "avg=", (t2 - t1) / n
+
+    print "Got '%s' '%s' from method"  % (ret, map(lambda x: str(x), ret))
--- AccessGrid/hosting/pyGlobus/tserver.py
+++ AccessGrid/hosting/pyGlobus/tserver.py
@@ -1,20 +1,47 @@
-import Server
-import ServiceBase
+import time
+import os
+from AccessGrid.hosting import access_control
+from AccessGrid.hosting.pyGlobus import Server, ServiceBase
 
 class C(ServiceBase.ServiceBase):
-    def meth(self, cinfo, x, **kargs):
-        print "Got meth ", cinfo, x, kargs
-        return ('you sent', cinfo, x)
+    def meth(self, x):
 
-    meth.pass_connection_info = 1
+        print "Got meth: ", x
+
+        sm = access_control.GetSecurityManager()
+
+        print "Executing as subject name: ", sm.GetSubject()
+
+        ident = "/O=Grid/OU=Access Grid/OU=mcs.anl.gov/CN=Bob Olson"
+        if not sm.ValidateUser(ident):
+            raise Exception("Invalid user!")
+        
+        return ('you sent', x)
+    
+
+    meth.pass_connection_info = 0
     meth.soap_export_as = "method"
+
+def cb(server, g_handle, remote_user, context):
+    print "%s Got cb %s" % (clk(), remote_user)
+    return 1
 
 if __name__ == "__main__":
 
-    server = Server.Server(8000)
+    server = Server.Server(8000, auth_callback = cb)
 
     s = server.create_service(C)
 
     print "Have %s %s" % (s, s.get_handle())
 
-    server.run()
+    server.run_in_thread()
+
+    try:
+        while 1:
+            time.sleep(1)
+
+    except Exception, e:
+        print "gotkeybaord int", e
+        os._exit(0)
+        print "Exit done"
+                                    
