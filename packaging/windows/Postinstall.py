b26abf973879c4b095af728e86b77d6ca24a071d
Ti Leggett <leggett@mcs.anl.gov>
2003-02-14 15:25:38 +0000

    Minor cleanups.

--- packaging/windows/Postinstall.py
+++ packaging/windows/Postinstall.py
@@ -4,6 +4,11 @@ import os
 import os.path
 import glob
 
+#
+# The following byte compiles all the AccessGrid modules after they've
+# been installed into their proper place
+#
+
 def modimport(module):
     for module_file in glob.glob(module.__path__[0] + "\\*.py"):
         __import__(module.__name__ + "." + os.path.basename(module_file[:-3]))
