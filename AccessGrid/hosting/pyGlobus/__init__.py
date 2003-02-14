40ac524d170749f3f9edaa758c203d7615be48ed
Eric Olson <eolson@mcs.anl.gov>
2003-02-14 20:53:01 +0000

    added ref to IOException

--- AccessGrid/hosting/pyGlobus/__init__.py
+++ AccessGrid/hosting/pyGlobus/__init__.py
@@ -5,8 +5,12 @@
 # Author:      Robert D. Olson
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: __init__.py,v 1.3 2003-02-10 14:48:06 judson Exp $
+# RCS-ID:      $Id: __init__.py,v 1.4 2003-02-14 20:53:01 olson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
+
+import pyGlobus.io
+
+IOException = pyGlobus.io.IOBaseException
 
