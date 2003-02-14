2ec9310c79aeb690c0226c421d98d85d1fb553d9
Ti Leggett <leggett@mcs.anl.gov>
2003-02-14 15:24:37 +0000

    Initial creation. This file creates the windows registry keys needed for AGNodeService.py to function happily on win32.

---
+++ packaging/windows/AGNodeServicePostinstall.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import _winreg
+
+if sys.platform == 'win32':
+    AG20 = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Access Grid Toolkit\\2.0")
+    ConfigDir, Type = _winreg.QueryValueEx(AG20, "ConfigPath")
+    InstallDir, Type = _winreg.QueryValueEx(AG20, "InstallPath")
+    AGNodeServiceFD = open(os.path.join(ConfigDir, "AGNodeService.cfg"), "w")
+    AGNodeServiceFD.write("[Node Configuration]\n")
+    AGNodeServiceFD.write("servicesDirectory = " + os.path.join(InstallDir, "services\n"))
+    AGNodeServiceFD.write("configDirectory = " + ConfigDir + "\n")
+    AGNodeServiceFD.close()
+    
