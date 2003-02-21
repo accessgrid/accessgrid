2553dfe2123842f13081b1f30b9f5bd1d15df6e6
Susanne Lefvert <lefvert@mcs.anl.gov>
2003-02-21 16:51:02 +0000

    Added static stream check

--- AccessGrid/tests/VenueServerTest.py
+++ AccessGrid/tests/VenueServerTest.py
@@ -5,7 +5,7 @@
 # Author:      Susanne Lefvert
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: VenueServerTest.py,v 1.6 2003-02-14 22:49:27 lefvert Exp $
+# RCS-ID:      $Id: VenueServerTest.py,v 1.7 2003-02-21 16:51:02 lefvert Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
@@ -13,26 +13,25 @@ from AccessGrid.hosting.pyGlobus import Client
 from AccessGrid.Descriptions import VenueDescription
 from AccessGrid.MulticastAddressAllocator import MulticastAddressAllocator
 from AccessGrid.hosting.pyGlobus import Client
+from AccessGrid.Descriptions import StreamDescription, Capability, MulticastNetworkLocation
 import sys
 
 class VenueServerTest:
     def __init__(self, url):
-        print " "
-        print "------------------ CONNECTING TO SERVER"
+        print "\n------------------ CONNECTING TO SERVER"
         
         self.client = Client.Handle(url).get_proxy()
         print "------------------ CONNECT OK"
         
         name = "test venue"
         description = "this is a test venue"
-        print " "
-        print "------------------ INSERT VENUE"
+     
+        print "\n------------------ INSERT VENUE"
         print "name: "+name+"\n"+description
         venue = VenueDescription(name, description, "", None)
         uri = self.client.AddVenue(venue)
         
-        print " "
-        print "------------------ All venues in server"
+        print "\n------------------ All venues in server"
         
         venueList = self.client.GetVenues()
         
@@ -41,72 +40,70 @@ class VenueServerTest:
             
         exit1 = VenueDescription("Exit1", "Exit1 description", "", None)
         exit2 = VenueDescription("Exit2", "Exit2 description", "", None)
-        print " "
-        print "------------------ Set connections"
+      
+        print "\n------------------ Set connections"
         print "name: "+exit1.name+", description: "+ exit1.description
         print "name: "+exit2.name+", description: "+ exit2.description
         
         Client.Handle(uri).get_proxy().SetConnections([exit1, exit2])
-        print " "
-        print "------------------ Get connections"
+       
+        print "\n------------------ Get connections"
          
         exitsList = Client.Handle(uri).get_proxy().GetConnections()
 
         for e in exitsList:
             print "name: "+e.name+", description: "+ e.description
             
-        print " "  
-        print "------------------ Modify The Venue"
+     
+        print "\n------------------ Modify The Venue"
         newName = "newName"
         newDescription = "newDescription"
         print "new venue has name: "+newName+" description: "+newDescription
         venue = VenueDescription(newName, newDescription, "", None)
         self.client.ModifyVenue(uri, venue)
-        print " "
-        print "------------------ All venues in server"
+     
+        print "\n------------------ All venues in server"
         venueList = self.client.GetVenues()
         for v in venueList:
             print "\n name: "+v.name+"\n "+v.description
 
         dnName = "dnName"
-        print " "
-        print "------------------ Add Administrator "+"dn: "+dnName
+     
+        print "\n------------------ Add Administrator "+"dn: "+dnName
 
         self.client.AddAdministrator(dnName)
         adminList = self.client.GetAdministrators()
-        print " "
-        print "------------------ All administrators in server"
+     
+        print "\n------------------ All administrators in server"
         for v in adminList:
             print "\n dnName: "+v
             
-        print " "
-        print "------------------ Remove the administrator"
+     
+        print "\n------------------ Remove the administrator"
         self.client.RemoveAdministrator(dnName)
         adminList = self.client.GetAdministrators()
 
-        print " "
-        print "------------------ All administrators in server"
+        print "\n------------------ All administrators in server"
         for v in adminList:
             print "\n dnName: "+v
 
         address = "225.224.224.224"
-        print " "
-        print "------------------ Set base address: "+address
+   
+        print "\n------------------ Set base address: "+address
         self.client.SetBaseAddress(address)
         address = self.client.GetBaseAddress()
-        print " "
-        print "------------------ Get base address: "+address
+       
+        print "\n------------------ Get base address: "+address
 
         mask = 20
-        print " "
-        print "------------------ Set mask: "+str(mask)
+     
+        print "\n------------------ Set mask: "+str(mask)
         self.client.SetAddressMask(mask)
         theMask = self.client.GetAddressMask()
-        print " "
-        print "------------------ Get mask: "+str(theMask)
+      
+        print "\n------------------ Get mask: "+str(theMask)
 
-        print ""
-        print "------------------ Set allocator =  MulticastAddressAllocator.RANDOM"
+        print "\n------------------ Set allocator =  MulticastAddressAllocator.RANDOM"
         self.client.SetAddressAllocationMethod(MulticastAddressAllocator.RANDOM)
         method = self.client.GetAddressAllocationMethod()
         print " "
@@ -126,19 +123,75 @@ class VenueServerTest:
             print "------------------ Get allocator: MulticastAddressAllocator.INTERVAL"
 
         path = "/homes/"
-        print " "
-        print "------------------ SetStorageLocation: "+path
+      
+        print "\n------------------ SetStorageLocation: "+path
         self.client.SetStorageLocation(path)
         store = self.client.GetStorageLocation()
-        print " "
-        print "------------------ GetStorageLocation: "+store
+    
+        print "\n------------------ GetStorageLocation: "+store
 
-        print " "
-        print "------------------ Remove the venue"
+        videoAddress = '111.111.11.111'
+        videoPort = 1000
+        videoTtl = 1
+       
+        print  "\n------------------ Create static video stream"
+        print '*** Static video stream:'
+        print "host: ", videoAddress
+        print "port: ", videoPort
+        print "time to live: ", videoTtl
+
+        venue = Client.Handle(uri).get_proxy()
+        
+        location = MulticastNetworkLocation(videoAddress, videoPort, videoTtl)
+        capability = Capability( Capability.PRODUCER, Capability.VIDEO)
+        videoStreamDescription = StreamDescription( "", "", location, capability)  
+        videoStreamDescription.static = 1
+        venue.AddStream(videoStreamDescription)
+
+        audioAddress = '222.222.22.222'
+        audioPort = 2000
+        audioTtl = 2
+        print  "\n------------------ Create static audio stream: "
+        print '*** Static audio stream:'
+        print "host: ", audioAddress
+        print "port: ", audioPort
+        print "time to live: ", audioTtl
+        location = MulticastNetworkLocation(audioAddress, audioPort, audioTtl)
+        capability = Capability( Capability.PRODUCER, Capability.AUDIO)
+        audioStreamDescription = StreamDescription( "", "", location, capability)  
+        audioStreamDescription.static = 1
+        venue.AddStream(audioStreamDescription)
+
+        print  "\n------------------ Get static streams"
+        streamList = venue.GetStaticStreams()
+        for stream in streamList:
+
+            if(stream.capability.type == Capability.VIDEO):
+                print "*** Static video stream:"
+                print 'host: ',stream.location.host
+                print 'port: ',stream.location.port
+                print 'time to live: ',stream.location.ttl
+                                    
+            elif(stream.capability.type == Capability.AUDIO):
+                print "\n*** Static audio stream:"
+                print 'host: ',stream.location.host
+                print 'port: ',stream.location.port
+                print 'time to live: ',stream.location.ttl
+               
+        print  "\n------------------ Remove static streams"
+        streamList = venue.GetStaticStreams()
+        for stream in streamList:
+            venue.RemoveStream(stream)
+
+        print  "\n------------------ Get static streams"
+        streamList = venue.GetStaticStreams()
+        print "We have ", len(streamList), " static streams in venue"
+            
+        print "\n------------------ Remove the venue"
         self.client.RemoveVenue(uri)
         self.client.GetVenues()
-        print " "
-        print "------------------ All venues in server"
+        
+        print "\n------------------ All venues in server"
         venueList = self.client.GetVenues()
         for v in venueList:
             print "\n name: "+v.name+"\n "+v.description
