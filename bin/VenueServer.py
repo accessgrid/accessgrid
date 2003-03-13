31a8a421be90e373b4c1fde80009f305acc7a00b
Ivan Judson <judson@mcs.anl.gov>
2003-03-13 12:11:05 +0000

    Added command line parsing for standard options. NOTE: Server Port is no longer valid in the config (it can't be used for anything but reference at that point), it's only a command line option now. Started working on clean shutdown, inserted alot of logging information to track down threads.

--- bin/VenueServer.py
+++ bin/VenueServer.py
@@ -4,12 +4,13 @@
 # Purpose:     This serves Venues.
 # Author:      Ivan R. Judson
 # Created:     2002/12/12
-# RCS-ID:      $Id: VenueServer.py,v 1.11 2003-03-12 08:46:13 judson Exp $
+# RCS-ID:      $Id: VenueServer.py,v 1.12 2003-03-13 12:11:05 judson Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
 import sys
+import getopt
 import signal
 import time
 import logging, logging.handlers
@@ -18,8 +19,13 @@ import threading
 from AccessGrid.hosting.pyGlobus import Server, ServiceBase
 from AccessGrid.VenueServer import VenueServer
 
+# defaults
 running = 0
+port = 8000
+configFile = None
+logFile = "VenueServer.log"
 
+# Signal Handler for clean shutdown
 def SignalHandler(signum, frame):
     """
     SignalHandler catches signals and shuts down the VenueServer (and
@@ -31,29 +37,37 @@ def SignalHandler(signum, frame):
     venueServer.Shutdown(0)
     running = 0
 
+# Authorization callback for the server
 def AuthCallback(server, g_handle, remote_user, context):
     log.debug("Server gets identity ", remote_user)
     return 1
 
-port = 8000
-
-# Once we have optik command line parsing we can make this more
-# elegant and have perhaps
-# -p port
-# -l logfile
-# -f configfile
-
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
-
-configFile = None
-if len(sys.argv) > 2:
-    configFile = sys.argv[2]
+def Usage():
+    print "%s:" % sys.argv[0]
+    print "    -p|--port <int>"
+    print "    -l|--logFile <log file name>"
+    print "    -c|--configFile <config file name>"
     
-logFile = "VenueServer.log"
-if len(sys.argv) > 3:
-    logFile = sys.argv[3]
+# Parse command line options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "p:l:c:h",
+                               ["port", "logfile", "configfile", "help"])
+except getopt.GetoptError:
+    Usage()
+    sys.exit(2)
 
+for o, a in opts:
+    if o in ("-p", "--port"):
+        port = int(a)
+    elif o in ("-l", "--logfile"):
+        logFile = a
+    elif o in ("-c", "--configFile"):
+        configFile = a
+    elif o in ("-h", "--help"):
+        Usage()
+        sys.exit(0)
+
+# Start up the logging
 log = logging.getLogger("AG.VenueServer")
 log.setLevel(logging.DEBUG)
 hdlr = logging.handlers.RotatingFileHandler(logFile, "a", 10000000, 0)
@@ -77,7 +91,6 @@ print "Service running at: %s" % venueServer.GetHandle()
 # We register signal handlers for the VenueServer. In the event of
 # a signal we just try to shut down cleanly.n
 signal.signal(signal.SIGINT, SignalHandler)
-#signal.signal(signal.SIGBREAK, SignalHandler)
 signal.signal(signal.SIGTERM, SignalHandler)
 
 log.debug("Starting Hosting Environment.")
@@ -85,6 +98,8 @@ log.debug("Starting Hosting Environment.")
 hostingEnvironment.RunInThread()
 
 # We run in a stupid loop so there is still a main thread
+# We might be able to simply join the hostingEnvironmentThread, but
+# we have to be able to catch signals.
 running = 1
 while running:
     time.sleep(1)
