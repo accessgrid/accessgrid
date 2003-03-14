34571aae1456b4068f3b100c4250f70177f7756e
Ivan Judson <judson@mcs.anl.gov>
2003-03-14 14:47:01 +0000

    Updated these to use logging, the modules already do (previously done). These are also now using command line arguments for port and logfile customization.

--- bin/AGNodeService.py
+++ bin/AGNodeService.py
@@ -6,17 +6,23 @@
 # Author:      Thomas D. Uram
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: AGNodeService.py,v 1.7 2003-02-12 17:21:46 turam Exp $
+# RCS-ID:      $Id: AGNodeService.py,v 1.8 2003-03-14 14:47:01 judson Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 import sys
 import signal, time, os
+import logging
+import getopt
 
 from AccessGrid.AGNodeService import AGNodeService
 from AccessGrid.hosting.pyGlobus.Server import Server
 
+# default arguments
+port = 11000
+logFile = "/var/log/agns.log"
 
+# Signal handler to catch signals and shutdown
 def SignalHandler(signum, frame):
     """
     SignalHandler catches signals and shuts down the VenueServer (and
@@ -28,28 +34,65 @@ def SignalHandler(signum, frame):
     # shut down the node service, saving config or whatever
     running = 0
 
-
+# Authorization callback for globus
 def AuthCallback(server, g_handle, remote_user, context):
     return 1
 
+# Print usage
+def Usage():
+    print "%s:" % sys.argv[0]
+    print "    -h|--help : print usage"
+    print "    -p|--port <int> : <port number to listen on>"
+    print "    -l|--logFile <filename> : log file name"
+
+# Parse command line options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "p:l:c:h",
+                               ["port", "logfile", "configfile", "help"])
+except getopt.GetoptError:
+    Usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o in ("-p", "--port"):
+        port = int(a)
+    elif o in ("-l", "--logfile"):
+        logFile = a
+    elif o in ("-h", "--help"):
+        Usage()
+        sys.exit(0)
+
+# Start up the logging
+log = logging.getLogger("AG.NodeService")
+log.setLevel(logging.DEBUG)
+hdlr = logging.handlers.RotatingFileHandler(logFile, "a", 10000000, 0)
+fmt = logging.Formatter("%(asctime)s %(levelname)-5s %(message)s", "%x %X")
+hdlr.setFormatter(fmt)
+log.addHandler(hdlr)
+
+# Create a Node Service
 nodeService = AGNodeService()
 
-# start the service
-port = 11000
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
+# Create a hosting environment
 server = Server( port , auth_callback=AuthCallback )
-service = server.create_service_object("NodeService")
-nodeService._bind_to_service( service )
-print "Starting service; URI: ", nodeService.get_handle()
-server.run_in_thread()
 
+# Create the Node Service Service
+service = server.CreateServiceObject("NodeService")
+nodeService._bind_to_service( service )
+
+# Tell the world where to find the service
+log.info("Starting service; URI: %s", nodeService.get_handle())
+
+# Register a signal handler so we can shut down cleanly
 signal.signal(signal.SIGINT, SignalHandler)
 
+# Run the service
+server.run_in_thread()
+
+# Keep the main thread busy so we can catch signals
 running = 1
 while running:
     time.sleep(1)
-
 
 # Exit cleanly
 os._exit(0)
--- bin/AGServiceManager.py
+++ bin/AGServiceManager.py
@@ -6,16 +6,22 @@
 # Author:      Thomas D. Uram
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: AGServiceManager.py,v 1.8 2003-02-12 17:21:46 turam Exp $
+# RCS-ID:      $Id: AGServiceManager.py,v 1.9 2003-03-14 14:47:01 judson Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 import sys
 import signal, time, os
+import logging
 
 from AccessGrid.AGServiceManager import AGServiceManager
 from AccessGrid.hosting.pyGlobus.Server import Server
 
+# default arguments
+port = 12000
+logFile = "/var/log/agsm.log"
+
+# Signal handler to shut down cleanly
 def SignalHandler(signum, frame):
     """
     SignalHandler catches signals and shuts down the VenueServer (and
@@ -27,26 +33,65 @@ def SignalHandler(signum, frame):
     # shut down the node service, saving config or whatever
     running = 0
 
+# Authorization callback for Globus security
 def AuthCallback(server, g_handle, remote_user, context):
     return 1
 
+# Print out the usage
+def Usage():
+    print "%s:" % sys.argv[0]
+    print "    -h|--help : print usage"
+    print "    -p|--port <int> : <port number to listen on>"
+    print "    -l|--logFile <filename> : log file name"
+
+# Parse command line options
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "p:l:c:h",
+                               ["port", "logfile", "configfile", "help"])
+except getopt.GetoptError:
+    Usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o in ("-p", "--port"):
+        port = int(a)
+    elif o in ("-l", "--logfile"):
+        logFile = a
+    elif o in ("-h", "--help"):
+        Usage()
+        sys.exit(0)
+
+# Start up the logging
+log = logging.getLogger("AG.ServiceManager")
+log.setLevel(logging.DEBUG)
+hdlr = logging.handlers.RotatingFileHandler(logFile, "a", 10000000, 0)
+fmt = logging.Formatter("%(asctime)s %(levelname)-5s %(message)s", "%x %X")
+hdlr.setFormatter(fmt)
+log.addHandler(hdlr)
+
+# Create the Service Manager
 serviceManager = AGServiceManager()
 
-port = 12000
-if len(sys.argv) > 1:
-    port = int(sys.argv[1])
+# Create the hosting environment
 server = Server( port, auth_callback=AuthCallback )
-service = server.create_service_object("ServiceManager")
-serviceManager._bind_to_service( service )
-print "Starting service; URI: ", serviceManager.get_handle()
-server.run_in_thread()
 
+# Create the Service Manager Service
+service = server.CreateServiceObject("ServiceManager")
+serviceManager._bind_to_service( service )
+
+# Tell the world where to find the service manager
+log.info("Starting service; URI: %s", serviceManager.get_handle())
+
+# Register the signal handler so we can shut down cleanly
 signal.signal(signal.SIGINT, SignalHandler)
 
+# Start the service
+server.run_in_thread()
+
+# Keep the main thread busy so we can catch signals
 running = 1
 while running:
     time.sleep(1)
-
 
 # Exit cleanly
 os._exit(0)
