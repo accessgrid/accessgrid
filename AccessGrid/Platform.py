3760f7a0af2e775d931990532a6b846832ec781b
Ivan Judson <judson@mcs.anl.gov>
2003-03-21 16:09:55 +0000

    Removed prints from GPI method.

--- AccessGrid/Platform.py
+++ AccessGrid/Platform.py
@@ -5,7 +5,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2003/09/02
-# RCS-ID:      $Id: Platform.py,v 1.10 2003-03-14 16:32:15 turam Exp $
+# RCS-ID:      $Id: Platform.py,v 1.11 2003-03-21 16:09:55 judson Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
@@ -20,9 +20,7 @@ def GPI():
     GlobusBin = os.path.join(os.environ['GLOBUS_LOCATION'], 'bin')
     files = os.listdir(GlobusBin)
     for f in files:
-        print "File: %s" % f
         if f == WinGPI:
-            print "WINDOWS: %s" % os.path.join(GlobusBin, f)
             os.spawnv(os.P_WAIT, os.path.join(GlobusBin, f), [])
         elif f == LinuxGPI:
             print "LINUX %s" % os.path.join(GlobusBin, f)
