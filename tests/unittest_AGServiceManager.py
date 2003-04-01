a8abf9f54003ccbde8eb8e0eafec2cd2fa5eb0c6
Eric Olson <eolson@mcs.anl.gov>
2003-04-01 23:09:58 +0000

    Initial checkin of basic pyunit tests.  Description below:
    At the moment not many real tests are included, but since Tom was committing tests, I thought I would get a sample test in cvs in case he wanted to use the pyunit format now or later.  Using pyunit will let us run multiple tests quickly and automatically.
    
    Notes:
    unittest_AGServiceManager.py is a pretty simple example.
    
    When defining a test class, define as many tests as you need in the form "testSomeName()", and define setUp() and/or tearDown() if you need to do anything before or after the group of tests.  Define multiple test classes if you need different setUp()'s or want to separate unrelated tests.
    
    After you define a test class, just add it to the list in the suite() function at the bottom of the file.
    
    When you're finished, you can add your test filename to the list in unittest_all.py.
    
    Run your test by itself or in unittest_all.py.

---
+++ tests/unittest_AGServiceManager.py
@@ -0,0 +1,57 @@
+#-----------------------------------------------------------------------------
+# Name:        unittest_AGServiceManager.py
+# Purpose:     
+#
+# Author:      Eric C. Olson
+#   
+# Created:     2003/04/01
+# RCS-ID:    
+# Copyright:   (c) 2003
+# Licence:     See COPYING.txt
+#-----------------------------------------------------------------------------
+
+import unittest
+import signal, time, os
+from AccessGrid.AGServiceManager import AGServiceManager
+from AccessGrid.hosting.pyGlobus.Server import Server
+from AccessGrid.Platform import GetConfigFilePath, AGTK_LOCATION 
+from AccessGrid.Platform import *
+
+class AGServiceManagerBasicTestCase(unittest.TestCase):
+    """Basic test case for AGServiceManager."""
+
+    def testInit(self):
+        """Test initialization"""
+        # For testing we'll change to a local test config path.
+        os.environ[AGTK_LOCATION] = "testInstall"
+        self.serviceManager = AGServiceManager()
+        del self.serviceManager
+
+class AGServiceManagerBasicCase(unittest.TestCase):
+
+    def setUp(self):
+        """Test initialization"""
+        # For testing we'll change to a local test config path.
+        os.environ[AGTK_LOCATION] = "testInstall"
+        self.serviceManager = AGServiceManager()
+
+    def testValidInitialization(self):
+        assert None != self.serviceManager.authManager
+        assert None != self.serviceManager.processManager
+
+  # def testOtherTestsHere
+
+    def tearDown(self):
+        del self.serviceManager
+
+
+def suite():
+    """Returns a suite containing all the test cases in this module."""
+    suite1 = unittest.makeSuite(AGServiceManagerBasicTestCase)
+    suite2 = unittest.makeSuite(AGServiceManagerTestCase)
+    return unittest.TestSuite((suite1, suite2))
+
+if __name__ == '__main__':
+    # When this module is executed from the command-line, run all its tests
+    unittest.main()
+
---
+++ tests/unittest_all.py
@@ -0,0 +1,26 @@
+#-----------------------------------------------------------------------------
+# Name:        unittest_all.py
+# Purpose:     
+#
+# Author:      Eric C. Olson
+#   
+# Created:     2003/04/01
+# RCS-ID:    
+# Copyright:   (c) 2003
+# Licence:     See COPYING.txt
+#-----------------------------------------------------------------------------
+
+import unittest
+
+def suite():
+    # List modules to test
+    modules_to_test = ('unittest_AGServiceManager' ,'unittest_AGServiceManager') 
+
+    alltests = unittest.TestSuite()
+    for module in map(__import__, modules_to_test):
+       alltests.addTest(unittest.findTestCases(module))
+    return alltests
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='suite')
+
