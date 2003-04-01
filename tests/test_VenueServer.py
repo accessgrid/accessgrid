8af465bed72b3804dec8bc5412bf3cef9afa3e33
Ivan Judson <judson@mcs.anl.gov>
2003-04-01 23:28:16 +0000

    Updated to use venue descriptions.

--- tests/test_VenueServer.py
+++ tests/test_VenueServer.py
@@ -5,7 +5,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/17/12
-# RCS-ID:      $Id: test_VenueServer.py,v 1.6 2003-03-25 17:03:15 judson Exp $
+# RCS-ID:      $Id: test_VenueServer.py,v 1.7 2003-04-01 23:28:16 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -16,6 +16,7 @@ import sys
 from AccessGrid.hosting.pyGlobus import Client
 from AccessGrid.ClientProfile import ClientProfile
 from AccessGrid.Descriptions import DataDescription, ConnectionDescription
+from AccessGrid.Descriptions import CreateVenueDescription, VenueDescription
 
 #
 # Set up the venue space
@@ -26,41 +27,46 @@ if len(sys.argv) > 1:
    venueServerUri = sys.argv[1]
 venueServerProxy = Client.Handle( venueServerUri ).get_proxy()
 
-
 # add venues
-outsideVenueUri = venueServerProxy.AddVenue( "Outside", "Outside venue" )
-insideVenueUri = venueServerProxy.AddVenue( "Inside", "Inside venue" )
-print "outsideVenueUri = ",  outsideVenueUri
-print "insideVenueUri = ", insideVenueUri
+outsideDesc = VenueDescription("Outside", "Outside venue description")
+insideDesc = VenueDescription("Inside", "Inside venue description")
+
+outsideDesc.uri = venueServerProxy.AddVenue(outsideDesc)
+insideDesc.uri = venueServerProxy.AddVenue(insideDesc)
+
+print "outsideVenueUri = ",  outsideDesc.uri
+print "insideVenueUri = ", insideDesc.uri
 
 # make connections between venues, using SetConnections method
 connectionList = [ ConnectionDescription("Inside", "The inside one.",
-                                         insideVenueUri)]
-Client.Handle( outsideVenueUri ).get_proxy().SetConnections( connectionList )
+                                         insideDesc.uri)]
+Client.Handle( outsideDesc.uri ).get_proxy().SetConnections( connectionList )
 connectionList = [ ConnectionDescription("Outside", "The outside one.",
-                                         outsideVenueUri)]
-Client.Handle( insideVenueUri ).get_proxy().SetConnections( connectionList )
+                                         outsideDesc.uri)]
+Client.Handle( insideDesc.uri ).get_proxy().SetConnections( connectionList )
 
 # add some data to each venue
 dataDescription = DataDescription("sample.data")
-Client.Handle( outsideVenueUri ).get_proxy().AddData( dataDescription )
+Client.Handle( outsideDesc.uri ).get_proxy().AddData( dataDescription )
 dataDescription = DataDescription("moreSample.data")
-Client.Handle( outsideVenueUri ).get_proxy().AddData( dataDescription )
+Client.Handle( outsideDesc.uri ).get_proxy().AddData( dataDescription )
 
 
 print "-- Venues -- "
 venueList = venueServerProxy.GetVenues()
-for venue in venueList:
-    print "  ", venue.name, venue.description, venue.uri
-    connectionList = Client.Handle( venue.uri ).get_proxy().GetConnections()
-    for connection in connectionList:
+print venueList, dir(venueList)
+for v in venueList:
+   venue = CreateVenueDescription(v)
+   print "  ", venue.name, venue.description, venue.uri
+   connectionList = Client.Handle( venue.uri ).get_proxy().GetConnections()
+   for connection in connectionList:
         print "      connection : ", connection.name, connection.description, connection.uri
 
 newname = "Modified outside venue."
 newdesc = "modified outside venue desc"
 
-Client.Handle(outsideVenueUri).get_proxy().SetName(newname)
-Client.Handle(outsideVenueUri).get_proxy().SetDescription(newdesc)
+Client.Handle(outsideDesc.uri).get_proxy().SetName(newname)
+Client.Handle(outsideDesc.uri).get_proxy().SetDescription(newdesc)
 
 print "-- Venues -- "
 venueList = venueServerProxy.GetVenues()
