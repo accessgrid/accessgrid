cf76f2f6a7a11ad941421a0ae9b7969d10bb86c1
Tom Uram <turam@mcs.anl.gov>
2003-04-03 18:12:38 +0000

    Fix update call so nodemgmt ui actually gets updated

--- bin/NodeManagement.py
+++ bin/NodeManagement.py
@@ -6,7 +6,7 @@
 # Author:      Thomas D. Uram
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: NodeManagement.py,v 1.13 2003-02-28 18:00:33 turam Exp $
+# RCS-ID:      $Id: NodeManagement.py,v 1.14 2003-04-03 18:12:38 turam Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
@@ -24,7 +24,7 @@ class MyApp(wxApp):
         frame = NodeManagementClientFrame(NULL, -1, "Access Grid Node Management")
         try:
             frame.AttachToNode( "https://localhost:11000/NodeService" )
-            frame.Update()
+            frame.UpdateUI()
         except:
             pass
         frame.Show(true)
