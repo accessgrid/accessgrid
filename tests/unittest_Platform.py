40089769d9731f31fb628379dc6ea6b9f4da9fa5
Eric Olson <eolson@mcs.anl.gov>
2003-04-03 21:41:42 +0000

    more tests. This now has a GPI test, that we'll have to evaluate if we want to run tests automatically (as it brings up a GUI on windows).

--- tests/unittest_Platform.py
+++ tests/unittest_Platform.py
@@ -11,7 +11,9 @@
 #-----------------------------------------------------------------------------
 
 import unittest
-import signal, time, os
+import os
+import os.path
+import tempfile
 from AccessGrid import Platform
 
 class DefaultPaths(unittest.TestCase):
@@ -64,10 +66,53 @@ class DefaultPaths(unittest.TestCase):
         free = Platform.GetFilesystemFreeSpace("/")
         print "Free space: ", free
 
+class EnvPaths(unittest.TestCase):
+
+    def setUp(self):
+        self.userdir = tempfile.mktemp()
+        self.sysdir = tempfile.mktemp()
+        self.foodir = os.path.join(self.sysdir, "foo")
+        os.mkdir(self.userdir)
+        os.mkdir(self.sysdir)
+        os.mkdir(self.foodir)
+        os.environ[Platform.AGTK_LOCATION] = self.sysdir
+        os.environ[Platform.AGTK_USER] = self.userdir
+
+    def tearDown(self):
+        os.rmdir(self.foodir)
+        os.rmdir(self.userdir)
+        os.rmdir(self.sysdir)
+    
+    def testSystem(self):
+        configDir = Platform.GetSystemConfigDir()
+        print "System config dir: ", configDir
+        assert configDir != "" and configDir is not None, 'empty config dir'
+        assert configDir == self.sysdir
+    
+    def testUser(self):
+        configDir = Platform.GetUserConfigDir()
+        print "User config dir: ", configDir
+        assert configDir != "" and configDir is not None, 'empty config dir'
+        assert configDir == self.userdir
+
+    def testConfigFile(self):
+        #
+        # Test for a config directory that's probably not there
+        #
+        configDir = Platform.GetConfigFilePath("foo")
+        print "Got foo dir ", configDir
+        assert configDir == self.foodir
+
+class GPI(unittest.TestCase):
+    def testGPI(self):
+        Platform.GPI()
+
 def suite():
     """Returns a suite containing all the test cases in this module."""
     suite1 = unittest.makeSuite(DefaultPaths)
-    return unittest.TestSuite((suite1,))
+    suite2 = unittest.makeSuite(EnvPaths)
+    suite3 = unittest.makeSuite(GPI)
+    return unittest.TestSuite((suite1, suite2, suite3))
 
 if __name__ == '__main__':
     # When this module is executed from the command-line, run all its tests
