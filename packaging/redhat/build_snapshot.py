6b7790ebca0785eaee3f9467c43a3ce546781f20
Eric Olson <eolson@mcs.anl.gov>
2003-04-23 18:26:02 +0000

    A few small additions.
    Output a few more things in verbose mode.
    
    Provide command-line options --specdir and --rpmsrcdir for changing the only
    redhat specific paths I saw.  I don't know how much work it would take
    to build rpms for other distributions like mandrake, but I figured I
    would at least provide an option to change paths in our build script that
    are redhat specific.
    
    If --tempdir wasn't already available, it is now and lets you specify the
    temporary build directory.

--- packaging/redhat/build_snapshot.py
+++ packaging/redhat/build_snapshot.py
@@ -13,6 +13,9 @@
 # chdir to /usr/src/redhat/SPECS
 # run the build: rpm -ba AccessGrid.spec
 #
+# You can use command line options --specdir and --rpmsrcdir to change
+#   rpm locations for other systems.  If you need do that, you probably
+#   need make similar entries in your .rpmrc file.
 
 import sys
 import shutil
@@ -23,11 +26,21 @@ import re
 import getopt
 
 def usage():
+    print ""
     print "%s:" % sys.argv[0]
     print "    -h|--help : print usage"
-    print "    -n|--buildname <buildversionname> : specify build version name (such as \"2.0beta3\""
-    print "    -r|--releasename <releasename> : specify release name (such as \"12\")"
+    print "    -n|--buildname <buildversionname> : specify build version name"
+    print "           such as: \"2.0beta\""
+    print "    -r|--releasename <releasename> : specify release name (such as \"3\")"
+    print "    -t|--tempdir <tempdir> : specify temp build directory.  default: /tmp/snaps"
+    print "    -p|--specdir <specdir> : specify spec directory. "
+    print "           default: /usr/src/redhat/SPECS"
+    print "           if not default, should reflect what's in your .rpmrc."
+    print "    -o|--rpmsrcdir <rpmsrcdir> : specify rpm src directory."
+    print "           default: /usr/src/redhat/SOURCES"
+    print "           if not default, should reflect what's in your .rpmrc."
     print "    -v|--verbose : print extra inforation"
+    print ""
 
 # temp directory
 build_base = "/tmp/snap"
@@ -38,15 +51,21 @@ build_tag = time.strftime("%Y-%m%d-%H%M%S")
 # The version and release that goes into the RPM.
 build_version = "2.0beta"
 build_release = time.strftime("%Y%m%d%H%M%S")
+default_spec_dir = "/usr/src/redhat/SPECS"
+spec_dir = default_spec_dir
 
+# Default rpm directories
+default_rpm_srcdir = "/usr/src/redhat/SOURCES"
+rpm_srcdir = default_rpm_srcdir
 
 # Verbosity flag
 verbose = 0
 
+
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "n:r:t:hv",
-                               ["buildname=", "releasename=",
-                                "tempdir=", "help", "verbose"])
+    opts, args = getopt.getopt(sys.argv[1:], "n:r:t:m:p:vh",
+                               ["buildname=", "releasename=", "tempdir=",
+                                "rpmsrcdir=", "specdir=", "verbose", "help"])
 except:
     usage()
     sys.exit(2)
@@ -54,10 +73,14 @@ except:
 for o, a in opts:
     if o in ("-n", "--buildname"):
         build_version = a
-    if o in ("-r", "--releasename"):
+    elif o in ("-r", "--releasename"):
         build_release = a
-    if o in ("-t", "--tempdir"):
+    elif o in ("-t", "--tempdir"):
         build_base = a
+    elif o in ("-m", "--rpmsrcdir"):
+        rpm_srcdir = a
+    elif o in ("-p", "--specdir"):
+        spec_dir = a
     elif o in ("-v", "--verbose"):
         verbose = 1
     elif o in ("-h", "--help"):
@@ -70,7 +93,12 @@ if verbose:
     print "\nbuild version:",build_version
     print "release name:",build_release
     print "temp dir:",build_base
-    print "full temp path:",build_dir
+    print "full temp path (build dir):",build_dir
+
+if spec_dir != default_spec_dir:
+    print "spec directory:",spec_dir
+if rpm_srcdir != default_rpm_srcdir:
+    print "rpm source directory:",rpm_srcdir
 
 os.makedirs(build_dir)
 os.chdir(build_dir)
@@ -90,17 +118,25 @@ if rc != 0:
 #
 
 os.rename("AccessGrid", "AccessGrid-%s" % (build_version))
-r = os.system("tar czf /usr/src/redhat/SOURCES/AccessGrid-%s.tar.gz AccessGrid-%s" % (build_version, build_version))
+tar_dst_filename = "AccessGrid-%s.tar.gz" % (build_version)
+tar_src_filename = "AccessGrid-%s" % (build_version)
+
+# default rpm_srcdir = "/usr/src/redhat/SOURCES"
+tar_command = "tar czf " + os.path.join(rpm_srcdir, tar_dst_filename + " " + tar_src_filename)
+r = os.system(tar_command)
 if r != 0:
-    print "tar failed with rc ", rc
+    print "tar command \"", tar_command, "\" failed with rc ", rc
     sys.exit(1)
 
 #
 # Copy the spec file into the redhat dir.
 #
 
-fh_orig = open("AccessGrid-%s/packaging/redhat/AccessGrid.spec" % (build_version), "r")
-fh_new = open("/usr/src/redhat/SPECS/AccessGrid.spec", "w")
+fh_orig_filename = "AccessGrid-%s/packaging/redhat/AccessGrid.spec" % (build_version)
+
+fh_orig = open(fh_orig_filename, "r")
+fh_new_filename = os.path.join(spec_dir, "AccessGrid.spec")
+fh_new = open(fh_new_filename, "w")
 
 #
 # Read lines, replacing the version and release with what we want.
@@ -117,7 +153,11 @@ for l in fh_orig:
 fh_orig.close()
 fh_new.close()
 
-os.chdir("/usr/src/redhat/SPECS")
+#os.chdir("/usr/src/redhat/SPECS")
+os.chdir(spec_dir)
 
-os.system("rpm -ba AccessGrid.spec")
+rpm_command = "rpm -ba AccessGrid.spec"
+if verbose:
+    print "rpm command:",rpm_command
+    os.system(rpm_command)
 
