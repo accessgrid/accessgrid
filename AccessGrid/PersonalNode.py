debc7bc0d6d80624fa23da410d73623aa6731a3f
Eric Olson <eolson@mcs.anl.gov>
2003-04-24 19:04:40 +0000

    fix import path

--- AccessGrid/PersonalNode.py
+++ AccessGrid/PersonalNode.py
@@ -5,7 +5,7 @@
 # Author:      Robert Olson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: PersonalNode.py,v 1.9 2003-04-23 19:26:23 olson Exp $
+# RCS-ID:      $Id: PersonalNode.py,v 1.10 2003-04-24 19:04:40 olson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -14,14 +14,14 @@ import sys
 
 if sys.platform == "win32":
 
-    import PersonalNodeWin32
+    from AccessGrid import PersonalNodeWin32
     PersonalNodeManager = PersonalNodeWin32.PersonalNodeManager
     PN_NodeService = PersonalNodeWin32.PN_NodeService
     PN_ServiceManager = PersonalNodeWin32.PN_ServiceManager
 
 else:
 
-    import PersonalNodeLinux
+    from AccessGrid import PersonalNodeLinux
     PersonalNodeManager = PersonalNodeLinux.PersonalNodeManager
     PN_NodeService = PersonalNodeLinux.PN_NodeService
     PN_ServiceManager = PersonalNodeLinux.PN_ServiceManager
