10cfa4a49f265a8b5a8134194b4fdc7ca9695132
Tom Uram <turam@mcs.anl.gov>
2003-04-24 22:13:48 +0000

    Moved file here from AccessGrid/AccessGrid/services

---
+++ services/node/__init__.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        __init__.py
+# Purpose:     
+#
+# Author:      Everyone
+#
+# Created:     2003/08/02
+# RCS-ID:      $Id: __init__.py,v 1.1 2003-04-24 22:13:48 turam Exp $
+# Copyright:   (c) 2002
+# Licence:     See COPYING.txt
+#-----------------------------------------------------------------------------
+# Package initialisation
