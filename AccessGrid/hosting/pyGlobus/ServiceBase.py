19005109c25bf5f433adee792f2c616edd92a8a6
Ivan Judson <judson@mcs.anl.gov>
2003-04-28 18:05:43 +0000

    Added unregister methods to fix bug #188. Added doc strings.

--- AccessGrid/hosting/pyGlobus/ServiceBase.py
+++ AccessGrid/hosting/pyGlobus/ServiceBase.py
@@ -5,7 +5,7 @@
 # Author:      Robert D. Olson
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: ServiceBase.py,v 1.6 2003-02-14 20:51:38 olson Exp $
+# RCS-ID:      $Id: ServiceBase.py,v 1.7 2003-04-28 18:05:43 judson Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
@@ -14,14 +14,31 @@ import re
 class ServiceBase:
 
     def _bind_to_service(self, service_obj):
+        """
+        Binds this object to an underlying web service.
+        """
         self._register_exports(service_obj)
         self._service_object = service_obj
 
+    def _UnbindFromService(self):
+        """
+        Removes the binding of this object to the underlying web service.
+        """
+        self._UnregisterExports(self._service_object)
+        self._service_object = None
+        
     def GetHandle(self):
+        """
+        Return the url to the web service for this web service object.
+        """
         return self._service_object.get_handle()
 
     def _register_exports(self, service_obj):
-
+        """
+        This method searches for methods that are supposed to be exported
+        via the underlying web service object. It then registers them for
+        exporting.
+        """
         #
         # Walk our list of attributes looking for
         # methods with docstrings.
@@ -32,8 +49,10 @@ class ServiceBase:
 
         # print "Register exports for ", self
 
-        export_regexp = re.compile("^\s+soap_export_as:\s*(\S+)\s*$", re.MULTILINE)
-        cinfo_regexp =  re.compile("^\s+pass_connection_info:\s*(\S+)\s*$", re.MULTILINE)
+        export_regexp = re.compile("^\s+soap_export_as:\s*(\S+)\s*$",
+                                   re.MULTILINE)
+        cinfo_regexp =  re.compile("^\s+pass_connection_info:\s*(\S+)\s*$",
+                                   re.MULTILINE)
 
         exported_methods = {}
         
@@ -66,7 +85,6 @@ class ServiceBase:
                     matches = export_regexp.findall(dstr)
                     for export in matches:
                         # print "Export %s as %s %s" % (value, export, method)
-
                         exported_methods[name] = (method, export, pass_cinfo)
 
                 #
@@ -82,6 +100,21 @@ class ServiceBase:
 
             for method, export_name, pass_cinfo in exported_methods.values():
                 service_obj.register_function(method, export_name, pass_cinfo)
+
+        # We keep the names for later unregistering them :-)
+        self.exportedMethodNames = map( lambda thing:
+                                        thing[0].__name__,
+                                        exported_methods.values() )
+        
+    def _UnregisterExports(self, service_obj):
+        """
+        This method unregisters the web service methods from the server,
+        this makes them unavailable to outside callers and allows python
+        garbage collection to properly cleanup.
+        """
+
+        for method in self.exportedMethodNames:
+            self._service_object.UnregisterFunction(method)
 
     #
     # Mappings to other function naming style.
