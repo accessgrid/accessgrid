2ea64c8fddb740d5399b338ad2a444a9d6649daa
Eric Olson <eolson@mcs.anl.gov>
2003-05-20 19:39:26 +0000

    Asynchronous IO mods, client side.

--- AccessGrid/EventClient.py
+++ AccessGrid/EventClient.py
@@ -6,7 +6,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: EventClient.py,v 1.16 2003-05-08 22:02:56 judson Exp $
+# RCS-ID:      $Id: EventClient.py,v 1.17 2003-05-20 19:39:26 olson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -25,6 +25,7 @@ from AccessGrid.Events import HeartbeatEvent, ConnectEvent, DisconnectEvent
 from AccessGrid.hosting.pyGlobus.Utilities import CreateTCPAttrAlwaysAuth
 
 log = logging.getLogger("AG.VenueClient")
+log.setLevel(logging.INFO)
 
 class EventClientConnectionException(Exception):
     """
@@ -52,7 +53,7 @@ class EventClientWriteDataException(Exception):
     """
     pass
 
-class EventClient(Thread):
+class EventClient:
     """
     The Event Client maintains a client side connection to the Event
     Service to maintain state among a set of clients. This is done by sending
@@ -69,7 +70,8 @@ class EventClient(Thread):
         self.buffer = Buffer(EventClient.bufsize)
         self.location = location
         self.callbacks = []
-        Thread.__init__(self)
+
+#        Thread.__init__(self)
         
         attr = CreateTCPAttrAlwaysAuth()
         self.sock = GSITCPSocket()
@@ -79,9 +81,75 @@ class EventClient(Thread):
             log.exception("Couldn't connect to event service.")
             raise EventClientConnectionException
         
-        self.rfile = self.sock.makefile('rb', -1)
+        # self.rfile = self.sock.makefile('rb', -1)
 
-    def run(self):
+    def start(self):
+        """
+        Register for asynch io.
+        """
+
+        self.dataBuffer = ''
+        self.waitingLen = 0
+        
+        h = self.sock.register_read(self.buffer, EventClient.bufsize, 1,
+                                    self.readCallbackWrap, None)
+        self.cbHandle = h
+
+    def readCallbackWrap(self, arg, handle, ret, buf, n):
+        """
+        Asynch read callback.
+
+        We just use this to wrap the call to the readCallback itself,
+        because the callback invocation mechanism silently ignores exceptions.
+        """
+        
+        try:
+            return self.readCallback(arg, handle, ret, buf, n)
+        except Exception, e:
+            log.exception("readcallback failed")
+                
+
+    def readCallback(self, arg, handle, ret, buf, n):
+
+        log.debug("Got read handle=%s ret=%s  n=%s \n", handle, ret, n)
+
+        if n == 0:
+            log.debug("EventClient got EOF")
+            return
+
+        dstr = str(buf)
+        self.dataBuffer += dstr
+
+        if self.waitingLen == 0:
+
+            sz = struct.calcsize('i')
+            
+            if len(self.dataBuffer) < sz:
+                return
+
+            lenstr = self.dataBuffer[:sz]
+            dlen = struct.unpack('i', lenstr)
+
+            self.dataBuffer = self.dataBuffer[sz:]
+
+            self.waitingLen = dlen[0]
+
+        if len(self.dataBuffer) >= self.waitingLen:
+            log.debug("finally read enough data, wait=%s buflen=%s",
+                      self.waitingLen, len(self.dataBuffer))
+
+            thedata = self.dataBuffer[:self.waitingLen]
+            self.dataBuffer = self.dataBuffer[self.waitingLen:]
+
+            self.handleData(thedata)
+
+            self.waitingLen = 0
+            
+        h = self.sock.register_read(self.buffer, EventClient.bufsize, 1,
+                                    self.readCallbackWrap, None)
+        self.cbHandle = h
+
+    def thr_run(self):
         """
         The run method starts this thread actively getting and
         processing event data provided by a EventService.
@@ -118,23 +186,27 @@ class EventClient(Thread):
                 log.exception("EventClient: Read data failed.")
                 raise EventClientReadDataException
 
-            # Unpack the data
-            event = pickle.loads(pdata)
-
-            # Invoke registered callbacks
-            calls = []
-            for (evt, callback) in self.callbacks:
-                if evt == event.eventType:
-                    calls.append(callback)
-                    
-                    if len(calls) != 0:
-                        for callback in calls:
-                            callback(event.data)
-                        else:
-                            log.info("No callback for %s!", event.eventType)
-                            self.DefaultCallback(event)
+            self.handleData(pdata)
 
         self.sock.close()
+
+    def handleData(self, pdata):
+        # Unpack the data
+        event = pickle.loads(pdata)
+
+        # Invoke registered callbacks
+        calls = []
+        for (evt, callback) in self.callbacks:
+            if evt == event.eventType:
+                calls.append(callback)
+
+                if len(calls) != 0:
+                    for callback in calls:
+                        callback(event.data)
+                    else:
+                        log.info("No callback for %s!", event.eventType)
+                        self.DefaultCallback(event)
+
 
     def Send(self, data):
         """
@@ -153,9 +225,11 @@ class EventClient(Thread):
             raise EventClientWriteDataException
         
     def Stop(self):
-        self.running = 0
 
         self.Send(DisconnectEvent(self.channel, self.privateId))
+        self.running = 0
+
+        self.sock.close()
 
     def DefaultCallback(self, event):
         log.info("Got callback for %s event!", event.eventType)
--- AccessGrid/TextClient.py
+++ AccessGrid/TextClient.py
@@ -5,7 +5,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2003/01/02
-# RCS-ID:      $Id: TextClient.py,v 1.14 2003-05-16 19:29:37 lefvert Exp $
+# RCS-ID:      $Id: TextClient.py,v 1.15 2003-05-20 19:39:26 olson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -15,13 +15,17 @@ import pickle
 import logging
 import struct
 
+log = logging.getLogger("AG.TextClient")
+
 from threading import Thread
 
 from pyGlobus.io import GSITCPSocket, IOBaseException
+from pyGlobus.util import Buffer
+from pyGlobus import ioc
+
 from AccessGrid.hosting.pyGlobus.Utilities import GetDefaultIdentityDN
 
 from AccessGrid.Utilities import formatExceptionInfo
-from AccessGrid.hosting.pyGlobus.Utilities import CreateTCPAttrAlwaysAuth
 
 class TextClientConnectException(Exception):
     """
@@ -57,30 +61,34 @@ class TextClientBadDataException(Exception):
     pass
 
 class SimpleTextProcessor:
-    def __init__(self, socket, venueId, callback):
+    def __init__(self, socket, venueId, callback, useThread = 0):
         """ """
         self.socket = socket
         self.venueId = venueId
         self.textOutCallback = callback
         self.wfile = self.socket.makefile('wb', 0)
-        self.rfile = self.socket.makefile('rb', -1)
 
         self.log = logging.getLogger("AG.TextClient")
         self.log.setLevel(logging.DEBUG)
         self.log.info("--------- START TextClient")
 
-        self.outputThread = Thread(target = self.ProcessNetwork)
-        self.outputThread.start()
+        if useThread:
+
+            self.outputThread = Thread(target = self.ProcessNetwork)
+            self.outputThread.start()
+
+        else:
+            self.InitAsynchIO()
 
     def Stop(self):
         self.log.debug("Stop")
         self.running = 0
         self.outputThread = 0
-        
+
     def Input(self, event):
         """ """
         self.log.debug("EVENT --- Input")
-        self.log.debug("%s", event)
+        self.log.debug(str(event))
         
         # the exception should be caught in UI not here.
         try:
@@ -117,8 +125,81 @@ class SimpleTextProcessor:
         # Added sender to the string.
         self.textOutCallback(text)
 
+    def InitAsynchIO(self):
+        """
+        Initialize for using asynchronous IO for reading data from the network.
+        """
+
+        self.bufsize = 4096
+        self.buffer = Buffer(self.bufsize)
+        self.dataBuffer = ''
+        self.waitingLen = 0
+
+        h = self.socket.register_read(self.buffer, self.bufsize, 1, self.readCallbackWrap, None)
+
+        log.debug("register returns %s", h)
+        self.cbHandle = h
+
+    def readCallbackWrap(self, arg, handle, ret, buf, n):
+        """
+        Asynch read callback.
+
+        We just use this to wrap the call to the readCallback itself,
+        because the callback invocation mechanism silently ignores exceptions.
+        """
+
+        try:
+            return self.readCallback(arg, handle, ret, buf, n)
+        except Exception, e:
+            log.exception("readcallback failed")
+
+    def readCallback(self, arg, handle, ret, buf, n):
+
+        log.debug("Got read handle=%s ret=%s  n=%s \n", handle, ret, n)
+
+        if n == 0:
+            log.debug("TextClient: asynch read gets n=0, EOF")
+            self.running = 0
+            return
+
+        dstr = str(buf)
+        self.dataBuffer += dstr
+
+        if self.waitingLen == 0:
+
+            sz = struct.calcsize('i')
+            
+            if len(self.dataBuffer) < sz:
+                return
+
+            lenstr = self.dataBuffer[:sz]
+            dlen = struct.unpack('i', lenstr)
+            log.debug("Data len %s", dlen)
+
+            self.dataBuffer = self.dataBuffer[sz:]
+
+            self.waitingLen = dlen[0]
+
+        if len(self.dataBuffer) >= self.waitingLen:
+            log.debug("finally read enough data, wait=%s buflen=%s",
+                      self.waitingLen, len(self.dataBuffer))
+
+            thedata = self.dataBuffer[:self.waitingLen]
+            self.dataBuffer = self.dataBuffer[self.waitingLen:]
+
+            self.handleData(thedata)
+
+            self.waitingLen = 0
+            
+        h = self.socket.register_read(self.buffer, self.bufsize, 1,
+                                    self.readCallbackWrap, None)
+        log.debug("register returns %s", h)
+        self.cbHandle = h
+
     def ProcessNetwork(self):
         """ """
+
+        self.rfile = self.socket.makefile('rb', -1)
         self.log.debug("Process network")
         self.running = 1
         while self.running:
@@ -147,15 +228,20 @@ class SimpleTextProcessor:
                 self.log.exception("TextClient: Read data failed.")
                 raise TextClientReadDataException
 
-            # Unpickle the data
-            try:
-                event = pickle.loads(pdata)
-            except EOFError, e:
-                self.running = 0
-                log.exception("TextClient: unpickle got EOF.")
-                raise TextClientBadDataException
+            self.handleData(pdata)
 
-            # Handle the data
-            self.Output(event.data)
-            
         self.socket.close()
+
+    def handleData(self, pdata):
+
+        # Unpickle the data
+        try:
+            event = pickle.loads(pdata)
+        except EOFError, e:
+            self.running = 0
+            log.exception("TextClient: unpickle got EOF.")
+            raise TextClientBadDataException
+
+        # Handle the data
+        self.Output(event.data)
+
