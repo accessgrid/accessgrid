8fe462ee9940ff6fa67803925aed81a2899cb684
Eric Olson <eolson@mcs.anl.gov>
2003-08-04 17:33:48 +0000

    Trivial code to exercise CertificateRepository.CreateCertificateRequest

---
+++ AccessGrid/tests/CreateCertTest.py
@@ -0,0 +1,12 @@
+from AccessGrid import cr
+
+repo = cr.CertificateRepository('repo')
+
+name = [("O", "test"),
+        ("OU", "here"),
+        ("CN", "bob")]
+desc = repo.CreateCertificateRequest(name, "asdfasdf")
+print desc.ExportPEM()
+print "subj is ", desc.GetSubject()
+print "mod is ", desc.GetModulus()
+print "modhash is ", desc.GetModulusHash()
