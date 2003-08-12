bb6093359459d97f2653402ef77267a6da3b8b60
Eric Olson <eolson@mcs.anl.gov>
2003-08-12 20:56:41 +0000

    Proxy generation code.

---
+++ AccessGrid/hosting/pyGlobus/ProxyGenProgrammatic.py
@@ -0,0 +1,30 @@
+"""
+Globus proxy generation using the programmatic method. We can currently (circa AGTk
+2.1) do this on windows only.
+
+"""
+
+from pyGlobus import security
+
+def CreateGlobusProxy(passphrase, certFile, keyFile, certDir, outFile, bits, hours):
+    """
+    Create a globus proxy.
+
+    certFile - filename of user certificate
+    keyFile - filename of user key
+    certDir - directory containing trusted CA certificates
+    outFile - filename for generated proxy certificate
+    bits - keysize of generated proxy certificate
+    hours - lifetime (in hours) of generated proxy certificate
+    """
+
+    print "Invoking gpi, dest=", outFile
+    security.grid_proxy_init(verbose = 1,
+                              certDir = certDir,
+                              certFile = certFile,
+                              keyFile = keyFile,
+                              outFile = outFile,
+                              bits = bits,
+                              hours = hours,
+                              passphraseString = passphrase)
+    
--- AccessGrid/hosting/pyGlobus/__init__.py
+++ AccessGrid/hosting/pyGlobus/__init__.py
@@ -5,12 +5,25 @@
 # Author:      Robert D. Olson
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: __init__.py,v 1.4 2003-02-14 20:53:01 olson Exp $
+# RCS-ID:      $Id: __init__.py,v 1.5 2003-08-12 20:56:41 olson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 
 import pyGlobus.io
+import sys
 
 IOException = pyGlobus.io.IOBaseException
 
+if sys.platform == "win32":
+    import ProxyGenProgrammatic
+    ProxyGen = ProxyGenProgrammatic
+    del ProxyGenProgrammatic
+    
+else:
+    import ProxyGenGPI
+    ProxyGen = ProxyGenProgrammatic
+    del ProxyGenGPI
+
+
+    
