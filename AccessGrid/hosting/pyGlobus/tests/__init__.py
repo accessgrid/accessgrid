6be6b35b344ff5d0a0d616d527f5a760630a33f2
Ivan Judson <judson@mcs.anl.gov>
2003-08-16 10:29:23 +0000

    Adding __init__ files in test directory.

---
+++ AccessGrid/hosting/pyGlobus/tests/__init__.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        __init__.py
+# Purpose:     
+#
+# Author:      Everyone
+#
+# Created:     2003/08/02
+# RCS-ID:      $Id: __init__.py,v 1.1 2003-08-16 10:29:23 judson Exp $
+# Copyright:   (c) 2002
+# Licence:     See COPYING.txt
+#-----------------------------------------------------------------------------
+# Package initialisation
