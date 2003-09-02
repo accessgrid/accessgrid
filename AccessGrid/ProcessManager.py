6e5c6f55187a65cfcce998358cd6ab6811461a6e
Eric Olson <eolson@mcs.anl.gov>
2003-09-02 16:45:55 +0000

    Fix typo for unix

--- AccessGrid/ProcessManager.py
+++ AccessGrid/ProcessManager.py
@@ -5,7 +5,7 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: ProcessManager.py,v 1.1 2003-08-28 20:36:13 judson Exp $
+# RCS-ID:      $Id: ProcessManager.py,v 1.2 2003-09-02 16:45:55 eolson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -15,4 +15,4 @@ from AccessGrid.Platform import isWindows, isLinux, isOSX
 if isWindows():
     from AccessGrid.ProcessManagerWin32 import ProcessManagerWin32 as ProcessManager
 elif isLinux():
-    from AccessGrid.ProcessManagerPipes import ProcessManagerPipes as ProcessManager
+    from AccessGrid.ProcessManagerUnix import ProcessManagerUnix as ProcessManager
