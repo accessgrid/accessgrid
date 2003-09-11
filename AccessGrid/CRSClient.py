64b6689258ddf7307bb60dee320195ef0be65459
Eric Olson <eolson@mcs.anl.gov>
2003-09-11 21:09:43 +0000

    HTTP proxy support.

--- AccessGrid/CRSClient.py
+++ AccessGrid/CRSClient.py
@@ -6,11 +6,8 @@
 """
 Sample code using a proxy.
 
-xmlrpc_urllib_transport is available at
-http://starship.python.net/crew/jjkunce/python/xmlrpc_urllib_transport.py
 
 from xmlrpc_urllib_transport import UrllibTransport
-import xmlrpclib
 import os
 os.environ['HTTP_PROXY']= 'http://localhost:3128'
 
@@ -20,7 +17,7 @@ proxy = xmlrpclib.ServerProxy(url, transport = transport, verbose=1)
 
 """
 
-from xmlrpclib import ServerProxy
+import xmlrpclib
 
 import logging
 
@@ -29,13 +26,27 @@ log = logging.getLogger("AG.CRSClient")
 class CRSClientInvalidURL(Exception):
     pass
 
+class CRSClientConnectionFailed(Exception):
+    pass
+
 class CRSClient:
-    def __init__(self, url):
+    def __init__(self, url, proxyHost = None, proxyPort = None):
         self.url = url
         log.debug('create client')
 
         if self.url is not None:
-            self.proxy = ServerProxy(url, verbose = 0)
+
+            if proxyHost is not None:
+                if proxyPort is None:
+                    proxyURL = "http://%s" % (proxyHost)
+                else:
+                    proxyURL = "http://%s:%s" % (proxyHost, proxyPort)
+
+                transport = UrllibTransport(proxyURL)
+            else:
+                transport = None
+                
+            self.proxy = xmlrpclib.ServerProxy(url, transport = transport, verbose = 1)
         else:
             raise CRSClientInvalidURL
 
@@ -53,6 +64,9 @@ class CRSClient:
         log.debug("request certificate")
         try:
             token = self.proxy.RequestCertificate(emailAddress, certReq)
+        except IOError, e:
+            log.exception("IOError on Proxy.RequestCertificate")
+            raise CRSClientConnectionFailed
         except StandardError, v:
             log.exception("Exception on xmlrpc RequestCertificate call")
             raise v
@@ -76,6 +90,9 @@ class CRSClient:
 
         try:
             certificate = self.proxy.RetrieveCertificate(token)
+        except IOError, e:
+            log.exception("IOError on Proxy.RetrieveCertificate")
+            raise CRSClientConnectionFailed
         except StandardError, v:
             log.exception("error on proxy.RetrieveCertificate(%s)", token)
             raise v
@@ -83,9 +100,35 @@ class CRSClient:
         log.debug("retrieved certificate %s", certificate)
         return certificate
 
+#
+# The following code is from:
+# http://starship.python.net/crew/jjkunce/python/xmlrpc_urllib_transport.py
+#
+class UrllibTransport(xmlrpclib.Transport):
+	'''Handles an HTTP transaction to an XML-RPC server via urllib
+	(urllib includes proxy-server support)
+	jjk  07/02/99'''
+
+        def __init__(self, proxy):
+            self.proxy = proxy
+
+	def request(self, host, handler, request_body, verbose = None):
+		'''issue XML-RPC request
+		jjk  07/02/99'''
+		import urllib
+		self.verbose = verbose
+                urlopener = urllib.URLopener(proxies = {"http": self.proxy})
+		urlopener.addheaders = [('User-agent', self.user_agent)]
+		# probably should use appropriate 'join' methods instead of 'http://'+host+handler
+		f = urlopener.open('http://'+host+handler, request_body)
+		return(self.parse_response(f))
+
+
 if __name__ == "__main__":
     req = """-----BEGIN CERTIFICATE REQUEST-----\nMIIB0TCCAToCADBgMRQwEgYDVQQKEwtBY2Nlc3MgR3JpZDEdMBsGA1UECxMUYWdk\nZXYtY2EubWNzLmFubC5nb3YxEjAQBgNVBAsTCW15LmRvbWFpbjEVMBMGA1UEAxMM\nUm9iZXJ0IE9sc29uMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC5lvEXiR5R\na6QOJJpRpOXz4R0DxnTVBOvTKvPijSmcc6IbNnNimS7oE/4U+IJtQblOGGdqRwLX\nHOmVY3nDQ60yhQ34ynME3Sr3ntAp6zFp5Ek7LgjOWyEP3hIVWh0Paa36FHn6nCvm\nDYz/1/Ns9c17zK/fWy+PYKMz8Vz0cs2O1wIDAQABoDIwMAYJKoZIhvcNAQkOMSMw\nITARBglghkgBhvhCAQEEBAMCBPAwDAYDVR0TAQH/BAIwADANBgkqhkiG9w0BAQQF\nAAOBgQB5HQyPLAR7XaD6S3Rsso/Q9cbPSxHeWJE4ehF5Ohp+0yAuBpc3L7/LlDkX\nvHri5JGXrGzJGf0/cqzzduh0S/ejMGksNiupsSPlHzkVhQNtAvD6A9OT+25wMyHI\nzSidh+6OJkSBLVb2tkEK5wd844MLE+m0lgTKbNX2C9UAZmfkKw==\n-----END CERTIFICATE REQUEST-----\n"""
     email = "olson+catext@mcs.anl.gov"
+
+    token = "412d8f4d766c6290bd3acc7e74763b72"
 
     import os
 
@@ -96,6 +139,10 @@ if __name__ == "__main__":
 
     submitServerURL = "http://www-unix.mcs.anl.gov/~judson/certReqServer.cgi"
     print "Sending..."
-    certificateClient = CRSClient(submitServerURL)
-    requestId = certificateClient.RequestCertificate(email, req)
-    print "Sent, got id ", requestId
+    #certificateClient = CRSClient(submitServerURL)
+    certificateClient = CRSClient(submitServerURL, "yips.mcs.anl.gov", 3128)
+    #requestId = certificateClient.RequestCertificate(email, req)
+    #print "Sent, got id ", requestId
+
+    ret = certificateClient.RetrieveCertificate(token)
+    print "got ret ", ret
