43f654e198a7b50a40c46150baca47861a0df50b
Tom Uram <turam@mcs.anl.gov>
2003-09-29 19:29:17 +0000

    Write RTP defaults to registry with _and_ without a leading *, since vic reads them with and rat reads them without.  This should be fixed so that both tools read the same registry keys.

--- AccessGrid/Platform.py
+++ AccessGrid/Platform.py
@@ -5,14 +5,14 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2003/09/02
-# RCS-ID:      $Id: Platform.py,v 1.51 2003-09-16 19:32:40 turam Exp $
+# RCS-ID:      $Id: Platform.py,v 1.52 2003-09-29 19:29:17 turam Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 The Platform Module is to isolate OS specific interfaces.
 """
-__revision__ = "$Id: Platform.py,v 1.51 2003-09-16 19:32:40 turam Exp $"
+__revision__ = "$Id: Platform.py,v 1.52 2003-09-29 19:29:17 turam Exp $"
 __docformat__ = "restructuredtext en"
 
 import os
@@ -588,11 +588,20 @@ def SetRtpDefaultsWin( profile ):
                         r"Software\Mbone Applications\common",
                         0,
                         _winreg.KEY_SET_VALUE)
+
+    # Vic reads these values (with '*')
     _winreg.SetValueEx(k, "*rtpName", 0, _winreg.REG_SZ, profile.name)
     _winreg.SetValueEx(k, "*rtpEmail", 0, _winreg.REG_SZ, profile.email)
     _winreg.SetValueEx(k, "*rtpPhone", 0, _winreg.REG_SZ, profile.phoneNumber)
     _winreg.SetValueEx(k, "*rtpLoc", 0, _winreg.REG_SZ, profile.location)
     _winreg.SetValueEx(k, "*rtpNote", 0, _winreg.REG_SZ, str(profile.publicId) )
+
+    # Rat reads these (without '*')
+    _winreg.SetValueEx(k, "rtpName", 0, _winreg.REG_SZ, profile.name)
+    _winreg.SetValueEx(k, "rtpEmail", 0, _winreg.REG_SZ, profile.email)
+    _winreg.SetValueEx(k, "rtpPhone", 0, _winreg.REG_SZ, profile.phoneNumber)
+    _winreg.SetValueEx(k, "rtpLoc", 0, _winreg.REG_SZ, profile.location)
+    _winreg.SetValueEx(k, "rtpNote", 0, _winreg.REG_SZ, str(profile.publicId) )
 
     _winreg.CloseKey(k)
 
