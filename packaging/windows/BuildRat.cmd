d62a6b09ef59c14a8b89da2fe8d919e800ba4134
Ivan Judson <judson@mcs.anl.gov>
2003-10-30 19:37:25 +0000

    Added stuff to explicitly build rat-kill, it wasn't here so I don't know how we were building it before (gulp)

--- packaging/windows/BuildRat.cmd
+++ packaging/windows/BuildRat.cmd
@@ -7,11 +7,14 @@ call %VSCOMNTOOLS%\vsvars32.bat
 @set RATDIR=%1
 @set DESTDIR=%2
 
-if not exist %RATDIR%\rat\Release\rat.exe goto do_compile
-if not exist %RATDIR%\rat\Release\ratmedia.exe goto do_compile
-if not exist %RATDIR%\rat\Release\ratui.exe goto do_compile
+if not exist %RATDIR%\rat\Release\rat.exe goto do_compile_rat
+if not exist %RATDIR%\rat\Release\ratmedia.exe goto do_compile_rat
+if not exist %RATDIR%\rat\Release\ratui.exe goto do_compile_rat
+if not exist %RATDIR%\rat\Release\rat-kill.exe goto do_compile_rat_kill
 
 :do_copy
+
+if not exist %DESTDIR% mkdir %DESTDIR%
 
 copy %RATDIR%\rat\Release\rat-kill.exe %DESTDIR%\rat-kill.exe
 copy %RATDIR%\rat\Release\rat.exe %DESTDIR%\rat.exe
@@ -21,10 +24,16 @@ copy %RATDIR%\rat\Release\ratui.exe %DESTDIR%\ratui.exe
 
 goto end
 
-:do_compile
+:do_compile_rat
 
 echo Building ag-rat
 devenv %RATDIR%\rat\rat.sln /rebuild Release
+
+goto do_copy
+
+:do_compile_rat_kill
+echo Building Rat Kill
+devenv %RATDIR%\rat\rat.sln /rebuild Release /project rat-kill
 
 goto do_copy
 
