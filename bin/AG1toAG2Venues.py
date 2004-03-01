1b8b03aab3aa03fab69f98b94ac6129e34f2444f
Tom Uram <turam@mcs.anl.gov>
2004-03-01 20:54:32 +0000

    Update to include server-specific details

--- bin/AG1toAG2Venues.py
+++ bin/AG1toAG2Venues.py
@@ -1,11 +1,14 @@
-#!/usr/bin/python2
+#!/usr/bin/python1.5
 #-----------------------------------------------------------------------------
 # Name:        AG1toAG2Venues.py
 # Purpose:     This connects to the db for AG1 gets the venues,
 #              then spits out a INI formatted file of the information.
+# 
+#              Note:  This script only works on the machine where the
+#                     ag1 venues db lives (currently ag.mcs.anl.gov)
 # Author:      Ivan R. Judson
 # Created:     2002/12/12
-# RCS-ID:      $Id: AG1toAG2Venues.py,v 1.1 2003-05-16 04:49:44 judson Exp $
+# RCS-ID:      $Id: AG1toAG2Venues.py,v 1.2 2004-03-01 20:54:32 turam Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -16,7 +19,7 @@ import os
 import ConfigParser
 import string
 
-import pgsqldb
+import pg
 
 def load_pass_1(db_conn, id_map):
     #
@@ -81,9 +84,9 @@ if __name__ == "__main__":
     """
     
     venue_id_map = {}
-    
-    db_conn = pgsqldb.connect(sys.argv[1])
-    
+
+    db_conn = pg.connect(dbname='ag_venues', host='pgsql.mcs.anl.gov',
+                         user='wwwtrans', passwd='bigvoyage')
     load_pass_1(db_conn, venue_id_map)
     load_pass_2(db_conn, venue_id_map)
     
