f9f51b4db6965032cce583e1c0c28094e399baa3
Eric Olson <eolson@mcs.anl.gov>
2004-03-17 23:32:36 +0000

    Copy linux platform files to get updates.

--- AccessGrid/Platform/darwin/ProcessManager.py
+++ AccessGrid/Platform/darwin/ProcessManager.py
@@ -1,17 +1,14 @@
 #-----------------------------------------------------------------------------
 # Name:        ProcessManager.py
 # Purpose:     
-#
-# Author:      Robert D. Olson
-#
 # Created:     2003/08/02
-# RCS-ID:      $Id: ProcessManager.py,v 1.2 2004-03-10 23:17:08 eolson Exp $
-# Copyright:   (c) 2002-2004
+# RCS-ID:      $Id: ProcessManager.py,v 1.3 2004-03-17 23:32:36 eolson Exp $
+# Copyright:   (c) 2002-2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: ProcessManager.py,v 1.2 2004-03-10 23:17:08 eolson Exp $"
+__revision__ = "$Id: ProcessManager.py,v 1.3 2004-03-17 23:32:36 eolson Exp $"
 __docformat__ = "restructuredtext en"
 
 import signal
@@ -22,44 +19,48 @@ from AccessGrid import Log
 log = Log.GetLogger(Log.ProcessManager)
 
 class ProcessManager:
-
     def __init__(self):
         self.processes = []
 
-    def start_process(self, command, arglist):
+    def StartProcess(self, command, arglist, detached = 1):
         """
         Start a new process.
         Command is the name of the command to be started. It can either be
         a full pathname or a command name to be found on the default path.
         Arglist is a list of the arguments to the command.
         """
-
-        arglist.insert(0, command)
-
         arglist = map(lambda a: str(a), arglist)
-        pid = os.spawnvp(os.P_NOWAIT, command, arglist)
+        arglist.insert(0, str(command))
+
+        if detached:
+            pid = os.spawnvp(os.P_NOWAIT, command, arglist)
+        else:
+            pid = os.spawnvp(os.P_WAIT, command, arglist)
 
         self.processes.append(pid)
 
         return pid
 
-    def terminate_all_processes(self):
-        for pid in self.processes:
+    def TerminateAllProcesses(self):
+        """
+        Cleanly shutdown all processes this manager has created.
+        """
+        # Use a copy of the process list
+        processList = self.processes[:]
+        
+        for pid in processList:
             try:
-                self._terminate_process(pid)   
+                self.TerminateProcess(pid)   
             except OSError, e:
                 log.debug( "couldn't terminate process: %s", e )
+                
+    def TerminateProcess(self, pid):
+        """
+        Cleanly shutdown the specified process this manager has created.
 
-        self.processes = []
-
-    def terminate_process(self, pid):
-        try:
-            self._terminate_process(pid)    
-            self.processes.remove(pid)
-        except OSError, e:
-            log.debug ( "couldn't terminate process: %s", e )
-    
-    def _terminate_process(self, pid):
+        @param pid: the id of the process to terminate.
+        @type pid: string? integer?
+        """
         os.kill(pid, signal.SIGINT)
         elapsedWaits = 0
         maxWaits = 5
@@ -68,7 +69,6 @@ class ProcessManager:
         try:
             while elapsedWaits < maxWaits:
                 (retpid,status) = os.waitpid(pid, os.WNOHANG )
-                #print "waitpid returns ", retpid, status
                 if retpid == pid and os.WIFEXITED(status):
                     break
                 time.sleep(waitTime)
@@ -81,48 +81,86 @@ class ProcessManager:
                 rc = os.WEXITSTATUS(status)
             elif os.WIFSIGNALED(status):
                 sig = os.WTERMSIG(status)
+            self.processes.remove(pid)
         else:
-            self._kill_process(pid)
+            self.KillProcess(pid)
 
-    def kill_all_processes(self):
-        for pid in self.processes:
+    def KillAllProcesses(self):
+        """
+        Kill all processes this manager has created.
+
+        @warning: this is not a clean shutdown, but a forced shutdown
+        that may result in system cruft.
+        """
+        processList = self.processes[:]
+        
+        for pid in processList:
             try:
-                self._kill_process(pid)   
+                self.KillProcess(pid)   
             except OSError, e:
                 log.debug ("couldn't kill process: %s", e)
 
-        self.processes = []
+    def KillProcess(self, pid):
+        """
+        Kill a single process this manager has created.
 
-    def _kill_process(self, pid):
-       os.kill(pid,signal.SIGKILL)
-       maxWaits = 5
-       waitTime = 1
-       elapsedWaits = 0
-       retpid = 0
-       try:
-           while elapsedWaits < maxWaits:
-               (retpid,status) = os.waitpid(pid, os.WNOHANG )
-               if retpid == pid and os.WIFSIGNALED(status):
-                   break
-               time.sleep(waitTime)
-               elapsedWaits += 1
-       except OSError, e:
-           log.debug ( "_kill_process, waitpid %i : %s", pid, e )
-       if retpid == pid:
-           if os.WIFEXITED(status):
-               rc = os.WEXITSTATUS(status)
-           elif os.WIFSIGNALED(status):
-               sig = os.WTERMSIG(status)
-       else:
-           log.debug("_kill_process, process %i not killed or waitpid() failed.", pid)
+        @warning: this is not a clean shutdown, but a forced shutdown
+        that may result in system cruft.
+        
+        @param pid: the id of the process to terminate.
+        @type pid: string? integer?
+        """
+        os.kill(pid,signal.SIGKILL)
+        maxWaits = 5
+        waitTime = 1
+        elapsedWaits = 0
+        retpid = 0
+        try:
+            while elapsedWaits < maxWaits:
+                (retpid,status) = os.waitpid(pid, os.WNOHANG )
+                if retpid == pid and os.WIFSIGNALED(status):
+                    break
+                time.sleep(waitTime)
+                elapsedWaits += 1
+        except OSError, e:
+            log.debug ( "_kill_process, waitpid %i : %s", pid, e )
 
+        if retpid == pid:
+            if os.WIFEXITED(status):
+                rc = os.WEXITSTATUS(status)
+            elif os.WIFSIGNALED(status):
+                sig = os.WTERMSIG(status)
+        else:
+            log.debug("Process %i not killed or waitpid() failed.", pid)
 
+        self.processes.remove(pid)
+
+    def ListProcesses(self):
+        """
+        Return a list of process id's for this process manager.
+        @returns: a list of process id's
+        """
+        return self.processes
+    
 if __name__ == "__main__":
+    import time
     mgr = ProcessManager()
-    mgr.start_process("date",[])
 
-    time.sleep(3)
+    try:
+        mgr.StartProcess("date", [])
+    except Exception, e:
+        print "Exception starting process: ", e
 
-    mgr.terminate_all_processes()
+    try:
+        print mgr.ListProcesses()
+    except Exception, e:
+        print "Exception listing processes: ", e
+
+    time.sleep(5)
+
+    try:
+        mgr.TerminateAllProcesses()
+    except Exception, e:
+        print "Exception terminating processes: ", e
 
     
