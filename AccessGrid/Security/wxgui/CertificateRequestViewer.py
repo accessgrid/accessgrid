bb80cb60bf5ab8fabb8746edf857577d759f8f55
Eric Olson <eolson@mcs.anl.gov>
2004-03-19 22:21:22 +0000

    New classes for cert request browsing.

---
+++ AccessGrid/Security/wxgui/CertificateRequestViewer.py
@@ -0,0 +1,134 @@
+from wxPython.wx import *
+
+import time
+import os
+
+from AccessGrid.Security.Utilities import GetCNFromX509Subject
+
+class CertificateRequestViewerPanel(wxPanel):
+    """
+    Panel subclass that incorporates the majority of the
+    certificate request viewer code.
+
+    """
+    
+    def __init__(self, parent, id, request, certMgr):
+        """
+        Constructor.
+
+        @param parent: parent widget
+        @param id: widget id
+        @param request: request to view
+        @param certMgr: our certificate manager
+        @type parent: wx widget
+        @type id: number
+        @type request: CertRequestWrapper (from CertificateStatusBrowser module)
+        @type certMgr: CertificateManager instance
+
+        """
+        
+        wxPanel.__init__(self, parent, id, style = 0)
+
+        self.reqWrapper = request
+        self.request, self.token, self.server, self.creationTime = request.GetRequestInfo()
+        self.certMgr = certMgr
+
+        self.__build()
+
+        #
+        # Set up text styles.
+        #
+
+        boldFont = wxFont(-1, wxDEFAULT, wxNORMAL, wxBOLD)
+        self.bold = bold = wxTextAttr(font = boldFont)
+
+        normalFont = wxFont(-1, wxDEFAULT, wxNORMAL, wxNORMAL)
+        self.normal = normal = wxTextAttr(font = normalFont)
+
+        defaultPoints = self.text.GetFont().GetPointSize()
+        hdrFont = wxFont(defaultPoints + 4, wxDEFAULT, wxNORMAL, wxBOLD)
+        self.hdr = hdr = wxTextAttr(font = hdrFont)
+
+        name = GetCNFromX509Subject(self.request.GetSubject())
+
+        #
+        # Write cert info into text.
+        #
+        self.text.SetDefaultStyle(hdr)
+        self.text.AppendText(name + "\n\n")
+        
+        self._writeValue("Subject", str(self.request.GetSubject()))
+        self.text.AppendText("\n")
+
+        self._writeValue("Request token", self.token)
+        self._writeValue("Server URL", self.server)
+        self._writeValue("Creation time", time.strftime("%X %x", time.localtime(int(self.creationTime))))
+        self.text.AppendText("\n")
+
+        self._writeValue("Status from last update", self.reqWrapper.GetStatus())
+        if self.reqWrapper.GetCertReady():
+            self._writeValue("Certificate ready", "Yes")
+            self._writeValue("Certificate", "")
+            self.text.AppendText(self.reqWrapper.GetCert())
+        else:
+            self._writeValue("Certificate ready", "No")
+            self._writeValue("Full status message", self.reqWrapper.GetFullStatus())
+
+        #self._writeValue("Certificate location", cert.GetPath())
+        #pkloc =  cert.GetKeyPath()
+        #if pkloc and os.access(pkloc, os.R_OK):
+        #    self._writeValue("Private key location", pkloc)
+        
+    def _writeValue(self, tag, value):
+        """
+        Write a tag/value pair into the text widget.
+        Make tag bold, value normal.
+        """
+
+        self.text.SetDefaultStyle(self.bold)
+        self.text.AppendText(str(tag) + ": ")
+        self.text.SetDefaultStyle(self.normal)
+        self.text.AppendText(str(value) + "\n")
+
+    def __build(self):
+        """
+        Construct the GUI.
+
+        """
+
+        sizer = wxBoxSizer(wxHORIZONTAL)
+
+        panel = self
+        
+        self.text = wxTextCtrl(panel, -1, style = wxTE_READONLY | wxTE_MULTILINE | wxTE_LINEWRAP | wxTE_RICH)
+        sizer.Add(self.text, 1, wxEXPAND)
+
+        panel.SetSizer(sizer)
+        panel.SetAutoLayout(1)
+        panel.Fit()
+
+
+class CertificateRequestViewer(wxDialog):
+    def __init__(self, parent, id, req, certMgr):
+
+        wxDialog.__init__(self, None, id, "", size = wxSize(700, 400),
+                          style= wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
+
+        title = req.GetShortName()
+        self.SetTitle(title)
+
+        sizer = wxBoxSizer(wxVERTICAL)
+        
+        self.panel = CertificateRequestViewerPanel(self, -1, req, certMgr)
+        sizer.Add(self.panel, 1, wxEXPAND)
+
+        b = wxButton(self, -1, "Close")
+        sizer.Add(b, 0)
+        EVT_BUTTON(self, b.GetId(), self.OnClose)
+        EVT_CLOSE(self, self.OnClose)
+
+        self.SetSizer(sizer)
+        self.SetAutoLayout(1)
+
+    def OnClose(self, event):
+        self.Destroy()
---
+++ AccessGrid/Security/wxgui/CertificateStatusBrowser.py
@@ -0,0 +1,309 @@
+import time
+
+from AccessGrid import Log
+log = Log.GetLogger("CertificateStatus")
+
+from wxPython.wx import *
+
+from AccessGrid.Security.Utilities import GetCNFromX509Subject
+
+from CertificateRequestViewer import CertificateRequestViewer
+from CertificateBrowserBase import CertificateBrowserBase
+
+from HTTPProxyConfigPanel import HTTPProxyConfigPanel
+
+import ImportExportUtils
+
+class CertRequestWrapper:
+    """
+    Small wrapper class to hold the information about a certificate request
+    so we can cache information, and make updating the information easier.
+    """
+
+    def __init__(self, browser, reqInfo):
+        """
+        Constructor.
+
+        @param browser: the certificate browser this request is being viewed in
+        @param reqInfo: information about this certificate request, as returned
+        by CertificateManager.GetPendingRequests().
+        @type browser: CertificateStatusBrowser instance
+        @type reqInfo: tuple containing (requestObj, token, server, creationTime)
+        """
+
+        self.request, self.token, self.server, self.creationTime = reqInfo
+        self.status = "Unknown"
+        self.fullStatus = "Unknown"
+        self.certReady = 0
+        self.browser = browser
+        self.cert = ""
+        
+    def GetCert(self):
+        return self.cert
+
+    def GetShortName(self):
+        return GetCNFromX509Subject(self.request.GetSubject())        
+
+    def GetStatus(self):
+        return self.status
+
+    def GetRequest(self):
+        return self.request
+
+    def GetFullStatus(self):
+        return self.fullStatus
+
+    def GetCertReady(self):
+        return self.certReady
+
+    def UpdateStatus(self):
+        """
+        Update the status of this request by querying the request server.
+
+        @return: new status
+        """
+
+        if self.server is None or self.token is None:
+            self.status = "Invalid"
+            return self.status
+
+        proxyEnabled, proxyHost, proxyPort = self.browser.GetProxyInfo()
+
+        certMgr = self.browser.GetCertificateManager()
+        if proxyEnabled:
+            certReturn = certMgr.CheckRequestedCertificate(self.request, self.token, self.server,
+                                                           proxyHost, proxyPort)
+        else:
+            certReturn = certMgr.CheckRequestedCertificate(self.request, self.token, self.server)
+
+        success, msg = certReturn
+        if not success:
+
+            #
+            # Map nonobvious errors
+            #
+
+            if msg.startswith("Couldn't open certificate file."):
+                self.status = "Not ready"
+            elif msg.startswith("Couldn't read from certificate file."):
+                self.status = "Not ready"
+            elif msg.startswith("There is no certificate for this token."):
+                self.status = "Request not found"
+            else:
+                self.status = msg
+
+        else:
+            self.status = "Ready for installation"
+            self.cert = msg
+
+        self.certReady = success
+        self.fullStatus = msg
+
+        return self.status
+        
+    def GetRequestInfo(self):
+        """
+        Return the tuple form of this request description.
+        """
+        
+        return (self.request, self.token, self.server, self.creationTime)
+
+    def Format(self):
+        """
+        Format the certificate request for display in the browser.
+
+        """
+        
+        typeMeta = self.request.GetMetadata("AG.CertificateManager.requestType")
+
+        if typeMeta is None or typeMeta == "":
+            type = "Identity"
+        else:
+            type = typeMeta[0].upper() + typeMeta[1:]
+            
+        # subject = str(requestDescriptor.GetSubject())
+        
+        subject = GetCNFromX509Subject(self.request.GetSubject())
+        if self.creationTime is not None:
+            date = time.strftime("%x %X", time.localtime(int(self.creationTime)))
+        else:
+            date = ""
+
+        return [type, subject, date, self.status]        
+    
+
+class CertificateStatusBrowser(CertificateBrowserBase):
+    def __init__(self, parent, id, certMgr):
+        self.updateStatus = 0
+
+        CertificateBrowserBase.__init__(self, parent, id, certMgr)
+
+
+    def _buildButtons(self, sizer):
+
+        #
+        # Buttons that are only valid when a cert is selected.
+        #
+        self.certOnlyButtons = []
+
+        b = wxButton(self, -1, "Install Certificate")
+        EVT_BUTTON(self, b.GetId(), self.OnInstallCert)
+        sizer.Add(b, 0, wxEXPAND | wxALL, 3)
+        self.certOnlyButtons.append(b)
+
+        b = wxButton(self, -1, "Delete Request")
+        EVT_BUTTON(self, b.GetId(), self.OnDeleteRequest)
+        sizer.Add(b, 0, wxEXPAND | wxALL, 3)
+        self.certOnlyButtons.append(b)
+
+        b = wxButton(self, -1, "View request")
+        EVT_BUTTON(self, b.GetId(), self.OnViewRequest)
+        sizer.Add(b, 0, wxEXPAND | wxALL, 3)
+        self.certOnlyButtons.append(b)
+
+        b = wxButton(self, -1, "Check status")
+        EVT_BUTTON(self, b.GetId(), self.OnCheckStatus)
+        sizer.Add(b, 0, wxEXPAND | wxALL, 3)
+
+        for b in self.certOnlyButtons:
+            b.Enable(0)
+
+    def _buildExtra(self, panel, sizer):
+        """
+        Put a http proxy panel in the window.
+        """
+
+        self.httpProxyPanel = HTTPProxyConfigPanel(panel)
+        sizer.Add(self.httpProxyPanel, 0, wxEXPAND)
+
+    def OnInstallCert(self, event):
+        cert = self.GetSelectedCertificate()
+        if cert is None:
+            return
+
+        print "Install ", cert
+
+    def OnDeleteRequest(self, event):
+        req = self.GetSelectedCertificate()
+
+        dlg = wxMessageDialog(self,
+                              "Deleting a certificate request is an irreversible operation.\n" +
+                              "If you delete this request, you will not be able to retreive the certificate.\n" +
+                              "Really delete certificate request for identity " +
+                              req.GetShortName() + "?",
+                              "Really delete?",
+                              style = wxYES_NO | wxNO_DEFAULT)
+        ret = dlg.ShowModal()
+        dlg.Destroy()
+
+        if ret == wxID_NO:
+            return
+
+        log.debug("Removing request %s", req.GetRequest().GetSubject())
+        self.certMgr.GetCertificateRepository().RemoveCertificateRequest(req.GetRequest())
+
+        #
+        # Turn off update so the load goes quickly.
+        #
+        self.updateStatus = 0
+        self.Load()
+
+    def OnViewRequest(self, event):
+
+        req = self.GetSelectedCertificate()
+        if req is None:
+            return
+
+        dlg = CertificateRequestViewer(self, -1, req, self.certMgr)
+        dlg.Show()
+
+    def OnCheckStatus(self, event):
+        """
+        Start a reload after setting the updateStatus flag; we don't
+        do an update when we first load the browser because it can be
+        an expensive operation.
+        """
+        
+        self.updateStatus = 1
+
+        self.Load()
+        
+    def OnCertActivated(self, event, req):
+        if req is None:
+            return
+
+        dlg = CertificateRequestViewer(self, -1, req, self.certMgr)
+        dlg.Show()
+
+    def GetProxyInfo(self):
+        return self.httpProxyPanel.GetInfo()
+
+    def GetCertificateManager(self):
+        return self.certMgr
+
+    #
+    # Overrides.
+    #
+
+    def OnCertSelected(self, event, cert):
+        if cert is None:
+            return
+
+        for b in self.certOnlyButtons:
+            b.Enable(1)
+
+    def OnCertDeselected(self, event, cert):
+        for b in self.certOnlyButtons:
+            b.Enable(0)
+
+    def _LoadCerts(self):
+
+        #
+        # Set up a progress dialog since this might take some time.
+        #
+
+        reqList = self.certMgr.GetPendingRequests()
+
+        if self.updateStatus:
+            progressDlg = wxProgressDialog("Checking request status",
+                                                "Checking request status",
+                                                len(reqList) * 2,
+                                                self,
+                                                style = wxPD_CAN_ABORT | wxPD_AUTO_HIDE | wxPD_APP_MODAL)
+        l = []
+        n = 1
+        for r in reqList:
+            w = CertRequestWrapper(self, r)
+            if self.updateStatus:
+                ok = progressDlg.Update(n, "Checking %s..." % (w.GetShortName()))
+                n += 1
+
+                if not ok:
+                    progressDlg.Destroy()
+                    self.updateStatus = 0
+                else:
+                    w.UpdateStatus()
+
+                ok = progressDlg.Update(n)
+                if not ok:
+                    progressDlg.Destroy()
+                    self.updateStatus = 0
+                n += 1                
+            l.append(w)
+
+        if self.updateStatus:
+            progressDlg.Destroy()
+
+        return l
+    
+    def _FormatCert(self, cert):
+
+        print "Format ", cert
+        return cert, cert.Format()
+
+    def _getListColumns(self):
+        return ["Certificate Type", "Subject Name", "Date Requested", "Status"]
+
+    def _getListColumnWidths(self):
+        return [wxLIST_AUTOSIZE_USEHEADER, wxLIST_AUTOSIZE, wxLIST_AUTOSIZE, wxLIST_AUTOSIZE]
+
