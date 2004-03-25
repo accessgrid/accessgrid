5552cc7757f2f751d60aa53397604cfa35d74053
Tom Uram <turam@mcs.anl.gov>
2004-03-25 20:00:00 +0000

    unix module init

---
+++ AccessGrid/Platform/unix/__init__.py
@@ -0,0 +1,16 @@
+#-----------------------------------------------------------------------------
+# Name:        __init__.py
+# Purpose:     
+#
+# Created:     2004/03/25
+# RCS-ID:      $Id: __init__.py,v 1.1 2004-03-25 20:00:00 turam Exp $
+# Copyright:   (c) 2004
+# Licence:     See COPYING.txt
+#-----------------------------------------------------------------------------
+"""
+unix platform interface
+"""
+__revision__ = "$Id: __init__.py,v 1.1 2004-03-25 20:00:00 turam Exp $"
+__docformat__ = "restructuredtext en"
+
+    
