e89dec01403ec1b7f0ae0b9f63942373fbf6a561
Susanne Lefvert <lefvert@mcs.anl.gov>
2004-03-29 22:55:55 +0000

    added import getpass

--- AccessGrid/Platform/unix/Config.py
+++ AccessGrid/Platform/unix/Config.py
@@ -3,18 +3,19 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.2 2004-03-29 19:08:42 olson Exp $
+# RCS-ID:      $Id: Config.py,v 1.3 2004-03-29 22:55:55 lefvert Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.2 2004-03-29 19:08:42 olson Exp $"
+__revision__ = "$Id: Config.py,v 1.3 2004-03-29 22:55:55 lefvert Exp $"
 
 import os
 import mimetypes
 import mailcap
 import socket
+import getpass
 
 from pyGlobus import utilc
 
