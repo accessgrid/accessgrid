618bd7267aca75e826e91d8910bc0c7ae0d8ff34
Tom Uram <turam@mcs.anl.gov>
2004-03-30 16:57:24 +0000

    Make portMax configurable, and add method for requesting a port within a range possibly different from that for which the allocator is configured

--- AccessGrid/NetworkAddressAllocator.py
+++ AccessGrid/NetworkAddressAllocator.py
@@ -5,19 +5,22 @@
 # Author:      Robert Olson, Ivan R. Judson
 #
 # Created:     2002/12/12
-# RCS-ID:      $Id: NetworkAddressAllocator.py,v 1.1 2004-02-26 14:41:55 judson Exp $
+# RCS-ID:      $Id: NetworkAddressAllocator.py,v 1.2 2004-03-30 16:57:24 turam Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: NetworkAddressAllocator.py,v 1.1 2004-02-26 14:41:55 judson Exp $"
+__revision__ = "$Id: NetworkAddressAllocator.py,v 1.2 2004-03-30 16:57:24 turam Exp $"
 __docformat__ = "restructuredtext en"
 
 import sys
 import socket
 import struct
 from random import Random
+
+class NoFreePortsError(Exception):
+    pass
 
 class NetworkAddressAllocator:
     """
@@ -27,18 +30,19 @@ class NetworkAddressAllocator:
     """
     RANDOM = "random"
     INTERVAL = "interval"
-    PORT_MAX = 65535
-    def __init__(self, portBase = 49152):
+    
+    def __init__(self, portBase = 49152, portMax=65535):
         """
         We only need to initialize port information.
         """
         self.portBase = portBase
+        self.portMax = portMax
         
         self.allocatedPorts = []
         self.random = Random()
 
         self.allocationMethod = NetworkAddressAllocator.RANDOM
-
+        
     def SetAllocationMethod( self, method ):
         self.allocationMethod = method
         return self.allocationMethod
@@ -46,18 +50,35 @@ class NetworkAddressAllocator:
     def GetAllocationMethod( self ):
         return self.allocationMethod
 
+    def SetPortBase(self, portBase):
+        self.portBase = portBase
+
+    def SetPortMax(self, portMax):
+        self.portMax = portMax
+
     def AllocatePort(self, even = 0):
         """
         if the even flag is set we allocate only an even port to support
         RTP standard use cases.
         """
+        
+        # Check to see whether any ports are available
+        numPorts = 1 + (self.portMax - self.portBase)/2
+        if numPorts == len(self.allocatedPorts):
+            raise NoFreePortsError
+            
+        port = self.AllocatePortInRange(even,self.portBase,self.portMax)
+        return port
+        
+    def AllocatePortInRange(self, even, portBase, portMax):
+    
         # Check to see if port is used
         if even:
             modulus = 2
         else:
             modulus = 1
         while 1:
-            port = self.random.randrange(self.portBase, self.PORT_MAX, modulus)
+            port = self.random.randrange(portBase, portMax, modulus)
             if port in self.allocatedPorts:
                 continue
             
@@ -70,7 +91,7 @@ class NetworkAddressAllocator:
                 continue
             
         self.allocatedPorts.append(port)
-        return port
+    
         
     def RecyclePort(self, port):
         self.allocatedPorts.remove(port)
