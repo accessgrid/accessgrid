c2d90223427f71a5e6da305809af263e3e5ea733
Ivan Judson <judson@mcs.anl.gov>
2004-04-05 18:30:57 +0000

    Renamed is? methods to Is?, put temporary maps in place to keep things working

--- AccessGrid/Platform/__init__.py
+++ AccessGrid/Platform/__init__.py
@@ -5,14 +5,14 @@
 # Author:      Ivan R. Judson
 #
 # Created:     2003/08/02
-# RCS-ID:      $Id: __init__.py,v 1.6 2004-03-26 04:30:11 judson Exp $
+# RCS-ID:      $Id: __init__.py,v 1.7 2004-04-05 18:30:57 judson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 Platform sub modules.
 """
-__revision__ = "$Id: __init__.py,v 1.6 2004-03-26 04:30:11 judson Exp $"
+__revision__ = "$Id: __init__.py,v 1.7 2004-04-05 18:30:57 judson Exp $"
 __docformat__ = "restructuredtext en"
 
 # mechanisms to support multiple hosting environments and to set defaults
@@ -28,31 +28,35 @@ WIN = 'win32'
 LINUX = 'linux2'
 OSX = 'darwin'
 
-def isWindows():
+def IsWindows():
     """Function that retusn 1 if the platform is windows, 0 otherwise """
     if sys.platform == WIN:
         return 1
     else:
         return 0
 
-def isLinux():
+def IsLinux():
     """Function that retusn 1 if the platform is linux, 0 otherwise """
     if sys.platform == LINUX:
         return 1
     else:
         return 0
 
-def isOSX():
+def IsOSX():
     """Function that retusn 1 if the platform is os x, 0 otherwise """
     if sys.platform == OSX:
         return 1
     else:
         return 0
 
-if isWindows():
+isWindows = IsWindows
+isLinux = IsLinux
+isOSX = IsOSX
+
+if IsWindows():
     from AccessGrid.Platform.win32 import Config as Config
     from AccessGrid.Platform.win32 import ProcessManager as ProcessManager
-elif isLinux() or isOSX():
+elif IsLinux() or IsOSX():
     from AccessGrid.Platform.unix import Config as Config
     from AccessGrid.Platform.unix import ProcessManager as ProcessManager
 else:
