b541aa02d7893ed970bb70c8b6d78ab89cfee98e
Ivan Judson <judson@mcs.anl.gov>
2004-04-07 17:52:09 +0000

    Made directory stuff clearer.

--- packaging/windows/BuildPythonModules.cmd
+++ packaging/windows/BuildPythonModules.cmd
@@ -18,42 +18,48 @@ if %PYVER%=="2.2" (
     python setup.py clean --all
     python setup.py build
     python setup.py install --prefix=%DEST% --no-compile
+    cd %SOURCE% 
 
     echo "Building Optik 1.4.1"
 
-    cd ..\Optik-1.4.1
+    cd Optik-1.4.1
     python setup.py clean --all
     python setup.py build
     python setup.py install --prefix=%DEST% --no-compile
+    cd %SOURCE% 
 )
 
 echo "Building fpconst 0.6.0"
 
-cd ..\fpconst-0.6.0
+cd fpconst-0.6.0
 python setup.py clean --all
 python setup.py build
 python setup.py install --prefix=%DEST% --no-compile
+cd %SOURCE% 
 
 echo "Building SOAPpy"
 
-cd ..\SOAPpy
+cd SOAPpy
 python setup.py clean
 python setup.py build
 python setup.py install --prefix=%DEST% --no-compile
+cd %SOURCE% 
 
 echo "Building pyOpenSSL_AG"
 
-cd ..\pyOpenSSL
+cd pyOpenSSL
 python setup.py clean --all
 python setup.py build
 python setup.py install --prefix=%DEST%  --no-compile
+cd %SOURCE% 
 
 echo "Building pyGLobus"
 
-cd ..\pyGlobus
+cd pyGlobus
 python setup.py clean --all
 set GLOBUS_LOCATION=%SOURCE%\WinGlobus
 python setup.py build --flavor=win32
 python setup.py install --flavor=win32 --prefix=%DEST%
+cd %SOURCE% 
 
 @cd %AGDIR%\packaging\windows
