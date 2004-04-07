3b42ac074cc92fbbea5ca909b5ff3c10e0d8b1d1
Ivan Judson <judson@mcs.anl.gov>
2004-04-07 18:15:16 +0000

    Adding globus build script, can be used on all platforms.

---
+++ packaging/build_globus.py
@@ -0,0 +1,29 @@
+import sys, os
+
+def BuildLinux():
+    print "Nothing to be done"
+
+def BuildDarwin():
+    print "Nothing to be done"
+    
+def BuildWindows():
+    wdir = "WinProjects/SourceSolution"
+    os.system("devenv %s/DebugLib/All_Libs.sln /rebuild Debug" % wdir)
+    os.system("devenv %s/DebugTreadedLib/All_Libs.sln /build Debug" % wdir)
+    os.system("devenv %s/DebugLib/All_Libs.sln /project gss_assist /rebuild Debug" % wdir)
+    os.system("devenv %s/DebugLib/All_Libs.sln /project gssapi /rebuild Debug" % wdir)
+
+os.chdir(os.path.abspath(os.path.join(os.environ['AGBUILDROOT'],
+                                      "WinGlobus")))
+
+if sys.platform == 'linux2':
+    BuildLinux()
+elif sys.platform == 'win32':
+    BuildWindows()
+elif sys.platform == 'darwin':
+    BuildDarwin()
+else:
+    print "Can't build for you!"
+
+
+
