66ae5fbb8e0dc9c7cdec8ddd6029dc080265393e
Tom Uram <turam@mcs.anl.gov>
2004-04-16 21:18:11 +0000

    Script to build install package, including rpms (not yet finished)

---
+++ packaging/linux/rpm/build_package.py
@@ -0,0 +1,196 @@
+#!/usr/bin/python2
+
+import sys, os, time
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", dest="sourcedir", metavar="SOURCEDIR",
+                  default=None,
+                  help="The source directory for the AGTk build.")
+parser.add_option("-b", dest="builddir", metavar="BUILDDIR",
+                  default=None,
+                  help="The working directory the AGTk build.")
+parser.add_option("-d", dest="destdir", metavar="DESTDIR",
+                  default=None,
+                  help="The destination directory of the AGTk build.")
+parser.add_option("-m", dest="metainfo", metavar="METAINFO",
+                  default=None,
+                  help="Meta information string about this release.")
+parser.add_option("-v", dest="version", metavar="VERSION",
+                  default=None,
+                  help="Version of the toolkit being built.")
+parser.add_option("--verbose", action="store_true", dest="verbose",
+                  default=0,
+                  help="A flag that indicates to build verbosely.")
+parser.add_option("-p", "--pythonversion", dest="pyver",
+                  metavar="PYTHONVERSION", default="2.2",
+                  help="Which version of python to build the installer for.")
+
+options, args = parser.parse_args()
+
+SourceDir = options.sourcedir
+BuildDir = options.builddir
+DestDir = options.destdir
+metainfo = options.metainfo
+version = options.version
+
+
+print "In rpm/build_package.py"
+print "SourceDir = ", SourceDir
+print "BuildDir = ", BuildDir
+print "DestDir = ", DestDir
+print "metainfo = ", metainfo
+print "version = ", version
+
+"""
+cmd =  /usr/bin/python2 build_package.py --verbose 
+    -s /home/turam/build 
+    -b /home/turam/build/AccessGrid-20040413-140420 
+    -d /home/turam/build/dist-20040413-174909 
+    -p 2.2 -m Snapshot_20040413-174909 
+    -v 2.2
+"""
+
+CurDir = os.getcwd()
+
+#
+# Build rpms for infrastructure packages
+#
+for pkg in [ "logging-0.4.7", "Optik-1.4.1", "fpconst-0.6.0", "SOAPpy"]:
+    os.chdir(SourceDir)
+    os.chdir(pkg)
+    cmd = "%s setup.py bdist_rpm --binary-only --dist-dir %s" % (sys.executable,
+                                                   DestDir)
+    print "cmd = ", cmd
+    os.system(cmd)
+
+# Build pyOpenSSL 
+# (must build with our spec file)
+# - create tar file for the rpmbuild
+os.chdir(SourceDir)
+cmd = "ln -s pyOpenSSL pyOpenSSL_AG-0.5.1"
+print "cmd = ", cmd
+os.system(cmd)
+cmd = "tar czhf /usr/src/redhat/SOURCES/pyOpenSSL_AG-0.5.1.tar.gz pyOpenSSL_AG-0.5.1"
+print "cmd = ", cmd
+os.system(cmd)
+
+# - build the rpm
+os.chdir("pyOpenSSL")
+cmd = "rpmbuild -bb pyOpenSSL.spec"
+print "cmd = ", cmd
+os.system(cmd)
+
+# - copy the rpm to the dest dir
+cmd = "cp /usr/src/redhat/RPMSW/i386/pyOpenSSL_AG-0.5.1-4.386.rpm %s" % (DestDir,)
+print "cmd = ", cmd
+os.system(cmd)
+
+#
+# Copy the gpt rpm
+#
+cmd = "cp %s %s" % ( os.path.join(SourceDir,"gpt-3.1-1-rh73.i386.rpm"), DestDir)
+print "cmd = ", cmd
+os.system(cmd)
+
+
+#
+# Build globus rpm
+#
+os.chdir(CurDir)
+print "cwd = ", os.getcwd()
+cmd = "rpm -ba globus.spec"
+print "cmd = ", cmd
+os.system(cmd)
+cmd = "cp /usr/src/redhat/RPMS/i386/globus-accessgrid-2.4-1.i386.rpm %s" % (DestDir,)
+print "cmd = ", cmd
+os.system(cmd)
+
+#
+# Build the pyGlobus rpm
+#
+# - create tar file for the rpm
+os.chdir(SourceDir)
+cmd = "ln -s pyGlobus pyGlobus-cvs"
+print "cmd = ", cmd
+os.system(cmd)
+cmd = "tar cvzhf /usr/src/redhat/SOURCES/pyGlobus-cvs.tar.gz"
+print "cmd = ", cmd
+os.system(cmd)
+
+# - build the rpm
+os.chdir(CurDir)
+cmd = "rpmbuild -ba pyGlobus.spec"
+print "cmd = ", cmd
+os.system(cmd)
+
+# - copy it to the dest dir
+cmd = "cp /usr/src/redhat/RPMS/i386/pyGlobus-cvs-11.i386.rpm %s" % (DestDir,)
+print "cmd = ", cmd
+os.system(cmd)
+
+#
+# Build AccessGrid rpms
+#
+# - build the targz file for the AG rpms
+os.chdir(SourceDir)
+tar_dst_filename = "AccessGrid-%s.tar.gz" % (version)
+tar_src_filename = "AccessGrid-%s" % (version)
+
+if not os.path.exists(tar_src_filename):
+    cmd = "ln -s AccessGrid AccessGrid-%s" % (version,)
+    print "cmd = ", cmd
+    os.system(cmd)
+
+rpm_srcdir = "/usr/src/redhat/SOURCES"
+tar_command = "tar czhf %s %s" % ( os.path.join(rpm_srcdir, tar_dst_filename), tar_src_filename)
+print "tar_command = ", tar_command
+r = os.system(tar_command)
+if r != 0:
+    print "tar command \"", tar_command, "\" failed with rc ", rc
+    sys.exit(1)
+    
+# - build the rpms
+os.chdir(BuildDir)
+os.chdir(os.path.join("packaging","linux","rpm"))
+cmd = "rpmbuild -bb AccessGrid.spec" 
+print "cmd = ", cmd
+os.system(cmd)
+
+# - copy the rpms to the dist dir
+cmd = "cp /usr/src/redhat/RPMS/i386/AccessGrid-%s-1.i386.rpm %s" % (version,DestDir)
+print "cmd = ", cmd
+os.system(cmd)
+cmd = "cp /usr/src/redhat/RPMS/i386/AccessGrid-VenueClient-%s-1.i386.rpm %s" % (version,DestDir)
+print "cmd = ", cmd
+os.system(cmd)
+cmd = "cp /usr/src/redhat/RPMS/i386/AccessGrid-VenueServer-%s-1.i386.rpm %s" % (version,DestDir)
+print "cmd = ", cmd
+os.system(cmd)
+cmd = "cp /usr/src/redhat/RPMS/i386/AccessGrid-BridgeServer-%s-1.i386.rpm %s" % (version,DestDir)
+print "cmd = ", cmd
+os.system(cmd)
+
+#
+# Copy the install.sh script into the dist dir
+#
+cmd = "cp %s %s" % (os.path.join(BuildDir,"packaging","linux","rpm","install.sh"),
+                    DestDir)
+print "cmd = ", cmd
+os.system(cmd)
+
+#
+# Create the targz file including the rpms and install script
+#
+targz=os.path.join(SourceDir,"AccessGrid-2.2.tar.gz")
+os.chdir(DestDir)
+cmd = "tar czf %s *.rpm install.sh" % (targz,)
+print "cmd = ", cmd
+os.system(cmd)
+
+
+
+
+
+
+
