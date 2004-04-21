2068802bb536c78ca0b2e416ac86c4a64cc14e6c
Ivan Judson <judson@mcs.anl.gov>
2004-04-21 18:50:41 +0000

    Put in a check for vs, only executes with vs.net (not 2003 or vc6).

--- packaging/BuildGlobus.py
+++ packaging/BuildGlobus.py
@@ -5,13 +5,36 @@ def BuildLinux():
 
 def BuildDarwin():
     print "Nothing to be done"
-    
+
+def vs_version():
+    try:
+        cmd = "cl"
+        po = os.popen4(cmd)
+    except IOError:
+        print "Error getting AGTk Version."
+        
+    version = po[1].read()
+    po[0].close()
+    po[1].close()
+        
+    v = version.split('\n')[0].split('Version')[1].strip().split(' ')[0]
+    major, minor, patch = map(int, v.split('.'))
+
+    if major != 13 or (major == 13 and minor > 0):
+        print "Please use MS Visual Studio v 7.0 to build globus."
+        sys.exit(-1)
+        
 def BuildWindows():
-    wdir = "WinProjects/SourceSolution"
-    os.system("devenv %s/DebugLib/All_Libs.sln /rebuild Debug" % wdir)
-    os.system("devenv %s/DebugThreadedLib/All_Libs.sln /build Debug" % wdir)
-    os.system("devenv %s/DebugLib/All_Libs.sln /project gss_assist /rebuild Debug" % wdir)
-    os.system("devenv %s/DebugLib/All_Libs.sln /project gssapi /rebuild Debug" % wdir)
+    vs_version()
+    
+    ntsln = "WinProjects/SourceSolution/DebugLib/All_Libs.sln"
+    tsln  = "WinProjects/SourceSolution/DebugThreadedLib/All_Libs.sln"
+    # I think what really needs to happen here is something like:
+    # build everything non-threaded
+    # then build common, io threaded
+    os.system("devenv %s /rebuild Debug" % ntsln)
+    os.system("devenv %s /project common /rebuild Debug" % tsln)
+    os.system("devenv %s /project io /rebuild Debug" % tsln)
 
 os.chdir(os.path.abspath(os.path.join(os.environ['AGBUILDROOT'],
                                       "WinGlobus")))
--- packaging/BuildOpenSSL.py
+++ packaging/BuildOpenSSL.py
@@ -5,12 +5,28 @@ def BuildLinux():
     os.system("make")
     os.system("make test")
 
-def BuildWindows():
-    os.system("perl Configure VC-WIN32")
+def vs_version():
+    try:
+        cmd = "cl"
+        po = os.popen4(cmd)
+    except IOError:
+        print "Error getting AGTk Version."
+        
+    version = po[1].read()
+    po[0].close()
+    po[1].close()
+        
+    v = version.split('\n')[0].split('Version')[1].strip().split(' ')[0]
+    major, minor, patch = map(int, v.split('.'))
 
-    #os.system("ms/do_masm.bat")
-    #os.system("nmake -f ms/ntdll.mak")
-    #os.system("nmake -f ms/nt.mak")
+    if major != 13 or (major == 13 and minor > 0):
+        print "Please use MS Visual Studio v 7.0 to build globus."
+        sys.exit(-1)
+        
+def BuildWindows():
+    vs_version()
+    
+    os.system("perl Configure VC-WIN32")
 
     masmPath = os.path.join(os.getcwd(), 'ms', 'do_masm.bat')
     os.system(masmPath)
