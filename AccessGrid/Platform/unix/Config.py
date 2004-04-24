15de8d1def1c89937bf670b77bcfac90385ca7c7
Tom Uram <turam@mcs.anl.gov>
2004-04-24 20:24:36 +0000

    Move Config dir under the toolkit base path (/etc/AccessGrid), instead of the install dir (/usr)

--- AccessGrid/Platform/unix/Config.py
+++ AccessGrid/Platform/unix/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.14 2004-04-23 15:28:54 olson Exp $
+# RCS-ID:      $Id: Config.py,v 1.15 2004-04-24 20:24:36 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.14 2004-04-23 15:28:54 olson Exp $"
+__revision__ = "$Id: Config.py,v 1.15 2004-04-24 20:24:36 turam Exp $"
 
 import os
 import mimetypes
@@ -121,10 +121,12 @@ class AGTkConfig(AccessGrid.Config.AGTkConfig):
         return self.installBase
         
     def GetConfigDir(self):
-        if self.installBase == None:
-            self.GetBaseDir()
 
-        self.configDir = os.path.join(self.installBase, "Config")
+        try:
+            basePath = os.environ[AGTK_LOCATION]
+            self.configDir = os.path.join(basePath, "Config")
+        except:
+            self.configDir = os.path.join(AGTkBasePath, "Config")
 
         # Check dir and make it if needed.
         if self.initIfNeeded:
