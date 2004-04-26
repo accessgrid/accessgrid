7d66d7ad0c0d5497ed09769aed05f4d3edbcd1fa
Tom Uram <turam@mcs.anl.gov>
2004-04-26 18:15:30 +0000

    Fix reference to AGTkBasePath

--- AccessGrid/Platform/unix/Config.py
+++ AccessGrid/Platform/unix/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.15 2004-04-24 20:24:36 turam Exp $
+# RCS-ID:      $Id: Config.py,v 1.16 2004-04-26 18:15:30 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.15 2004-04-24 20:24:36 turam Exp $"
+__revision__ = "$Id: Config.py,v 1.16 2004-04-26 18:15:30 turam Exp $"
 
 import os
 import mimetypes
@@ -126,7 +126,7 @@ class AGTkConfig(AccessGrid.Config.AGTkConfig):
             basePath = os.environ[AGTK_LOCATION]
             self.configDir = os.path.join(basePath, "Config")
         except:
-            self.configDir = os.path.join(AGTkBasePath, "Config")
+            self.configDir = os.path.join(self.AGTkBasePath, "Config")
 
         # Check dir and make it if needed.
         if self.initIfNeeded:
