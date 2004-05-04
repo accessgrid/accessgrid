498f4b3d876a8fef3540a049c83e9a10605337a2
Ivan Judson <judson@mcs.anl.gov>
2004-05-04 18:13:22 +0000

    Working for windows.

--- tools/SetupVideoWin32.py
+++ tools/SetupVideoWin32.py
@@ -1,41 +1,56 @@
+import sys
 import _winreg
 import win32api
+
+out = file(sys.argv[1], "w")
 
 try:
     key = "SYSTEM\\ControlSet001\\Control\\MediaResources\\msvideo"
     videoKey = _winreg.OpenKey(_winreg.HKEY_LOCAL_MACHINE, key)
 except Exception, e:
     print "Got an exception doing OpenKey: ", e
-    
+    out.close()
+    sys.exit(-1)
+
 try:
     (nSubKeys, nValues, lastModified) = _winreg.QueryInfoKey(videoKey)
-    print "NK: %d NV: %d LM: %d" % (nSubKeys, nValues, lastModified)
 except Exception, e:
     print "Got an exception doing QueryInfoKey: ", e
+    out.close()
+    sys.exit(-1)
 
 try:
     sVal = _winreg.EnumKey(videoKey, 0)
     sKey = _winreg.OpenKey(videoKey, sVal)
-    print "Subkey: ", sKey
 except Exception, e:
     print "Got an exception retrieving: %s %d" % (videoKey, 0)
+    out.close()
+    sys.exit(-1)
 
 try:
     (nSubKeys, nValues, lastModified) = _winreg.QueryInfoKey(sKey)
-    print "NK: %d NV: %d LM: %d" % (nSubKeys, nValues, lastModified)
 except Exception, e:
     print "Got an exception retrieving: %s %d" % (videoKey, 0)
+    out.close()
+    sys.exit(-1)
 
 try:
     for i in range(0, nSubKeys):
         kName = _winreg.EnumKey(sKey, i)
-        print "SubKey: %s" % kName
 except Exception, e:
     print "Got an exception retrieving: %s %d" % (sKey, i)
+    out.close()
+    sys.exit(-1)
 
 try:
     for i in range(0, nValues):
         (vName, vData, vType) = _winreg.EnumValue(sKey, i)
-        print "NV: %s VD: %s VT: %s" % (vName, vData, vType)
+        if vName == "FriendlyName":
+            out.write("device: %s\n" % vData)
+            out.write("portnames: external-in\n")
 except Exception, e:
     print "Got an exception retrieving: %s %d" % (sKey, i)
+    out.close()
+    sys.exit(-1)
+
+out.close()
