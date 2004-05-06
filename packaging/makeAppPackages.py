ae1a1a14643cd618a2d778f0f46b8a0e4a33f6ea
Tom Uram <turam@mcs.anl.gov>
2004-05-06 21:45:40 +0000

    Remove CVS from list of shared app dirs to process

--- packaging/makeAppPackages.py
+++ packaging/makeAppPackages.py
@@ -34,7 +34,12 @@ if not os.path.isdir(inputDir):
 else:
     things = [d for d in os.listdir(inputDir) if os.path.isdir(os.path.join(inputDir, d))]
     print "THINGS: ", things
-    
+
+# Remove CVS from the list if exists
+if 'CVS' in things:
+    things.remove('CVS')
+
+
 for thing in things:
     Desc = None
     adir = os.path.join(inputDir, thing)
