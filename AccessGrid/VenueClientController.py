e5d61e1a119794cdfdb0e5b4846f71c60e504e28
Eric Olson <eolson@mcs.anl.gov>
2004-05-27 21:13:33 +0000

    Fixed bug adding personal data file that would overwrite an existing personal data file. RemoveData was expecting a single file instead of a list.

--- AccessGrid/VenueClientController.py
+++ AccessGrid/VenueClientController.py
@@ -2,12 +2,12 @@
 # Name:        VenueClientController.py
 # Purpose:     This is the controller module for the venue client
 # Created:     2004/02/20
-# RCS-ID:      $Id: VenueClientController.py,v 1.27 2004-05-26 14:44:18 lefvert Exp $
+# RCS-ID:      $Id: VenueClientController.py,v 1.28 2004-05-27 21:13:33 eolson Exp $
 # Copyright:   (c) 2002-2004
 # Licence:     See COPYING.TXT
 #---------------------------------------------------------------------------
 
-__revision__ = "$Id: VenueClientController.py,v 1.27 2004-05-26 14:44:18 lefvert Exp $"
+__revision__ = "$Id: VenueClientController.py,v 1.28 2004-05-27 21:13:33 eolson Exp $"
 __docformat__ = "restructuredtext en"
 
 # standard imports
@@ -1023,7 +1023,7 @@ class VenueClientController:
                     info = "A file named %s is already added, do you want to overwrite?" % name
                     # Overwrite?
                     if self.gui.Prompt( info, title ):
-                        self.__venueClient.RemoveData([data])
+                        self.__venueClient.RemoveData(data)
                     else:
                         return
                                          
