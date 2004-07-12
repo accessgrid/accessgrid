f7a665a40c9e3b96e54a674730b4096f51849600
Ivan Judson <judson@mcs.anl.gov>
2004-07-12 15:32:41 +0000

    Added SystemConfig.PerformanceReport to gather a performance snapshot for debugging, it's currently empy.

--- AccessGrid/Platform/unix/Config.py
+++ AccessGrid/Platform/unix/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.41 2004-06-24 20:19:27 eolson Exp $
+# RCS-ID:      $Id: Config.py,v 1.42 2004-07-12 15:32:41 judson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.41 2004-06-24 20:19:27 eolson Exp $"
+__revision__ = "$Id: Config.py,v 1.42 2004-07-12 15:32:41 judson Exp $"
 
 import os
 import mimetypes
@@ -681,6 +681,15 @@ class SystemConfig(AccessGrid.Config.SystemConfig):
             resourceList.append(r)
         
         return resourceList
+
+    def PerformanceSnapshot(self):
+        """
+        This method grabs a snapshot of relevent system information to report
+        it. This helps track the effect of the AG Toolkit on the system.
+        """
+        perfData = dict()
+        
+        return perfData
 
 class MimeConfig(AccessGrid.Config.MimeConfig):
     """
