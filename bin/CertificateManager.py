c75ad8b8406b45e43ddc6e6666cc804a5414e9c9
Ivan Judson <judson@mcs.anl.gov>
2004-07-18 02:03:03 +0000

    Added gui version of certificate manager, need to check symmetric functionality.

---
+++ bin/CertificateManager.py
@@ -0,0 +1,30 @@
+import os, sys
+from optparse import Option
+
+from wxPython.wx import *
+
+from AccessGrid.Toolkit import WXGUIApplication
+from AccessGrid.Security.wxgui import CertificateManagerDialog
+
+def main():
+    pyapp = wxPySimpleApp()
+
+    app = WXGUIApplication()
+
+    try:
+        app.Initialize("CertificateManager")
+    except Exception, e:
+        print "Exception initializing toolkit:", e
+        print "Exiting."
+        sys.exit(-1)
+
+    d = CertificateManagerDialog.CertificateManagerDialog(None, -1,
+                                                 "Certificate Manager",
+                                                 app.GetCertificateManager()) 
+    d.Show(1)
+    
+    pyapp.SetTopWindow(d)
+    pyapp.MainLoop()
+    
+if __name__ == "__main__":
+    main()
