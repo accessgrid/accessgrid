d9b47ac253c0e4e6a25343754c208e350f406a34
Ivan Judson <judson@mcs.anl.gov>
2004-08-27 19:18:39 +0000

    Added the other dll, d'oh

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # Name:        setup.py
 # Purpose:     This is the setup.py for the Access Grid python module.
 # Created:     2003/17/01
-# RCS-ID:      $Id: setup.py,v 1.72 2004-06-02 03:14:31 turam Exp $
+# RCS-ID:      $Id: setup.py,v 1.73 2004-08-27 19:18:39 judson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -38,7 +38,8 @@ win32_data = [
     ('doc/VenueManagementManual/VenueManagementManual_files',
      glob.glob('doc/VenueManagementManual/VenueManagementManual_files/*.*')),
     ('install', [r'packaging/windows/agicons.exe',
-                 r'packaging/windows/msvcr70.dll']),
+                 r'packaging/windows/msvcr70.dll',
+                 r'packaging/windows/msvcr71.dll']),
     ('config/CAcertificates',
      glob.glob('packaging/config/CAcertificates/*.*')),
     ]
