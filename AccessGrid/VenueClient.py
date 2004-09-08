de9cc94d4b6cd98b0c079cf8c7253fe301882391
Tom Uram <turam@mcs.anl.gov>
2004-09-08 18:57:43 +0000

    pychecker fix

--- AccessGrid/VenueClient.py
+++ AccessGrid/VenueClient.py
@@ -2,14 +2,14 @@
 # Name:        VenueClient.py
 # Purpose:     This is the client side object of the Virtual Venues Services.
 # Created:     2002/12/12
-# RCS-ID:      $Id: VenueClient.py,v 1.191 2004-09-07 21:55:29 judson Exp $
+# RCS-ID:      $Id: VenueClient.py,v 1.192 2004-09-08 18:57:43 turam Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
 """
 """
-__revision__ = "$Id: VenueClient.py,v 1.191 2004-09-07 21:55:29 judson Exp $"
+__revision__ = "$Id: VenueClient.py,v 1.192 2004-09-08 18:57:43 turam Exp $"
 __docformat__ = "restructuredtext en"
 
 from AccessGrid.hosting import Client
@@ -1476,7 +1476,7 @@ class VenueClientI(SOAPInterface):
     def EnterVenue(self, url):
         return self.impl.EnterVenue(url)
         
-    def ExitVenue():
+    def ExitVenue(self):
         self.impl.ExitVenue()
 
     def GetDataStoreInformation(self):
