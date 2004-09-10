825568a8d0216803c80f9c5320623926b25617df
Ivan Judson <judson@mcs.anl.gov>
2004-09-10 18:59:03 +0000

    Cleaned up; we have an archvie in cvs.

--- bin/CertificateRequestTool.py
+++ bin/CertificateRequestTool.py
@@ -3,27 +3,29 @@
 # Name:        CertificateRequestTool.py
 # Purpose:     Starts the CertificateRequestTool
 # Created:     2003/08/12
-# RCS_ID:      $Id: CertificateRequestTool.py,v 1.7 2004-09-10 18:52:08 judson Exp $
+# RCS_ID:      $Id: CertificateRequestTool.py,v 1.8 2004-09-10 18:59:03 judson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 This is the tool used to get certificates.
 """
-__revision__ = "$Id: CertificateRequestTool.py,v 1.7 2004-09-10 18:52:08 judson Exp $"
+__revision__ = "$Id: CertificateRequestTool.py,v 1.8 2004-09-10 18:59:03 judson Exp $"
 
 from AccessGrid.Toolkit import WXGUIApplication
+from wxPython.wx import wxPySimpleApp
 
-if __name__ == "__main__":
-    app = WXGUIApplication()
+pp = wxPySimpleApp()
+app = WXGUIApplication()
 
-    # This will not work if we don't have any certificates
-    try:
-        args = app.Initialize('CertificateRequestTool')
-    except:
-        # Doesn't matter if we have certificates or proxies
-        # we just want to request a new certificate
-        pass
+# This will not work if we don't have any certificates
+try:
+    args = app.Initialize('CertificateRequestTool')
+except:
+    # Doesn't matter if we have certificates or proxies
+    # we just want to request a new certificate
+    print "Failed to init toolkit, continuing..."
+    pass
 
-    certMgrUI = app.GetCertMgrUI()
-    certMgrUI.HandleNoCertificateInteraction()
+certMgrUI = app.GetCertMgrUI()
+certMgrUI.HandleNoCertificateInteraction()
