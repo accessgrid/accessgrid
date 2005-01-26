794f83e25a742bcf70802b5de3cefb4a4963bb1e
Ivan Judson <judson@mcs.anl.gov>
2005-01-26 22:46:25 +0000

    Testing client for ZSI server work.

---
+++ AccessGrid/test_venueserver_client.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+from AccessGrid.cache.VenueServer_services import *
+from AccessGrid.cache.AccessGrid_types import www_accessgrid_org_v3_0 as AGTk_types
+
+
+if __name__ == '__main__':
+    loc = VenueServerLocator()
+    VenueServer = loc.getVenueServer(
+        portAddress="http://localhost:7000/VenueServer",
+        tracefile=sys.stdout)
+
+    # Go through each interface and see what we can do
+    sr = CheckpointRequest()
+    sr.secondsFromNow = 10
+    msg = VenueServer.Checkpoint(sr)
+    print "Checkpoint result: ", msg.result
+
+    avr = AddVenueRequest()
+    vd = AGTk_types.VenueDescription()
+    vd.id = ""
+    vd.uri = ""
+    vd.name = "Test"
+    vd.description = "This is a description."
+    vd.encryptMedia = 0
+    vd.encryptionKey = ""
+    vd.connections = AGTk_types.ConnectionList()
+    vd.streams = AGTk_types.StreamList()
+
+    avr.venueDescription = vd
+    
+    msg = VenueServer.AddVenue(avr)
+
+    print dir(msg)
