b132e53e544d898174e17d039ee8449d2d71476d
Ivan Judson <judson@mcs.anl.gov>
2005-01-31 19:31:40 +0000

    with the latest ZSI from cvs this will create the python from the wsdl. (windows only atm)

---
+++ AccessGrid/wsdl/generate.bat
@@ -0,0 +1,4 @@
+wsdl2py -f VenueServerBinding.wsdl -e -d ..\cache -t AccessGrid_Types
+wsdl2dispatch -f VenueServerBinding.wsdl -e -d ..\cache -t AccessGrid_Types
+wsdl2dispatch -f VenueBinding.wsdl -e -d ..\cache -t AccessGrid_Types
+wsdl2py -f VenueBinding.wsdl -e -d ..\cache -t AccessGrid_Types
