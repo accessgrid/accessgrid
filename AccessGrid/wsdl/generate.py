72cc7874869e529834ae5369d08ef86b61da6014
Eric Olson <eolson@mcs.anl.gov>
2005-03-07 21:01:38 +0000

    Create cache/__init__.py so cache can be imported as a module.

--- AccessGrid/wsdl/generate.py
+++ AccessGrid/wsdl/generate.py
@@ -6,6 +6,10 @@ dstPath = os.path.join("..", "cache")
 if not os.path.exists(dstPath):
     os.mkdir(dstPath)
 
+initFile = os.path.join(dstPath, "__init__.py")
+if not os.path.exists(initFile):
+    open(initFile, "w").close()
+
 command = "wsdl2py.py -f VenueServerBinding.wsdl -e -d %s -t AccessGrid_Types" % dstPath
 os.system(command)
 command = "wsdl2dispatch.py -f VenueServerBinding.wsdl -e -d %s -t AccessGrid_Types" % dstPath
