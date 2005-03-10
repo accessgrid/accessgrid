b0c9678a92de572a2a2ed23db00e7aec4fe89937
Susanne Lefvert <lefvert@mcs.anl.gov>
2005-03-10 23:55:24 +0000

    Updated for 2.3 node services programmers manual

--- services/node/DebugService/DebugService.py
+++ services/node/DebugService/DebugService.py
@@ -7,119 +7,78 @@
 # Copyright:   (c) 2003-2004
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
-import sys
-import os
 
-from AccessGrid.hosting import SecureServer as Server
-from AccessGrid.hosting.SOAPInterface import SOAPInterface, SOAPIWrapper
-from AccessGrid.Platform.Config import SystemConfig
+import os
+import sys
+
 from AccessGrid.GUID import GUID
-from AccessGrid.AGService import AGService, AGServiceI, AGServiceIW, RunService
+from AccessGrid.AGService import AGService, AGServiceI, RunService
 from AccessGrid.Types import Capability
+from AccessGrid.AGParameter import ValueParameter, OptionSetParameter
+from AccessGrid.AGParameter import RangeParameter, TextParameter
 
 class DebugService(AGService):
    """
-   Simple service that prints out configuration information. It claims to be
+   Simple service that prints configuration information. It claims to be
    a video and audio producer and consumer just to get information.
    """
+   
    def __init__(self):
       AGService.__init__(self)
-      
+      self.log.debug("DebugService.__init__: Init Service")
       self.id = str(GUID())
 
-      # a fictional debug level
-      self.level = 0
-      
+      # Set capabilities
       self.capabilities = [ Capability( Capability.CONSUMER, Capability.AUDIO ),
                             Capability( Capability.CONSUMER, Capability.VIDEO ),
                             Capability( Capability.PRODUCER, "debug" ) ]
-
-   def SetLevel(self, level):
-      self.level = level
-
-   def GetLevel(self):
-      return self.level
-
-   def Start(self):
-      pass
-
-   def Stop(self):
-      pass
-
-   def ConfigureStream(self, streamDescription):
-      if self.level > 0:
-         print streamDescription
-
-   def SetIdentity(self, profile):
-      if self.level > 0:
-         print profile
-         
-class DebugServiceI(AGServiceI):
-   """
-   Interface wrapper for the Debug Service implementation, so you can for
-   example turn the verbosity up and down, or perhaps in the future redirect
-   logging to someplace interesting.
-   """
-   def __init__(self, impl):
-      AGServiceI.__init__(self, impl)
-
-   def GetLevel(self):
-      return self.impl.GetLevel()
-
-   def SetLevel(self, level):
-      return self.impl.SetLevel(level)
+      # Set executable
+      self.executable = os.path.join(os.getcwd(),"DebugService.py")
       
+      # Set configuration parameters
+      self.param1 = OptionSetParameter( "Configuration Parameter 1: ", "On", ["On", "Off"] )
+      self.param2 = RangeParameter( "Configuration Parameter 2: ", 0, 0, 100 )
+      self.param3 = TextParameter( "Configuration Parameter 3: ", "DebugService" )
+      self.param4 = ValueParameter( "Configuration Parameter 4: ", 25 )
+
+      # Append parameters to the configuration list
+      self.configuration.append(self.param1)
+      self.configuration.append(self.param2)
+      self.configuration.append(self.param3)
+      self.configuration.append(self.param4)
+
+   # -------------------------------------------------------------------
+   # Overidden methods inherited from AGService
+   # -------------------------------------------------------------------
+            
    def Start(self):
-      return self.impl.Start()
-
+      self.log.debug("DebugService.Start")
+      
    def Stop(self):
-      return self.impl.Stop()
-
+      self.log.debug("DebugService.Stop")
+            
    def ConfigureStream(self, streamDescription):
-      # This needs to unmarshal the data
-      # lucky we aren't using this yet
-      return self.impl.ConfigureStream(streamDescription)
-   
+      self.log.debug("DebugService.ConfigureStream %s "%streamDescription)
+      if self.param1.value == "On":
+         self.log.info("DebugService.ConfigureStream: value parameter is on!")
+
    def SetIdentity(self, profile):
-      # This needs to unmarshal the data
-      # lucky we aren't using this yet
-      return self.impl.SetIdentity(profile)
+      self.log.debug("DebugService.Set Identity %s"%profile)
+      if self.param1.value == "On":
+         self.log.info("DebugService.SetIdentity: value parameter is on!")
 
-class DebugServiceIW(AGServiceIW):
-   def __init__(self, url):
-      AGServiceIW.__init__(self, url)
-
-   def GetLevel(self):
-      return self.proxy.GetLevel()
-
-   def SetLevel(self, level):
-      return self.proxy.SetLevel(level)
-   
-   def Start(self):
-      return self.proxy.Start()
-
-   def Stop(self):
-      return self.proxy.Stop()
-
-   def ConfigureStream(self, streamDescription):
-      return self.proxy.ConfigureStream(streamDescription)
-   
-   def SetIdentity(self, profile):
-      return self.proxy.SetIdentity(profile)
 
 if __name__ == '__main__':
-   if len(sys.argv) == 1:
-      doTest = 1
-      import pprint
-      port = 1300
-   else:
-      port = int(sys.argv[1])
-      
+   
+   # The port argument should always be set to sys.argv[1]
+   port = int(sys.argv[1])
+                
    # Create the display service
    debugService = DebugService()
 
-   # Then it's interface
-   debugServiceI = DebugServiceI(debugService)
-
+   # Create the SOAP interface for the service
+   debugServiceI = AGServiceI(debugService)
+  
+   # Start the service
    RunService(debugService, debugServiceI, port)
 
