4bd3ca8c72226eda7e51dab0578dabaf702d9de2
Susanne Lefvert <lefvert@mcs.anl.gov>
2005-04-06 20:50:38 +0000

    Added command line options and retreival of venue and local pdf files.

--- sharedapps/SharedPDF/SharedPDF.py
+++ sharedapps/SharedPDF/SharedPDF.py
@@ -1,3 +1,14 @@
+#-----------------------------------------------------------------------------
+# Name:        SharedPDF.py
+# Purpose:     Shared PDF viewer for Windows platform.
+#
+# Author:      Susanne Lefvert
+#
+# Created:     $Date: 2005-04-06 20:50:38 $
+# RCS-ID:      $Id: SharedPDF.py,v 1.2 2005-04-06 20:50:38 lefvert Exp $
+# Copyright:   (c) 2002
+# Licence:     See COPYING.TXT
+#-----------------------------------------------------------------------------
 
 # wxPython imports
 from wxPython.wx import *
@@ -12,9 +23,12 @@ from AccessGrid.Platform.Config import UserConfig
 from AccessGrid.ClientProfile import ClientProfile
 from AccessGrid import icons
 from AccessGrid.Toolkit import WXGUIApplication
+from AccessGrid.DataStoreClient import GetVenueDataStore
 
 # Standard imports
 import os
+import getopt
+import sys
 
 class PdfViewer(wxPanel):
     '''
@@ -24,7 +38,7 @@ class PdfViewer(wxPanel):
     the defined pdf activex class provided by wxPython.
     '''
     
-    def __init__(self, parent, name, appUrl):
+    def __init__(self, parent, name, appUrl, venueUrl):
         wxPanel.__init__(self, parent, -1)
 
         # Create ActiveX interface to adobe acrobate reader
@@ -32,7 +46,7 @@ class PdfViewer(wxPanel):
         
         # Do UI layout
         self.__Layout()
-
+    
         # Create UI events
         EVT_BUTTON(self, self.openButton.GetId(), self.OnOpenButton)
         EVT_BUTTON(self, self.prevButton.GetId(), self.OnPrevPageButton)
@@ -43,6 +57,7 @@ class PdfViewer(wxPanel):
         self.sharedAppClient = SharedAppClient(name)
         self.log = self.sharedAppClient.InitLogging()
         self.id = self.sharedAppClient.GetPublicId()
+        self.log.debug("PdfViewer.__init__: Start pdf viewer, venueUrl: %s, appUrl: %s"%(venueUrl, appUrl))
 
         # Get client profile
         clientProfileFile = os.path.join(UserConfig.instance().GetConfigDir(), "profile")
@@ -54,18 +69,27 @@ class PdfViewer(wxPanel):
         # Register callbacks for external events
         self.sharedAppClient.RegisterEventCallback("open", self.OpenCallback)
         self.sharedAppClient.RegisterEventCallback("position", self.ChangePositionCallback)
+
+        # Create data store interface
+        self.dataStoreClient = GetVenueDataStore(venueUrl)
         
         # Get current state
         ret = self.sharedAppClient.GetData("position")
-        self.filePath = None
+        self.file = None
         self.pageNr = 1
       
         if ret:
-            self.filePath, self.pageNr = ret
-            self.pdf.LoadFile(self.filePath)
-            self.pdf.setCurrentPage(self.pageNr)
+            self.file, self.pageNr = ret
+            try:
+                self.dataStoreClient.Download(self.file, "tmp")
+                self.pdf.LoadFile("tmp")
+                self.pdf.setCurrentPage(self.pageNr)
+            except:
+                self.log.exception("PdfViewer.__init__: Download failed %s"%(self.file))
 
+        self.Layout()
         self.Show()
+
     #
     # Callbacks for local UI events.
     #
@@ -74,27 +98,44 @@ class PdfViewer(wxPanel):
         '''
         Invoked when user clicks the open button.
         '''
-        dlg = wxFileDialog(self, wildcard="*.pdf")
+        dlg = FileSelectorDialog(self, -1, "Select File Location", self.dataStoreClient)
 
         if dlg.ShowModal() == wxID_OK:
-            wxBeginBusyCursor()
-            self.pdf.LoadFile(dlg.GetPath())
-            self.pageNr = 1
-            self.filePath = dlg.GetPath()
-            self.sharedAppClient.SetData("position", (self.filePath, self.pageNr))
-            self.sharedAppClient.SendEvent("load", (self.filePath, self.pageNr))
-            wxEndBusyCursor()
+            selectedFile = dlg.GetFile()
+            if selectedFile:
+                wxBeginBusyCursor()
 
-        dlg.Destroy()
+                # Get file from venue
+                try:
+                    self.dataStoreClient.Download(selectedFile, "tmp")
+                    self.pdf.LoadFile("tmp")
+                    self.pageNr = 1
+                    self.file = selectedFile
 
+                except:
+                    self.log.exception("PdfViewer.OnOpenButton: Failed to download %s"%(selectedFile))
+                    dlg = wxMessageDialog(frame, 'Failed to download %s.'%(selectedFile),
+                                          'Download Failed', wxOK | wxICON_ERROR)
+                    dlg.ShowModal()
+                    dlg.Destroy()
+
+                else:
+                    # Update shared app status
+                    self.sharedAppClient.SetData("position", (self.file, self.pageNr))
+                    
+                    # Send event
+                    self.sharedAppClient.SendEvent("load", (self.file, self.pageNr))
+                    
+                wxEndBusyCursor()
+          
     def OnPrevPageButton(self, event):
         '''
         Invoked when user clicks the previous button.
         '''
         self.pageNr = self.pageNr - 1
         self.pdf.setCurrentPage(self.pageNr)
-        self.sharedAppClient.SendEvent("position", (self.id, self.filePath, self.pageNr))
-        self.sharedAppClient.SetData("position", (self.id, self.pageNr))
+        self.sharedAppClient.SendEvent("position", (self.id, self.pageNr))
+        self.sharedAppClient.SetData("position", (self.file, self.pageNr))
 
     def OnNextPageButton(self, event):
         '''
@@ -102,12 +143,12 @@ class PdfViewer(wxPanel):
         '''
         self.pageNr = self.pageNr + 1
         self.pdf.setCurrentPage(self.pageNr)
-        self.sharedAppClient.SendEvent("position", (self.filePath, self.pageNr))
-        self.sharedAppClient.SetData("position", (self.id, self.pageNr))
+        self.sharedAppClient.SendEvent("position", (self.id, self.pageNr))
+        self.sharedAppClient.SetData("position", (self.file, self.pageNr))
 
     def OnExit(self, event):
         '''
-        Shut down shared browser.
+        Shut down shared pdf.
         '''
         self.sharedAppClient.Shutdown()
         
@@ -119,12 +160,12 @@ class PdfViewer(wxPanel):
         '''
         Invoked when a open event is received.
         '''
-        id, self.filePath, self.pageNr = event.data
+        id, self.file, self.pageNr = event.data
         
         # Ignore my own events
         if self.id != id:
             wxBeginBusyCursor()
-            wxCallAfter(self.pdf.LoadFile, self.filePath)
+            wxCallAfter(self.pdf.LoadFile, self.file)
             wxCallAfter(self.pdf.setCurrentPage, self.pageNr)
             wxEndBusyCursor()
         
@@ -132,12 +173,11 @@ class PdfViewer(wxPanel):
         '''
         Invoked when a next event is received.
         '''
-        id, pageNr = event.data
-        self.pageNr = pageNr
-
+        id, self.pageNr = event.data
+      
         # Ignore my own events
         if self.id != id:
-            wxCallAfter(self.pdf.setCurrentPage, self.pageNr)
+            wxCallAfter(self.pdf.setCurrentPage, self.pageNr)        
                
     def __Layout(self):
         '''
@@ -145,7 +185,6 @@ class PdfViewer(wxPanel):
         '''
 
         # Create UI objects
-       
         self.openButton = wxButton(self, wxNewId(), "Open PDF File")
         self.prevButton = wxButton(self, wxNewId(), "<-- Previous Page")
         self.nextButton = wxButton(self, wxNewId(), "Next Page -->")
@@ -167,32 +206,165 @@ class PdfViewer(wxPanel):
 
 #----------------------------------------------------------------------
 
+class FileSelectorDialog(wxDialog):
+    def __init__(self, parent, id, title, dataStoreClient):
+        wxDialog.__init__(self, parent, id, title)
 
-if __name__ == '__main__':
+        # Create UI components.
+        self.infoText = wxStaticText(self, -1, "Select pdf file to open: ",
+                                     size = wxSize(300, 20), style=wxALIGN_LEFT)
+        self.infoText2 = wxStaticText(self, -1, "File:")
+        self.pdfList = wxComboBox(self, wxNewId(), size = wxSize(200, 20),
+                                  choices = [], style=wxCB_DROPDOWN|wxCB_SORT)
+        self.addFileButton = wxButton(self, wxNewId(), "Add File")
+        self.okButton = wxButton(self, wxID_OK, "Ok")
+        self.cancelButton = wxButton(self, wxID_CANCEL, "Cancel")
+        self.__Layout()
 
-    appUrl = 'https://zuz.mcs.anl.gov:8000/Venues/default/apps/00000103140ba74c008c00dd0022006dbd6'
+        EVT_BUTTON(self, self.addFileButton.GetId(), self.OpenFileDialog)
+      
+        # Create the data store client.
+        self.dataStoreClient = dataStoreClient
+        self.PopulateCombobox()
+        
+    def PopulateCombobox(self, default = None):
+        # Get pdf files from venue
+        fileNames = []
+
+        wxBeginBusyCursor()
+        try:
+            self.dataStoreClient.LoadData()
+            fileNames = self.dataStoreClient.QueryMatchingFiles("*.pdf")
+        except:
+            self.log.exception("FileSelectorDialog.PopulateCombobox: QueryMatchingFiles failed.")
+        wxEndBusyCursor()
+                
+        self.pdfList.Clear()
+        for file in fileNames:
+            self.pdfList.Append(file)
+
+        if default and not len(default) == 0:
+            self.pdfList.SetValue(default)
+        else:
+            self.pdfList.SetSelection(0)
+
+    def OpenFileDialog(self, event):
+        dlg = wxFileDialog(self, wildcard="*.pdf")
+        filePath = None
+                
+        if dlg.ShowModal() == wxID_OK:
+            wxBeginBusyCursor()
+            self.pageNr = 1
+            filePath = dlg.GetPath()
+
+            # Upload file to venue
+         
+            try:
+                self.dataStoreClient.Upload(filePath)
+            except:
+                self.log.exception("OpenFileDialog: Upload file %s failed"%(filePath))
+
+            self.PopulateCombobox(default = os.path.basename(filePath))
+            wxEndBusyCursor()
+
+        dlg.Destroy()
+               
+    def GetFile(self):
+        return self.pdfList.GetValue()
+                            
+    def __Layout(self):
+        # Create UI objects
+        mainSizer = wxBoxSizer(wxVERTICAL)
+        
+        sizer = wxBoxSizer(wxVERTICAL)
+        btnSizer1 = wxBoxSizer(wxHORIZONTAL)
+        btnSizer = wxBoxSizer(wxHORIZONTAL)
+
+        sizer.Add(wxSize(5,5))
+        sizer.Add(self.infoText, 0, wxEXPAND|wxALL, 5)
+        sizer.Add(wxSize(5,5))
+
+        btnSizer1.Add(self.infoText2, 0, wxEXPAND|wxALL, 5)
+        btnSizer1.Add(self.pdfList, 0, wxEXPAND|wxALL, 5)
+        btnSizer1.Add(self.addFileButton, 0, wxEXPAND|wxALL, 5)
+        sizer.Add(btnSizer1, 0, wxEXPAND, 10)
+        
+        sizer.Add(wxStaticLine(self, -1), 0, wxEXPAND|wxALL, 10)
+        
+        btnSizer.Add(self.okButton, 1, wxRIGHT, 5)
+        btnSizer.Add(self.cancelButton, 1)
+        
+        sizer.Add(btnSizer, 0, wxALIGN_CENTER)
+
+        mainSizer.Add(sizer, 0, wxALL, 10)
+        
+        self.SetSizer(mainSizer)
+        mainSizer.Fit(self)
+        self.SetAutoLayout(1)
+        
+#----------------------------------------------------------------------
+
+def Usage():
+    """
+    How to use the program.
+    """
+    print "%s:" % sys.argv[0]
+    print "    -a|--applicationURL : <url to application in venue>"
+    print "    -v|--venueURL : <url to venue>"
+    print "    -h|--help : print usage"
+    print "    -d|--debug : print debugging output"
     
+if __name__ == '__main__':
+    # Get command line options
+    initArgs = []
+    appUrl = None
+    venueUrl = None
+    
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "a:d:v:h",
+                                   ["applicationURL=","venueURL=",
+                                    "debug", "help"])
+    except getopt.GetoptError:
+        Usage()
+        sys.exit(2)
+        
+    for o, a in opts:
+        if o in ("-a", "--applicationURL"):
+            appUrl = a
+        elif o in ("-v", "--venueURL"):
+            venueUrl = a
+        elif o in ("-d", "--debug"):
+            initArgs = ['--debug']
+        elif o in ("-h", "--help"):
+            Usage()
+            sys.exit(0)
+
+    if not appUrl:
+        Usage()
+        sys.exit(0)
+                
     # Create the wx python application
     wxapp = wxPySimpleApp()
-
+    wxBeginBusyCursor()
+       
     # Inizialize AG application
     app = WXGUIApplication()
     name = "SharedPDF"
-
-    #initArgs = ['--debug']
-    initArgs = []
     app.Initialize(name, initArgs)
 
     if wxPlatform == '__WXMSW__':
         # Create the UI
-        mainFrame = wxFrame(None, -1, name)
-        viewer = PdfViewer(mainFrame, name, appUrl)
+        mainFrame = wxFrame(None, -1, name, size = wxSize(600, 600))
+        viewer = PdfViewer(mainFrame, name, appUrl, venueUrl)
 
         # Start the UI main loop
         mainFrame.Show()
+        wxEndBusyCursor()
+           
         wxapp.MainLoop()
 
     else:
+        wxEndBusyCursor()
         dlg = wxMessageDialog(frame, 'This application only works on MSW.',
                               'Sorry', wxOK | wxICON_INFORMATION)
         dlg.ShowModal()
