dfb9c68330e315931156e06701f61b604b2ce642
Eric Olson <eolson@mcs.anl.gov>
2005-04-18 21:11:58 +0000

    Generate service interfaces.  Will be made gradually more general.

---
+++ tools/GenerateInterfaces.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python2
+import os
+
+dstPath = os.path.join("..", "AccessGrid", "interfaces")
+srcPath = os.path.join("..", "AccessGrid", "wsdl")
+
+if not os.path.exists(dstPath):
+    os.mkdir(dstPath)
+
+initFile = os.path.join(dstPath, "__init__.py")
+if not os.path.exists(initFile):
+    open(initFile, "w").close()
+
+# FIXME: this call shouldn't need the server wsdl to create the AccessGrid_Types.py
+command = "wsdl2py -f %s -e -o %s -t _AccessGrid_Types --simple-naming" % ( os.path.join(srcPath, "VenueServerBinding.wsdl"), dstPath)
+print "* ", command
+os.system(command)
+
+command = "wsdl2py -f %s -e -o %s -t AG_VenueServer_Types --simple-naming" % ( os.path.join(srcPath, "VenueServerBinding.wsdl"),dstPath)
+print "* ", command
+os.system(command)
+command = "wsdl2dispatch -f %s -e -o %s -t AG_VenueServer_Types --simple-naming" %  ( os.path.join(srcPath, "VenueServerBinding.wsdl"), dstPath)
+print "* ", command
+os.system(command)
+command = "wsdl2py -f %s -e -o %s -t AG_Venue_Types --simple-naming" %  ( os.path.join(srcPath, "VenueBinding.wsdl"), dstPath )
+print "* ", command
+os.system(command)
+command = "wsdl2dispatch -f %s -e -o %s -t AG_Venue_Types --simple-naming" %   ( os.path.join(srcPath, "VenueBinding.wsdl"), dstPath )
+print "* ", command
+os.system(command)
+
