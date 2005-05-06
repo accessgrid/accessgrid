a959016f143c97cb8a8aabb038c5a6c6e5b8f4f2
Eric Olson <eolson@mcs.anl.gov>
2005-05-06 16:05:09 +0000

    Use the schema to python type mapping when generating code.

--- tools/GenerateInterfaces.py
+++ tools/GenerateInterfaces.py
@@ -12,17 +12,17 @@ if not os.path.exists(initFile):
     open(initFile, "w").close()
 
 # FIXME: this call shouldn't need the server wsdl to create the AccessGrid_Types.py
-command = "wsdl2py -f %s -e -o %s -t AccessGrid_Types --simple-naming" % ( os.path.join(srcPath, "VenueServerBinding.wsdl"), dstPath)
+command = "wsdl2py -f %s -e -o %s -t AccessGrid_Types --simple-naming -m AccessGrid.wsdl.SchemaToPyTypeMap" % ( os.path.join(srcPath, "VenueServerBinding.wsdl"), dstPath)
 print "* ", command
 os.system(command)
 
-command = "wsdl2py -f %s -e -o %s -t AG_VenueServer_Types --simple-naming --clientClassSuffix=IW" % ( os.path.join(srcPath, "VenueServerBinding.wsdl"),dstPath)
+command = "wsdl2py -f %s -e -o %s -t AG_VenueServer_Types --simple-naming --clientClassSuffix=IW -m AccessGrid.wsdl.SchemaToPyTypeMap" % ( os.path.join(srcPath, "VenueServerBinding.wsdl"),dstPath)
 print "* ", command
 os.system(command)
 command = "wsdl2dispatch -f %s -e -o %s -t AG_VenueServer_Types --simple-naming" %  ( os.path.join(srcPath, "VenueServerBinding.wsdl"), dstPath)
 print "* ", command
 os.system(command)
-command = "wsdl2py -f %s -e -o %s -t AG_Venue_Types --simple-naming --clientClassSuffix=IW" %  ( os.path.join(srcPath, "VenueBinding.wsdl"), dstPath )
+command = "wsdl2py -f %s -e -o %s -t AG_Venue_Types --simple-naming --clientClassSuffix=IW -m AccessGrid.wsdl.SchemaToPyTypeMap" %  ( os.path.join(srcPath, "VenueBinding.wsdl"), dstPath )
 print "* ", command
 os.system(command)
 command = "wsdl2dispatch -f %s -e -o %s -t AG_Venue_Types --simple-naming" %   ( os.path.join(srcPath, "VenueBinding.wsdl"), dstPath )
