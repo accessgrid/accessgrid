995ebc6884513b230b87e0363b898410473312b3
Eric Olson <eolson@mcs.anl.gov>
2005-05-06 15:33:42 +0000

    File to tell zsi which python types to use for schema types. VenueState will be included once python dictionary types are handled by zsi.

---
+++ AccessGrid/wsdl/SchemaToPyTypeMap.py
@@ -0,0 +1,10 @@
+
+# Schema: module, import, class
+mapping = {
+    "ClientProfile" : ("AccessGrid.ClientProfile", "ClientProfile"),
+    "ConnectionDescription" : ("AccessGrid.Descriptions", "ConnectionDescription"),
+    "ApplicationDescription" : ("AccessGrid.Descriptions", "ApplicationDescription"),
+    "ServiceDescription" : ("AccessGrid.Descriptions", "ServiceDescription"),
+    "DataDescription" : ("AccessGrid.Descriptions", "DataDescription"),
+}
+
