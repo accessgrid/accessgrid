99c5981a26f15bd2b84207707c35aefc77cf077d
Eric Olson <eolson@mcs.anl.gov>
2005-05-09 22:11:57 +0000

    Ad VenueDescription, StreamDescription, NetworkLocation, and  ProviderProfile.

--- AccessGrid/wsdl/SchemaToPyTypeMap.py
+++ AccessGrid/wsdl/SchemaToPyTypeMap.py
@@ -6,5 +6,9 @@ mapping = {
     "ApplicationDescription" : ("AccessGrid.Descriptions", "ApplicationDescription"),
     "ServiceDescription" : ("AccessGrid.Descriptions", "ServiceDescription"),
     "DataDescription" : ("AccessGrid.Descriptions", "DataDescription"),
+    "VenueDescription" : ("AccessGrid.Descriptions", "VenueDescription"),
+    "StreamDescription" : ("AccessGrid.Descriptions", "StreamDescription"),
+    "NetworkLocation" : ("AccessGrid.NetworkLocation", "NetworkLocation"),
+    "ProviderProfile" : ("AccessGrid.NetworkLocation", "ProviderProfile")
 }
 
