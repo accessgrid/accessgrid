fdf8f043308755e3f4e65dfdd7ff3ffbdf8360bb
Susanne Lefvert <lefvert@mcs.anl.gov>
2005-05-19 18:34:48 +0000

    New jabber client for bajjer library

--- AccessGrid/Jabber/JabberClient.py
+++ AccessGrid/Jabber/JabberClient.py
@@ -1,138 +1,94 @@
-
-import os
-import sys
-from string import split, strip, find, lower
-from random import seed, randint
-from time import sleep
+from gov.lbl.dsd.bajjer import *
+import string, random
+import time
+import string
 
 from AccessGrid import Log
-from AccessGrid.Platform.Config import UserConfig
-import jabber
-import xmlstream
-import threading
-
-NS_P_MUC      = "http://jabber.org/protocol/muc" # JEP-0045
-NS_IQ_MUC_OWNER = "http://jabber.org/protocol/muc#owner" 
-NS_DATA = "jabber:x:data"
-
-JABBER_CONNECTION = xmlstream.TCP_SSL
+#from AccessGrid.Platform.Config import UserConfig
 
 log = Log.GetLogger('JabberClient')
 Log.SetDefaultLevel('JabberClient', Log.DEBUG)
 
 # Generating random numbers between:
 start = 100000000
-end   = 999999999
+end   = 999999999 
 
 class JabberClient:
 
     def __init__(self, host, port):
-        self.host = host
-        self.port = port
+        log.info("Connecting to Jabber Server '%s' ..." % host) 
+        self._stream = stream.JabberClientStream(host, port=port, use_ssl=True)
         self.currentRoom = ''
         self.currentRoomId = ''
-        # Initialize the default error codes and messgaes
-        self.__init_error()
-        # Set the seed for the random number generation
-        seed()
-        self._connect()
-
-    def __init_error(self):
-        self.errors = {'401':'Incorrect Jabber ID or password',
-                       '409':'Jabber ID is already in use. Please choose another id'}
-        self.errorCode = ''
-        self.errorMsg  = ''
-
-    def _connect(self):
-        logDir = UserConfig.instance().GetLogDir()
-        #logFile = os.path.join(logDir, "jabber.log")
-        logFile = "c:\\jabber.log"
-
-        self.host = "phosphorus.mcs.anl.gov"
-        self.port = 5223
-
-        print '=========== host:', self.host
-        print '=========== port:', self.port
-                
-        self.con = jabber.Client(self.host, self.port, debug=[], log=logFile,
-                                 connection = JABBER_CONNECTION)
-
-        log.info("Connecting to Jabber Server '%s' ..." % self.host)
-        #try:
-        self.con.connect()
-        #except Exception,E:
-        #    log.exception("Connection failed: %s " % E)
-        #else:
-        #    log.info('Connected to Jabber Server Successfully')
-
-        self.con.registerHandler('message',self.messageCB,makefirst=True)
-        self.con.registerHandler('presence',self.presenceCB)
-        self.con.registerHandler('iq',self.iqCB, makefirst=True)
-        self.con.setDisconnectHandler(self.disconnectedCB)
-
-        print '====================== connected to server!!!!!!!!'
-
-    def setUserInfo(self, name, email, id, pwd, res):
-        self.name = name
-        self.jabberId = id
-        self.password = pwd
-        self.resource = res
-        self.email = email
-
-    def SetChatRoom(self, room):
-        self.currentRoom = lower(room)
-        self.currentRoomId = room + "@conference.phosphorus.mcs.anl.gov"
-        self.to = self.currentRoomId + "/" + self.jabberId
+        self._auth_info = None
+        self.email = None
+        self.jid = None
+        self.name = None
+        self.resource = None
         self.roomLocked = 0
 
-    def GetChatRoom(self):
-        return self.currentRoomId
+    def setUserInfo(self, name, id, pwd, res):
+        self.name = name
+        self.jid = auth.Jid(id) 
+        self.auth_info = auth.AuthInfo(self.jid) 
+        self.auth_info.password = pwd
+        self.resource = res
+        #email isn't passed in
+        #self.email = email
+
+    def SetChatRoom(self, room):
+        self.currentRoom = string.lower(room)
+        self.currentRoomId = room + "@conference.phosphorus.mcs.anl.gov"
+        self.to = self.currentRoomId + "/" + self.name
 
     def SetPanel(self, panel):
         self.jabberPanel = panel
 
     def sendMessage(self, text):
-        log.debug("currentRoom inside sendMessage(): '%s'" % self.currentRoomId)
+        log.debug("currentRoom inside sendMessage(): '%s'" %self.currentRoomId)
         sent = 0
         if self.currentRoomId == '':
             log.error("No venue is selected")
-        else:
-            msg = jabber.Message(self.currentRoomId, strip(text))
-            msg.setType('groupchat')
-            try:
-                log.debug("Sending message to jabber server ...")
-                self.con.send(msg)
-                sent = 1
-            except Exception, E:
-                print "Error:", E
-                log.exception(E)
-                sent = 0
+            return sent
+      
+        try:
+            log.debug("Sending message to jabber server ...")
+            self._stream.message(self.currentRoomId, string.strip(text), msg_type="groupchat") 
+            sent = 1
+        except Exception, E:
+            print "Error:", E
+            log.exception(E)
+            sent = 0
         return sent
-        
+
     def sendPresence(self, type='available'):
         log.debug("Sending the presence to '%s' of type '%s'..." % (self.to, type))
-        p = jabber.Presence(self.to, type)
-        if type == 'available':
-            p.setX(NS_P_MUC)
-        self.con.send(p)
+        req = stanza.Presence()
+        if type == 'available': 
+            req.to_ = self.to
+            req.type_ = type 
+            req.x_ = (stanza.External(util.Namespaces.Muc.muc),)
 
-    def messageCB(self, con, msg):
-        message = msg.getBody()
-        sender  = str(msg.getFrom())
-        print msg.getFrom()
-        
-        if message:
+        self._stream.write(req)
+
+    def messageCB(self, msg_stanza):
+        message = msg_stanza.body_
+        sender  = msg_stanza.from_ 
+   
+        if not message == '':
             if self.roomLocked:
-                print '----------- room is locked'
-                if find(message,'This room is locked') > -1:
+                if string.find(message,'This room is locked') > -1:
                     # Creating a room with default configuration
                     log.debug(message)
                     log.debug("Unlocking the chat room with default settings ...")
-                    iq = jabber.Iq(to=sender, type='set')
-                    iq.setID(self.__nextRand())
-                    iq.setQuery(NS_IQ_MUC_OWNER)
-                    iq.setQueryPayload("<x xmlns='%s' type='submit'/>" % NS_DATA)
-                    self.con.send(iq)
+                    iq = stanza.Iq()
+                    iq.to_ = sender
+                    iq.type_ = 'set' 
+                    iq.id_ = self.__nextRand() 
+                    iq.query_e = stanza.Query(util.Namespaces.Muc.owner)  
+                    iq.query_e.x_e = stanza.External(util.Namespaces.x_form)
+                    iq.query_e.x_e.type_= 'submit' 
+                    self._stream.write(iq)
                 elif find(message, 'Configuration confirmed') > -1:
                     log.debug("Unlocked the chat room successfully.")
                     self.roomLocked = 0
@@ -141,85 +97,59 @@ class JabberClient:
                     pass
                 else:
                     self.roomLocked = 0
-                    speaker = split(sender, '/')
-                    print '***************', speaker
+                    speaker = string.split(sender, '/')
+                    
                     if len(speaker)<2:
                         log.debug(speaker[0] + ": " + message)
                     else:
                         self.jabberPanel.OutputText(speaker[1] + ": ", message)
             else:
-                print '----------- room is not locked\n\n'
-                print '\n\n============ this is sender', sender, '\n\n'
-                speaker = split(str(sender), '/')
-                print '\n\n------------- this is speaker ', speaker, '\n\n'
+                speaker = string.split(str(sender), '/')
                 if len(speaker)<2:
                     log.debug(speaker[0] + ": " + message)
                 else:
                     self.jabberPanel.OutputText(speaker[1] + ": ", message)
-                
 
-    def presenceCB(self, con, prs):
+    def presenceCB(self, prs_stanza):
         """Called when a presence is recieved"""
-        who = str(prs.getFrom())
-        type = prs.getType()
-        if type == None: type = 'available'
+        who = prs_stanza.from_
+        prs_type = prs_stanza.type_
+        if prs_type == None: 
+            prs_type = 'available'
 
-        if type == 'available':
+        if prs_type == 'available':
             log.debug("%s is available (%s / %s)" %
-                      (who, prs.getShow(), prs.getStatus()))
-        elif type == 'unavailable':
+                      (who, prs_stanza.show_ , prs_stanza.status_ ))
+        elif prs_type == 'unavailable':
             log.debug("%s is unavailable (%s / %s)" %
-                      (who, prs.getShow(), prs.getStatus()))
-
-    def iqCB(self, con,iq):
+                      (who, prs_stanza.show_ , prs_stanza.status_ ))
+    
+    def iqCB(self, iq_stanza):
         self.errorCode = ''
-        if (iq.getType() == 'error'):
-            self.errorCode = iq.getErrorCode()
-            self.errorMsg  = iq.getError()
+        if (iq_stanza.type_ == 'error'):
+            self.errorCode = iq_stanza.error_e.code_ 
+            self.errorMsg  = iq_stanza.error_e.getText()
             log.error(self.errorCode + ": " + self.errorMsg)
-        
-    def disconnectedCB(self, con):
-        print "Ouch, network error"
-        sys.exit(1)
 
     def register(self):
-        log.info("Registering the user '%s' in jabber server ..." % self.jabberId)
-        regInfo = self.con.getRegInfo()
-        regInfo[u'name'] = self.name
-        regInfo[u'username'] = self.jabberId
-        regInfo[u'password'] = self.password
-        regInfo[u'email'] = self.email
-
-        self.con.sendRegInfo()
-        sleep(1)
+        """
+        TODO: user name and email, when registering a user
+        """
+        log.info("Registering the user '%s' in jabber server ..." % self.jid) 
+        if self.auth_info is None:
+            raise RuntimeError("setUserInfo must be called before register")
+        self._stream.register(self.auth_info)
+        #msg = self._stream.read(expected=stanza.Message)
+        time.sleep(1)
 
     def login(self):
-        log.info("Attempting to log in as %s ..." % self.jabberId)
+        log.info("Attempting to log in as %s ..." % self.jid)
         self.errorCode = ''
-        if self.con.auth(self.jabberId,self.password,self.resource):
+        if self._stream.authorize(self.auth_info):
             log.info("Successfully logged in")
-
-        ###################################
-        ## Question ?
-        ## Need user's roster information ?
-        ###################################
-        #self.con.requestRoster()
-        #self.con.sendInitPresence()
-
-        threading.Thread(target=self.__readIncoming).start()
-
-    def disconnect(self):
-        log.info("Disconnecting from the jabber server")
-        try:
-            self.con.disconnect()
-        except Exception, e:
-            log.debug("Error in disconnecting from the jabber server")
-            log.exception(e)
-
-    def __readIncoming(self):
-        while 1:
-            self.con.process(0.5)
-        return
+        self._stream.setMessageHandler(self.messageCB)
+        self._stream.setPresenceHandler(self.presenceCB)
+        self._stream.setIqHandler(self.iqCB)
 
     def __nextRand(self):
-        return randint(start, end)
+        return random.randint(start, end)
