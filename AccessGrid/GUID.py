8e8133460904e78abd6c347b512ec709ac4329cc
Eric Olson <eolson@mcs.anl.gov>
2005-07-06 21:39:10 +0000

    Import MIT licensed GUID code from Pyro project.

--- AccessGrid/GUID.py
+++ AccessGrid/GUID.py
@@ -1,109 +1,120 @@
-#!/usr/bin/python
-# A globally unique identifier made up of time and ip
-# Copyright (C) 2002  Dr. Conan C. Albrecht <conan_albrecht@byu.edu>
+"""
+Pyro Software License (MIT license):
+
+
+      Pyro is Copyright (c) 2002  by Irmen de Jong.
+
+
+      Permission is hereby granted, free of charge, to any person obtaining a copy of this
+      software and associated documentation files (the "Software"), to deal in the Software
+      without restriction, including without limitation the rights to use, copy, modify, merge,
+      publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+      to whom the Software is furnished to do so, subject to the following conditions:
+
+      The above copyright notice and this permission notice shall be included in all copies or
+      substantial portions of the Software.
+
+      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+      INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+      PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+      FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+      OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+      DEALINGS IN THE SOFTWARE.
+"""
+
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
+# GUID code originally written by the Pyro project.
+#   * 2004/07/06 Initial Import
+#        Minor changes to move supporting functions into this file.
 #
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307	USA
 
-import random
-import socket
-import time
+import os, sys, time, random
 
-class GUID:
-  '''
-  A globally-unique identifier made up of time and ip and 3 random
-  digits: 35 characters wide A globally unique identifier that
-  combines ip, time, and random bits.  Since the time is listed first,
-  you can sort records by guid.  You can also extract the time and ip
-  if needed.
+def supports_multithreading():
+        try:
+                from threading import Thread, Lock
+                return 1
+        except:
+                return 0
 
-  GUIDs make wonderful database keys.  They require no access to the
-  database (to get the max index number), they are extremely unique,
-  and they sort automatically by time.  GUIDs prevent key clashes when
-  merging two databases together, combining data, or generating keys
-  in distributed systems.
-  '''
-  rand = random.Random(time.time())
-  ip = ''
-  try:
-    ip = socket.gethostbyname(socket.gethostname())
-    # if we don't have an ip, default to someting in the 10.x.x.x private range
-  except (socket.gaierror): 
-    ip = '10'
-    for i in range(3):
-      ip += '.' + str(rand.randrange(1, 254))
-  # leave space for ip v6 (65K in each sub)
-  hexip = ''.join(["%04x" % long(i) for i in ip.split('.')]) 
-  lastguid = ''
+# bogus lock class, for systems that don't have threads.
+class BogusLock:
+        def acquire(self): pass
+        def release(self): pass
 
-  def __init__(self, guid=None):
-    ''' Constructor.  Use no args if you want the guid generated (this is
-    the normal method) or send a string-typed guid to generate it from
-    the string'''
-    if guid == None:
-      self.guid = self.__class__.lastguid
-      while self.guid == self.__class__.lastguid:
-	# time part
-	now = long(time.time() * 1000)
-	self.guid = ("%016x" % now) + self.__class__.hexip
-	# random part
-	self.guid += ("%03x" % (self.__class__.rand.randrange(0, 4095)))
-      self.__class__.lastguid = self.guid
+def getLockObject():
+        if supports_multithreading():
+                from threading import Lock
+                return Lock()
+        else:
+                return BogusLock()
 
-    elif type(guid) == type(self): # if a GUID object, copy its value
-      self.guid = str(guid)
+#------ Get the hostname (possibly of other machines) (returns None on error)
+def getHostname(ip=None):
+        try:
+                if ip:
+                        (hn,alias,ips) = socket.gethostbyaddr(ip)
+                        return hn
+                else:
+                        return socket.gethostname()
+        except socket.error:
+                return None
 
-    else: # if a string, just save its value
-      self.guid = guid
+#------ Get IP address (return None on error)
+def getIPAddress(host=None):
+        try:
+                return socket.gethostbyname(host or getHostname())
+        except socket.error:
+                return None
 
-  def __str__(self):
-    '''Returns the string value of this guid'''
-    return self.guid
+_getGUID_counter=0              # extra safeguard against double numbers
+_getGUID_lock=getLockObject()
 
-  def time(self):
-    '''Extracts the time portion out of the guid and returns the 
-      number of milliseconds since the epoch'''
-    return long(self.guid[0:16], 16)
+if os.name=='java':
+        def getGUID():
+                # Jython uses java's own ID routine used by RMI
+                import java.rmi.dgc
+                return java.rmi.dgc.VMID().toString().replace(':','-')
+else:
+        import socket, binascii
+        def getGUID():
+                # Generate readable GUID string.
+                # The GUID is constructed as follows: hexlified string of
+                # AAAAAAAA-AAAABBBB-BBBBBBBB-BBCCCCCC  (a 128-bit number in hex)
+                # where A=network address, B=timestamp, C=random. 
+                # The 128 bit number is returned as a string of 16 8-bits characters.
+                # For A: should use the machine's MAC ethernet address, but there is no
+                # portable way to get it... use the IP address + 2 bytes process id.
 
-  def ip(self):
-    '''Extracts the ip portion out of the guid and returns it
-      as a string like 10.10.10.10'''
-    # there's probably a more elegant way to do this
-    ip = ''
-    index = 16
-    while index < 32:
-      if ip != '':
-	ip += "."
-      ip += str(int(self.guid[index: index + 4], 16))
-      index += 4
-    return ip
+                ip=getIPAddress()
+                if ip:
+                        networkAddrStr=binascii.hexlify(socket.inet_aton(ip))+"%04x" % os.getpid()
+                else:
+                        # can't get IP address... use another value, like our Python id() and PID
+                        Log.warn('getGUID','Can\'t get IP address')
+                        try:
+                                ip=os.getpid()
+                        except:
+                                ip=0
+                        ip += id(getGUID)
+                        networkAddrStr = "%08lx%04x" % (ip, os.getpid())
 
-  def random(self):
-    ''' Extracts the random bits from the guid. I have no idea how this
-    would be useful, but I've included it for completeness. '''
-    return int(self.guid[-3:], 16)
+                _getGUID_lock.acquire()  # cannot generate multiple GUIDs at once
+                global _getGUID_counter
+                t1=time.time()*100 +_getGUID_counter
+                _getGUID_counter+=1
+                _getGUID_lock.release()
+                t2=int((t1*time.clock())%sys.maxint) & 0xffffff
+                t1=int(t1%sys.maxint)
+                timestamp = (long(t1) << 24) | t2
+                r2=(random.randint(0,sys.maxint/2)>>4) & 0xffff
+                r3=(random.randint(0,sys.maxint/2)>>5) & 0xff
+                return networkAddrStr+'%014x%06x' % (timestamp, (r2<<8)|r3 )
+
+GUID = getGUID
 
 if __name__ == "__main__":
   # just print out a for testing
-  guid = GUID()
-  print "GUID: " + str(guid)
-  millis = guid.time()
-  print "Time: " + str(millis) + " millis (" + time.asctime(time.gmtime(millis / 1000)) + " UTC)"
-  print "IP:   " + guid.ip()
-  print "Rand: " + str(guid.random()) 
-
-
-
-
+  guid = getGUID()
+  print "GUID: " + guid
 
