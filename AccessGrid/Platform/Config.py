b3c64dd248415ee80f4facb90049d4a33bca4d6b
Eric Olson <eolson@mcs.anl.gov>
2005-10-10 16:18:14 +0000

    (port from 2.x) Basic platform support for FreeBSD, from Chris Willing.

--- AccessGrid/Platform/Config.py
+++ AccessGrid/Platform/Config.py
@@ -3,20 +3,20 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.4 2004-09-10 03:58:53 judson Exp $
+# RCS-ID:      $Id: Config.py,v 1.5 2005-10-10 16:18:14 eolson Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.4 2004-09-10 03:58:53 judson Exp $"
+__revision__ = "$Id: Config.py,v 1.5 2005-10-10 16:18:14 eolson Exp $"
 
 import sys
-from AccessGrid.Platform import IsWindows, IsLinux, IsOSX
+from AccessGrid.Platform import IsWindows, IsLinux, IsOSX, IsFreeBSD5
 
 if IsWindows():
     from AccessGrid.Platform.win32.Config import *
-elif IsLinux() or IsOSX():
+elif IsLinux() or IsOSX() or IsFreeBSD5():
     from AccessGrid.Platform.unix.Config import *
 else:
     print "No support for Platform %s" % sys.platform
--- AccessGrid/Platform/__init__.py
+++ AccessGrid/Platform/__init__.py
@@ -2,14 +2,14 @@
 # Name:        __init__.py
 # Purpose:     
 # Created:     2003/08/02
-# RCS-ID:      $Id: __init__.py,v 1.9 2004-09-10 03:58:53 judson Exp $
+# RCS-ID:      $Id: __init__.py,v 1.10 2005-10-10 16:18:14 eolson Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 Platform sub modules.
 """
-__revision__ = "$Id: __init__.py,v 1.9 2004-09-10 03:58:53 judson Exp $"
+__revision__ = "$Id: __init__.py,v 1.10 2005-10-10 16:18:14 eolson Exp $"
 
 # mechanisms to support multiple hosting environments and to set defaults
 import sys
@@ -22,6 +22,7 @@ AGTK_INSTALL = 'AGTK_INSTALL'
 WIN = 'win32'
 LINUX = 'linux2'
 OSX = 'darwin'
+FREEBSD5 = 'freebsd5'
 
 def IsWindows():
     """Function that retusn 1 if the platform is windows, 0 otherwise """
@@ -44,14 +45,22 @@ def IsOSX():
     else:
         return 0
 
+def IsFreeBSD5():
+    """Function that retusn 1 if the platform is linux, 0 otherwise """
+    if sys.platform == FREEBSD5:
+        return 1
+    else:
+        return 0
+
 isWindows = IsWindows
 isLinux = IsLinux
 isOSX = IsOSX
+isFreeBSD5 = IsFreeBSD5
 
 if IsWindows():
     from AccessGrid.Platform.win32 import Config as Config
     from AccessGrid.Platform.win32 import ProcessManager as ProcessManager
-elif IsLinux() or IsOSX():
+elif IsLinux() or IsOSX() or IsFreeBSD5():
     from AccessGrid.Platform.unix import Config as Config
     from AccessGrid.Platform.unix import ProcessManager as ProcessManager
 else:
