675286b3371b065bff56824c6901d97b47775c05
Tom Uram <turam@mcs.anl.gov>
2005-11-13 23:30:17 +0000

    Update for changes in cert mgr ui

--- bin/CertificateRequestTool.py
+++ bin/CertificateRequestTool.py
@@ -3,17 +3,18 @@
 # Name:        CertificateRequestTool.py
 # Purpose:     Starts the CertificateRequestTool
 # Created:     2003/08/12
-# RCS_ID:      $Id: CertificateRequestTool.py,v 1.9 2005-10-07 22:44:51 eolson Exp $
+# RCS_ID:      $Id: CertificateRequestTool.py,v 1.10 2005-11-13 23:30:17 turam Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 This is the tool used to get certificates.
 """
-__revision__ = "$Id: CertificateRequestTool.py,v 1.9 2005-10-07 22:44:51 eolson Exp $"
+__revision__ = "$Id: CertificateRequestTool.py,v 1.10 2005-11-13 23:30:17 turam Exp $"
 
 from AccessGrid.Toolkit import WXGUIApplication
 from wxPython.wx import wxPySimpleApp
+from AccessGrid.Security.wxgui import CertificateManagerWXGUI
 
 pp = wxPySimpleApp()
 app = WXGUIApplication()
@@ -27,5 +28,5 @@ except:
     print "Failed to init toolkit, continuing..."
     pass
 
-certMgrUI = app.GetCertMgrUI()
+certMgrUI = CertificateManagerWXGUI.CertificateManagerWXGUI(app.GetCertificateManager())
 certMgrUI.HandleNoCertificateInteraction()
