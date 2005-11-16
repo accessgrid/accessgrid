25051a22e8621f18b3a0e938c17ca95ab8d1711a
Tom Uram <turam@mcs.anl.gov>
2005-11-16 22:50:35 +0000

    Disable currently broken tests

--- tests/unittest_all.py
+++ tests/unittest_all.py
@@ -15,23 +15,23 @@ import unittest
 def suite():
     # List modules to test
     modules_to_test = [
-        'unittest_AGNodeService',
+        #'unittest_AGNodeService',
         'unittest_AGParameter',
-        'unittest_AGServiceManager',
+        #'unittest_AGServiceManager',
         'unittest_ClientProfile',
         'unittest_Descriptions',
         'unittest_GUID',
         'unittest_MulticastAddressAllocator',
         'unittest_NetworkLocation',
-        'unittest_Node',
-        'unittest_Platform',
-        'unittest_VenueServer',
+        #'unittest_Node',
+        #'unittest_Platform',
+        #'unittest_VenueServer',
         'unittest_version'
         ]
 
     alltests = unittest.TestSuite()
     for module in map(__import__, modules_to_test):
-       alltests.addTest(unittest.findTestCases(module))
+       alltests.addTest(unittest.findTn unitestCases(module))
     return alltests
 
 if __name__ == '__main__':
