a1391f8c978b9e92529a5a78aa04d1e108f782e7
Tom Uram <turam@mcs.anl.gov>
2006-01-27 16:00:06 +0000

    Quote arguments

--- tools/runag.py
+++ tools/runag.py
@@ -2,7 +2,7 @@
 # Name:        runag.py
 # Purpose:     This script sets up the python env and executes the given command/args
 # Created:     2006/01/26
-# RCS-ID:      $Id: runag.py,v 1.1 2006-01-26 23:12:28 turam Exp $
+# RCS-ID:      $Id: runag.py,v 1.2 2006-01-27 16:00:06 turam Exp $
 # Copyright:   (c) 2006
 # License:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -17,5 +17,6 @@ if os.environ.has_key('PYTHONPATH'):
     os.environ['PYTHONPATH'] = '%s;%s' % (installpath,os.environ['PYTHONPATH'],)
 else:
     os.environ['PYTHONPATH'] = installpath
+args = map( lambda x: '"' + x + '"' , sys.argv[1:])
 if sys.argv[1:]:
-    os.spawnv(os.P_WAIT,sys.executable,[sys.executable] + sys.argv[1:])
+    os.spawnv(os.P_WAIT,sys.executable,[sys.executable] + args)
