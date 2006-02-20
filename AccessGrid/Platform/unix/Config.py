2762aa32c2985c8e0565507c5dd9b4396da0a325
Tom Uram <turam@mcs.anl.gov>
2006-02-20 18:50:20 +0000

    Add resources as list instead of tuple

--- AccessGrid/Platform/unix/Config.py
+++ AccessGrid/Platform/unix/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.64 2006-02-08 21:19:48 turam Exp $
+# RCS-ID:      $Id: Config.py,v 1.65 2006-02-20 18:50:20 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.64 2006-02-08 21:19:48 turam Exp $"
+__revision__ = "$Id: Config.py,v 1.65 2006-02-20 18:50:20 turam Exp $"
 
 import sys
 import os
@@ -423,7 +423,7 @@ class SystemConfig(Config.SystemConfig):
             resourceList = list()
             for device,portList in deviceList.items():
                 try:
-                    resourceList.append((device,portList))
+                    resourceList.append([device,portList])
                 except Exception, e:
                     log.exception("Unable to add video resource to list. device: " + device + "  portlist: " + portList)
 
