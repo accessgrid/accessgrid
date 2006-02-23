b06e69ec6d43fdb4cc5e4f01bb5c7718a356b5d9
Susanne Lefvert <lefvert@mcs.anl.gov>
2006-02-23 21:19:35 +0000

    reset AGTK_LOCATION variable, otherwise some paths will point to cvs location if that is set

--- tools/runag.py
+++ tools/runag.py
@@ -1,22 +1,1 @@
-#-----------------------------------------------------------------------------
-# Name:        runag.py
-# Purpose:     This script sets up the python env and executes the given command/args
-# Created:     2006/01/26
-# RCS-ID:      $Id: runag.py,v 1.2 2006-01-27 16:00:06 turam Exp $
-# Copyright:   (c) 2006
-# License:     See COPYING.TXT
-#-----------------------------------------------------------------------------
-import sys
-import os
-
-# define install path of ag module
-installpath=r'c:\python23\lib\site-packages\accessgrid3'
-
-# prepend ag install path to pythonpath env variable
-if os.environ.has_key('PYTHONPATH'):
-    os.environ['PYTHONPATH'] = '%s;%s' % (installpath,os.environ['PYTHONPATH'],)
-else:
-    os.environ['PYTHONPATH'] = installpath
-args = map( lambda x: '"' + x + '"' , sys.argv[1:])
-if sys.argv[1:]:
-    os.spawnv(os.P_WAIT,sys.executable,[sys.executable] + args)
+#-----------------------------------------------------------------------------# Name:        runag.py# Purpose:     This script sets up the python env and executes the given command/args# Created:     2006/01/26# RCS-ID:      $Id: runag.py,v 1.3 2006-02-23 21:19:35 lefvert Exp $# Copyright:   (c) 2006# License:     See COPYING.TXT#-----------------------------------------------------------------------------import sysimport os# define install path of ag moduleinstallpath = 'c:\python23\lib\site-packages\accessgrid3'# reset AGTK_LOCATION env variableos.environ['AGTK_LOCATION'] = ''# prepend ag install path to pythonpath env variableif os.environ.has_key('PYTHONPATH'):    os.environ['PYTHONPATH'] ='%s;%s '%(installpath, os.environ['PYTHONPATH'])else:    os.environ['PYTHONPATH'] = installpathargs = map(lambda x: '"' + x + '"', sys.argv[1:])if sys.argv[1:]:    os.spawnv(os.P_WAIT, sys.executable, [sys.executable]+args)
