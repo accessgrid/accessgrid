bb6a226ae2c9a3cd9837200a8395af83e4a8d86c
Chris Willing <c.willing@uq.edu.au>
2006-03-19 23:38:34 +0000

    Added 'import sys' so that sys.path.insert can succeed (having been added during Linux packaging to accommodate simultaneous AG2 & AG3 installations)

--- bin/CertificateRequestTool.py
+++ bin/CertificateRequestTool.py
@@ -3,15 +3,16 @@
 # Name:        CertificateRequestTool.py
 # Purpose:     Starts the CertificateRequestTool
 # Created:     2003/08/12
-# RCS_ID:      $Id: CertificateRequestTool.py,v 1.10 2005-11-13 23:30:17 turam Exp $
+# RCS_ID:      $Id: CertificateRequestTool.py,v 1.11 2006-03-19 23:38:34 willing Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.txt
 #-----------------------------------------------------------------------------
 """
 This is the tool used to get certificates.
 """
-__revision__ = "$Id: CertificateRequestTool.py,v 1.10 2005-11-13 23:30:17 turam Exp $"
+__revision__ = "$Id: CertificateRequestTool.py,v 1.11 2006-03-19 23:38:34 willing Exp $"
 
+import sys
 from AccessGrid.Toolkit import WXGUIApplication
 from wxPython.wx import wxPySimpleApp
 from AccessGrid.Security.wxgui import CertificateManagerWXGUI
