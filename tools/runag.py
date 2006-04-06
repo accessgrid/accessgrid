e2c8bf0bcc6222fb4f5a1ec50a5c91e958d41259
Susanne Lefvert <lefvert@mcs.anl.gov>
2006-04-06 21:53:31 +0000

    attempt to fix broken file..

--- tools/runag.py
+++ tools/runag.py
@@ -1,1 +1,21 @@
-#-----------------------------------------------------------------------------# Name:        runag.py# Purpose:     This script sets up the python env and executes the given command/args# Created:     2006/01/26# RCS-ID:      $Id: runag.py,v 1.4 2006-04-06 20:51:44 lefvert Exp $# Copyright:   (c) 2006# License:     See COPYING.TXT#-----------------------------------------------------------------------------import sysimport os# define install path of ag moduleinstallpath = 'c:\python23\lib\site-packages\AccessGrid3'# reset AGTK_LOCATION env variableos.environ['AGTK_LOCATION'] = ''# prepend ag install path to pythonpath env variableif os.environ.has_key('PYTHONPATH'):    os.environ['PYTHONPATH'] ='%s;%s '%(installpath, os.environ['PYTHONPATH'])else:    os.environ['PYTHONPATH'] = installpathargs = map(lambda x: '"' + x + '"', sys.argv[1:])if sys.argv[1:]:    os.spawnv(os.P_WAIT, sys.executable, [sys.executable]+args)
+import sys
+import os
+
+# define install path of ag module
+installpath = 'c:\python23\lib\site-packages\AccessGrid3'
+
+# reset AGTK_LOCATION env variable
+os.environ['AGTK_LOCATION'] = ''
+
+# prepend ag install path to pythonpath env variable
+if os.environ.has_key('PYTHONPATH'):
+    os.environ['PYTHONPATH'] ='%s;%s '%(installpath, os.environ['PYTHONPATH'])
+
+else:
+    os.environ['PYTHONPATH'] = installpath
+
+args = map(lambda x: '"' + x + '"', sys.argv[1:])
+if sys.argv[1:]:
+    os.spawnv(os.P_WAIT, sys.executable, [sys.executable]+args)
+
+    
