71d408548c39853f4d9bcc35c4036d976e296234
Tom Uram <turam@mcs.anl.gov>
2006-04-21 20:59:47 +0000

    Accept hostname argument in Publisher ctor

--- AccessGrid/ServiceDiscovery.py
+++ AccessGrid/ServiceDiscovery.py
@@ -49,8 +49,8 @@ try:
 
     class BonjourPublisher:
 
-        def __init__(self,serviceName,regtype,url,port=9999):
-
+        def __init__(self,serviceName,regtype,url,hostname=None,port=9999):
+        
             if port == 0:
                 raise PublisherError('Service registered with invalid port %d' % (port,))
 
@@ -70,7 +70,7 @@ try:
                                           serviceName,        
                                           regtype,            
                                           'local.',           
-                                          socket.gethostbyname(socket.gethostname()),           
+                                          hostname,           
                                           port,               
                                           txtRecordLen,       
                                           txtRecord,     
