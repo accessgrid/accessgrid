02d747680fd672d093e512f42f7b2b839e5b2162
Tom Uram <turam@mcs.anl.gov>
2006-04-27 16:45:33 +0000

    Remove hostname arg to publisher, use None instead

--- AccessGrid/ServiceDiscovery.py
+++ AccessGrid/ServiceDiscovery.py
@@ -49,7 +49,7 @@ try:
 
     class BonjourPublisher:
 
-        def __init__(self,serviceName,regtype,url,hostname="",port=9999):
+        def __init__(self,serviceName,regtype,url,port=9999):
         
             if port == 0:
                 raise PublisherError('Service registered with invalid port %d' % (port,))
@@ -70,7 +70,7 @@ try:
                                           serviceName,        
                                           regtype,            
                                           'local.',           
-                                          hostname,           
+                                          None,           
                                           port,               
                                           txtRecordLen,       
                                           txtRecord,     
