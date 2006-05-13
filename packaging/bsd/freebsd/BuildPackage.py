8c61144181d6f6d707f266680b95692abba68cb0
Chris Willing <c.willing@uq.edu.au>
2006-05-13 11:32:25 +0000

    Prepare for FreeBSD6 packaging

---
+++ packaging/bsd/freebsd/BuildPackage.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import sys, os, time
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-s", dest="sourcedir", metavar="SOURCEDIR",
+                  default=None,
+                  help="The source directory for the AGTk build.")
+parser.add_option("-b", dest="builddir", metavar="BUILDDIR",
+                  default=None,
+                  help="The working directory the AGTk build.")
+parser.add_option("-d", dest="destdir", metavar="DESTDIR",
+                  default=None,
+                  help="The destination directory of the AGTk build.")
+parser.add_option("-m", dest="metainfo", metavar="METAINFO",
+                  default=None,
+                  help="Meta information string about this release.")
+parser.add_option("-v", dest="version", metavar="VERSION",
+                  default=None,
+                  help="Version of the toolkit being built.")
+parser.add_option("--verbose", action="store_true", dest="verbose",
+                  default=0,
+                  help="A flag that indicates to build verbosely.")
+parser.add_option("-p", "--pythonversion", dest="pyver",
+                  metavar="PYTHONVERSION", default="2.4",
+                  help="Which version of python to build the installer for.")
+
+options, args = parser.parse_args()
+
+SourceDir = options.sourcedir
+BuildDir = options.builddir
+DestDir = options.destdir
+metainfo = options.metainfo
+version = options.version
+pyver = options.pyver
+
+
+print "In freebsd/BuildPackage.py"
+print "SourceDir = ", SourceDir
+print "BuildDir = ", BuildDir
+print "DestDir = ", DestDir
+print "metainfo = ", metainfo
+print "version = ", version
+print "pyver = ", pyver
+
+print
+print "This script to be done"
+
+
+###################################################################################
+#print "DONE build for FreeBSD"
+###################################################################################
+
