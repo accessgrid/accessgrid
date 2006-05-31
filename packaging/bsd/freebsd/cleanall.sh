8669dd6a31ac405324df682ad3d1a4733f98c8fb
Chris Willing <c.willing@uq.edu.au>
2006-05-31 22:55:14 +0000

    In Makefile, changed pyOpenSSL and bsddb dependencies from executables to packages and corrected some directory renaming logic. Added options to cleanall.sh

--- packaging/bsd/freebsd/cleanall.sh
+++ packaging/bsd/freebsd/cleanall.sh
@@ -1,7 +1,36 @@
 here=`pwd`
-CMD='sudo make deinstall clean'
+CMD='sudo make'
+ALLDIRS=0
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    -d)
+      CMD="${CMD} deinstall"
+      ;;
+    -c)
+      CMD="${CMD} clean"
+      ;;
+    -a)
+      ALLDIRS=1
+      ;;
+    *)
+      echo "Unknown option ($1). Exiting!"
+      exit 1
+      ;;
+  esac
+  shift
+done
+if [ "${CMD}" = "sudo make" ]; then
+   CMD='sudo make clean'
+fi
+#echo "Running command: ${CMD}"
+
+if [ ${ALLDIRS} -eq 1 ]; then
+  cd ${here} && ${CMD}
+fi
 
 for d in `ls -1 ports` ; do
+  if [ "$d" = "CVS" ]; then continue; fi
   echo ports/$d
   cd ports/$d && ${CMD}
   cd ${here}
