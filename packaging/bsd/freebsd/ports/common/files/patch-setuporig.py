ac1c7354e205d925582cfcd22dae9b434cb379ab
Chris Willing <c.willing@uq.edu.au>
2006-06-18 23:54:48 +0000

    Add my_build to wrap building of upper level .so files

--- packaging/bsd/freebsd/ports/common/files/patch-setuporig.py
+++ packaging/bsd/freebsd/ports/common/files/patch-setuporig.py
@@ -1,22 +1,44 @@
 --- setuporig.py.orig	Sat Nov 12 01:47:47 2005
-+++ setuporig.py	Tue May 23 12:19:40 2006
-@@ -96,6 +96,15 @@
++++ setuporig.py	Mon Jun 19 09:23:04 2006
+@@ -1,6 +1,7 @@
+ """
+ Common module
+ """ 
++from distutils.command.build import build
+ from distutils.core import setup, Extension
+ from distutils.spawn import spawn
+ from distutils.sysconfig import get_python_inc
+@@ -96,6 +97,19 @@
  swigList = [ ("common.i", "common_wrap.cpp") ]
  pyModList = [ "common", "scheduler" ]
  
-+# Make the higher level stuff first
-+here = os.getcwd()
-+ulpath = os.path.join('..', '..')
-+os.chdir(ulpath)
-+cmd = 'sh dobuild.sh'
-+print "cmd = ", cmd
-+os.system(cmd)
-+os.chdir(here)
++class my_build(build):
++    def run(self):
++        """Specialized Python source builder."""
++        # Make the higher level stuff first
++        here = os.getcwd()
++        ulpath = os.path.join('..', '..')
++        os.chdir(ulpath)
++        cmd = 'sh dobuild.sh'
++        print "cmd = ", cmd
++        os.system(cmd)
++        os.chdir(here)
++        build.run(self)
 +
  # Distutils doesn't currently work with recent 1.3.x
  # versions of swig.
  def run_swig(swigList):
-@@ -127,10 +136,7 @@
+@@ -116,7 +130,8 @@
+     check_swig_version()
+     run_swig(swigList)
+ 
+-setup(name="common", version="1.2",
++setup(cmdclass={'build': my_build},
++      name="common", version="1.2",
+       description="Python UCL Common Library wrapper",
+       author="Ivan R. Judson", author_email="judson@mcs.anl.gov",
+       url="http://www.mcs.anl.gov/fl/research/common",
+@@ -127,10 +142,7 @@
                               extra_link_args=ldFlagsList,
                               libraries=libList)],
        package_dir = { "common" : "common" }, packages = ["common"],
