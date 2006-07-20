ff16fb1a0e7386105da5c0c5456c70cea454a4df
Tom Uram <turam@mcs.anl.gov>
2006-07-20 22:07:58 +0000

    Add methods for getting and loading node configurations (contributed by Jason Bell)

--- AccessGrid/VenueClient.py
+++ AccessGrid/VenueClient.py
@@ -3,14 +3,14 @@
 # Name:        VenueClient.py
 # Purpose:     This is the client side object of the Virtual Venues Services.
 # Created:     2002/12/12
-# RCS-ID:      $Id: VenueClient.py,v 1.322 2006-07-20 22:01:01 turam Exp $
+# RCS-ID:      $Id: VenueClient.py,v 1.323 2006-07-20 22:07:58 turam Exp $
 # Copyright:   (c) 2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
 """
 """
-__revision__ = "$Id: VenueClient.py,v 1.322 2006-07-20 22:01:01 turam Exp $"
+__revision__ = "$Id: VenueClient.py,v 1.323 2006-07-20 22:07:58 turam Exp $"
 
 import sys
 import os
@@ -1292,6 +1292,16 @@ class VenueClient:
             self.cache.updateProfile(profile)
         except InvalidProfileException:
             log.exception("UpdateProfileCache: InvalidProfile when storing a venue user's profile in the cache.")
+	    
+	    
+    """
+    This method returns the node configurations
+    """
+    def GetNodeConfigurations(self):
+	return self.nodeService.GetConfigurations()
+    
+    def LoadNodeConfiguration(self, configuration):
+	return self.nodeService.LoadConfiguration( configuration )
             
     #
     # Venue calls
