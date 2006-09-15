ec6b1ba805d0145021ddadb98d13a0fe362c2d2a
Tom Uram <turam@mcs.anl.gov>
2006-09-15 21:34:08 +0000

    Add GetProxyFile method to UserConfig

--- AccessGrid/Platform/unix/Config.py
+++ AccessGrid/Platform/unix/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.74 2006-06-13 05:09:38 douglask Exp $
+# RCS-ID:      $Id: Config.py,v 1.75 2006-09-15 21:34:08 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.74 2006-06-13 05:09:38 douglask Exp $"
+__revision__ = "$Id: Config.py,v 1.75 2006-09-15 21:34:08 turam Exp $"
 
 import sys
 import os
@@ -164,7 +164,13 @@ class UserConfig(Config.UserConfig):
             log.error("UserConfig configuration: TempDir %s is not writable", self.tempDir)
 
         return self.tempDir
-    
+
+    def GetProxyFile(self):
+        if not self.proxyFile:
+            proxyFileName = 'x509up_u%d' % os.getuid()
+            self.proxyFile = os.path.join(self.GetTempDir(),
+                                          proxyFileName)
+        return self.proxyFile
 
 class SystemConfig(Config.SystemConfig):
     """
--- AccessGrid/Platform/win32/Config.py
+++ AccessGrid/Platform/win32/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.71 2006-05-02 15:50:09 lefvert Exp $
+# RCS-ID:      $Id: Config.py,v 1.72 2006-09-15 21:34:03 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.71 2006-05-02 15:50:09 lefvert Exp $"
+__revision__ = "$Id: Config.py,v 1.72 2006-09-15 21:34:03 turam Exp $"
 
 import os
 import socket
@@ -162,6 +162,11 @@ class UserConfig(Config.UserConfig):
 
         return self.tempDir
     
+    def GetProxyFile(self):
+        if not self.proxyFile:
+            self.proxyFile = os.path.join(self.GetTempDir(),
+                                          'proxy')
+        return self.proxyFile
 
 class SystemConfig(Config.SystemConfig):
     """
