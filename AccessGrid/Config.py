41ca613708c8813470d97ce562712cc3b6582f88
Tom Uram <turam@mcs.anl.gov>
2006-09-15 21:45:52 +0000

    Add abstract GetProxyFile method

--- AccessGrid/Config.py
+++ AccessGrid/Config.py
@@ -3,13 +3,13 @@
 # Purpose:     Configuration objects for applications using the toolkit.
 #              there are config objects for various sub-parts of the system.
 # Created:     2003/05/06
-# RCS-ID:      $Id: Config.py,v 1.39 2006-06-01 07:46:18 braitmai Exp $
+# RCS-ID:      $Id: Config.py,v 1.40 2006-09-15 21:45:52 turam Exp $
 # Copyright:   (c) 2002
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 """
-__revision__ = "$Id: Config.py,v 1.39 2006-06-01 07:46:18 braitmai Exp $"
+__revision__ = "$Id: Config.py,v 1.40 2006-09-15 21:45:52 turam Exp $"
 
 import os
 import sys
@@ -255,6 +255,7 @@ class UserConfig:
         self.preferencesFilename = None
         self.bridgesFilename = None
         self.logDir = None
+        self.proxyFile = None
 
         self._Initialize()
         
@@ -478,6 +479,10 @@ class UserConfig:
             raise Exception, "AGTkConfig: services dir does not exist %s."%self.servicesDir
 
         return str(self.servicesDir)
+        
+    def GetProxyFile(self):
+        raise Exception, "This method is abstract and should be overridden by subclasses."
+
 
 class SystemConfig:
     """
