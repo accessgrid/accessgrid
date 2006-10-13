cddf9d4d617fcb26acc8becde45fc54dcbd13bef
Tom Uram <turam@mcs.anl.gov>
2006-10-13 19:56:53 +0000

    Remove commented code

--- AccessGrid/SecureGroupMsgService.py
+++ AccessGrid/SecureGroupMsgService.py
@@ -3,7 +3,7 @@
 # Name:        SecureGroupMsgService.py
 # Purpose:     A Secure Group Messaging service server.
 # Created:     2005/10/10
-# RCS-ID:      $Id: SecureGroupMsgService.py,v 1.2 2006-01-09 22:32:27 eolson Exp $
+# RCS-ID:      $Id: SecureGroupMsgService.py,v 1.3 2006-10-13 19:56:53 turam Exp $
 # Copyright:   (c) 2005 
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
@@ -17,18 +17,12 @@ class SecureContextFactory:
     def getContext(self):
         """Create an SSL context.
         """
-        """
-        ctx = SSL.Context(SSL.SSLv23_METHOD)
-        ctx.use_certificate_file('cert.pem')
-        ctx.use_privatekey_file('key.pem')
-        """
         return Toolkit.Service.instance().GetContext()
 
 class SecureGroupMsgService(GroupMsgService):
     def Start(self):
         port = self.location[1]
         self.contextFactory = SecureContextFactory()
-        #self.listenPort = reactor.listenSSL(port, self.factory, self.contextFactory)
         wrapper.listenSSL(port, self.factory, self.contextFactory)
 
 
