774dcef55c12731220222d11bc3cf98e58300c86
Tom Uram <turam@mcs.anl.gov>
2007-04-28 00:25:18 +0000

    Update dependencies, remove pyOpenSSL build

--- packaging/BuildPythonModules.py
+++ packaging/BuildPythonModules.py
@@ -58,31 +58,9 @@ else:
 
 print "Python: ", PYVER
 
-print "*********** Building pyOpenSSL_AG\n"
-if sys.platform == 'win32':
-   # Find better solution later...
-    os.chdir(os.path.join(SOURCE,"pyOpenSSL"))
-    os.spawnl(os.P_WAIT,sys.executable,sys.executable,"setup.py","clean","--all")
-    sslp = os.path.join(SOURCE, "openssl-0.9.7g")
-    ret = os.spawnl(os.P_WAIT,sys.executable,sys.executable,"setup.py","build_ext","-I%s"%(os.path.join(sslp,"inc32")),"-L%s"%(os.path.join(sslp, "out32dll")))
-    if ret:
-        print 'Command failed; exiting' % (cmd,ret)
-        sys.exit(1)
-
-    ret = os.spawnl(os.P_WAIT,sys.executable,sys.executable,"setup.py","build")
-    if ret:
-        print 'Command failed; exiting' % (cmd,ret)
-        sys.exit(1)
-    ret = os.spawnl(os.P_WAIT,sys.executable,sys.executable,"setup.py","install",
-              "--prefix=%s"%(DEST,), "--no-compile")
-    if ret:
-        print 'Command failed; exiting' % (cmd,ret)
-        sys.exit(1)
-else:
-    SetupModule("pyOpenSSL", SOURCE, DEST)
 
 print "*********** Building bajjer\n"
-SetupModule("Bajjer-0.2.4", SOURCE, DEST)
+SetupModule("Bajjer-0.2.5", SOURCE, DEST)
 
 print "*********** Building feedparser\n"
 SetupModule("feedparser", SOURCE, DEST)
@@ -97,13 +75,13 @@ print "*********** Building zsi\n"
 SetupModule("zsi", SOURCE, DEST)
 
 print "*********** Building m2crypto\n"
-SetupModule("m2crypto-0.15", SOURCE, DEST)
+SetupModule("m2crypto-0.17", SOURCE, DEST)
 
 print "*********** Building twisted\n"
 SetupModule("Twisted-2.1.0", SOURCE, DEST)
 
 print "*********** Building bonjour-py\n"
-SetupModule("bonjour-py-0.1", SOURCE, DEST)
+SetupModule("bonjour-py-0.2", SOURCE, DEST)
 
 print "*********** Building common\n"
 SetupModule(os.path.join("common","examples", "_common"), SOURCE, DEST,['--debug'])
