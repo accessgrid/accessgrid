5d59ab0b375000d0b8ffaef1a5aa261230cf34d7
Tom Uram <turam@mcs.anl.gov>
2007-05-30 19:20:22 +0000

    Detect input ports

--- tools/wdmscan.cpp
+++ tools/wdmscan.cpp
@@ -3,9 +3,30 @@
 #include <uuids.h>
 #include <stdio.h>
 #include <atlconv.h>
+#include <dshow.h>   // DirectShow
+#include <atlbase.h> // DirectShow
+
 
 
 static int NUM_DEVS = 20;
+int debug=0;
+
+void printcsv(const char *str) {
+        if (strchr(str, ',')) {
+                if (strchr(str, '"')) {
+                        printf("\"\"%s\"\"", str);
+                } else {
+                        printf("\"%s\"", str);
+                }
+        } else {
+                if (strchr(str, '"')) {
+                        printf("\"%s\"", str);
+                } else {
+                        printf("%s", str);
+                }
+        }
+}
+
 
 
 class DirectShowScanner {
@@ -14,31 +35,47 @@ class DirectShowScanner {
       ~DirectShowScanner();
 };
 
-
 DirectShowScanner::DirectShowScanner() {
    ICreateDevEnum *pDevEnum      = 0;
    int             hr;
-   int             devNum;
-   char            nameBuf[80];
    
    // Initialize the COM subsystem
    CoInitialize(NULL);
 
    // Create a helper object to find the capture device.
    hr = CoCreateInstance(CLSID_SystemDeviceEnum, NULL, CLSCTX_INPROC_SERVER, IID_ICreateDevEnum, (LPVOID*)&pDevEnum);
-
-   IEnumMoniker *pEnum    = 0;
-   IMoniker     *pMoniker = 0;
-   IPropertyBag *pPropBag = 0;
-   VARIANT      varName;
-   CLSID		clsid;
-
    if( pDevEnum == 0 )
 	   return;
-   
+
+   IEnumMoniker						*pEnum    = 0;
+   IMoniker							*pMoniker = 0;
+   IPropertyBag						*pPropBag = 0;
+   VARIANT							varName;
+   CLSID							clsid;
+   IAMCrossbar						*pXBar    = NULL;
+   long								input     = -1;
+   long								output    = -1;
+   long								related;
+   long								pinType;    
+   char								*ports[10];
+   int								numports = 0;
+   int								devNum;
+   char								nameBuf[80];
+   CComPtr<IGraphBuilder>			pGraph_;
+   CComPtr<ICaptureGraphBuilder2>   pBuild_;
+
+
+   // Set up filter graph builder and filter graph
+   CoCreateInstance(CLSID_CaptureGraphBuilder2, NULL, CLSCTX_INPROC_SERVER,
+                         IID_ICaptureGraphBuilder2, (void **)&pBuild_);
+   hr = CoCreateInstance(CLSID_FilterGraph, NULL, CLSCTX_INPROC_SERVER,
+                         IID_IGraphBuilder, (void **)&pGraph_);
+   hr = pBuild_->SetFiltergraph(pGraph_);
+
    // Get an enumerator over video capture filters
    hr = pDevEnum->CreateClassEnumerator(CLSID_VideoInputDeviceCategory, &pEnum, 0);
-   //showErrorMessage(hr);
+   if(debug==2) printf("post CreateClassEnumerator\n");
+
 
    if( pEnum == 0 )
 	   return;
@@ -48,29 +85,56 @@ DirectShowScanner::DirectShowScanner() {
       if ( pEnum->Next(1, &pMoniker, NULL) == S_OK ) {
 
          hr = pMoniker->BindToStorage(0, 0, IID_IPropertyBag, (void **)&pPropBag);
-         //showErrorMessage(hr);
-         //debug_msg("propbag bound to storage ok= %d\n", hr);
-
+		 if(debug==2) printf("post BindToStorage\n");
          VariantInit(&varName);
          hr = pPropBag->Read(L"FriendlyName", &varName, 0);
-         //showErrorMessage(hr);
-         //debug_msg("friendly name read ok= %d\n", hr);
 
-         // Need this macro in atlconv.h to go from bStr to char* - msp
+		 // Need this macro in atlconv.h to go from bStr to char* - msp
          USES_CONVERSION;
          strcpy(nameBuf, W2A(varName.bstrVal));
 
-         //debug_msg("DirectShowScanner::DirectShowScanner():  found nameBuf/FriendlyName=%s\n", nameBuf);
-
 		 IBaseFilter *pCap = NULL;
 		 hr = pMoniker->BindToObject(0, 0, IID_IBaseFilter, (void**)&pCap);
+		 hr = pGraph_->AddFilter(pCap, L"VicCaptureFilter");
 		 pCap->GetClassID(&clsid);
-		 if (!IsEqualGUID(clsid,CLSID_VfwCapture))
-		 {
-			printf("%s\n", nameBuf);
-         } else {
+		 if (IsEqualGUID(clsid,CLSID_VfwCapture)) {
             //fprintf(stderr,"discarding an apparent VFW device= %s\n", nameBuf);
+			continue;
          }
+
+		 if(debug==2) printf("pre FindInterface\n");
+
+		 // Find ports on this device
+		 numports = 0;
+         hr = pBuild_->FindInterface(&LOOK_UPSTREAM_ONLY, NULL, pCap, IID_IAMCrossbar, 
+                                     (void**)&pXBar);
+		 if(debug==2) printf("post FindInterface: pXBar=%x\n", pXBar);
+		 if(pXBar) {
+
+			 hr = pXBar->get_PinCounts(&output, &input);
+			 if(debug==2) printf("post get_PinCounts: output=%d input=%d\n", output,input);
+
+			 if (debug)  printf("inputs %d\n", input);
+			 for( int i = 0; i < input; ++i ) {
+				 pXBar->get_CrossbarPinInfo(TRUE, i, &related, &pinType);
+				 if (debug)  printf("input %d pintype %d\n", i, pinType);
+				 if( pinType == PhysConn_Video_SVideo ) {
+					ports[numports] = "S-Video";  numports++;
+				 }
+				 if( pinType == PhysConn_Video_Composite ) {
+					 ports[numports] = "Composite"; numports++;
+				 }
+			 }
+		 }
+		 else {
+			 ports[numports] = "external-in"; numports++;
+		 }
+
+		 // Print the list of ports
+		 printf("%s",nameBuf);
+		 for(int i=0; i<numports; i++ )
+			 printf(",%s", ports[i]);
+	     printf("\n");
 
          VariantClear(&varName);
          pPropBag->Release();
@@ -88,8 +152,13 @@ DirectShowScanner::~DirectShowScanner()
 
 }
 
-int main(char **argv, int argc)
+int main(int argc, char **argv)
 {
+  for(int i=0; i<argc; i++) {
+	  if(!strcmp(argv[i],"-d"))
+		  debug++;
+  }
+
   DirectShowScanner();
 
 }
