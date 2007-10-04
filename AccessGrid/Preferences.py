0583cf1a082af7e5bb9fc45e598521d7c4332dca
Tom Uram <turam@mcs.anl.gov>
2007-10-04 17:36:06 +0000

    Add repair network preference

--- AccessGrid/Preferences.py
+++ AccessGrid/Preferences.py
@@ -27,6 +27,7 @@ class Preferences:
     NODE_CONFIG = "defaultNodeConfig"
     NODE_CONFIG_TYPE = "defaultNodeConfigType"
     MULTICAST = "multicast"
+    REPAIR_NETWORK = "repair network"
     BEACON = "beacon"
     LOG_TO_CMD = "logToCmd"
     ENABLE_DISPLAY = "enableDisplay"
@@ -66,6 +67,7 @@ class Preferences:
                          self.NODE_CONFIG_TYPE : NodeConfigDescription.SYSTEM,
                          self.NODE_CONFIG: "default",
                          self.MULTICAST: 1,
+                         self.REPAIR_NETWORK: 1,
                          self.BEACON: 1,
                          self.LOG_TO_CMD: 0,
                          self.ENABLE_DISPLAY: 1,
