131b2d0d9e269231ae0a550d4c88133059cb368d
Chris Willing <c.willing@uq.edu.au>
2007-10-22 05:19:08 +0000

    Use newer swig version

--- packaging/bsd/freebsd/ports/common/files/patch-setuporig.py
+++ packaging/bsd/freebsd/ports/common/files/patch-setuporig.py
@@ -1,5 +1,5 @@
 --- setuporig.py.orig	Sat Nov 12 01:47:47 2005
-+++ setuporig.py	Mon Jun 19 09:23:04 2006
++++ setuporig.py	Thu Oct 18 16:20:32 2007
 @@ -1,6 +1,7 @@
  """
  Common module
@@ -8,7 +8,15 @@
  from distutils.core import setup, Extension
  from distutils.spawn import spawn
  from distutils.sysconfig import get_python_inc
-@@ -96,6 +97,19 @@
+@@ -20,6 +21,7 @@
+ RUN_EPYDOC    = 0
+ RUN_EPYDOCIFY = 1
+ SWIG_ARGS = [ "-python", "-outdir", "common", "-c++", "-new_repr", 
++              "-cpperraswarn",
+               "-I%s" % get_python_inc(plat_specific=1),
+               "-I%s" % os.path.join("..", "..", "src")]
+ SRC = os.path.join('..','..')
+@@ -96,6 +98,19 @@
  swigList = [ ("common.i", "common_wrap.cpp") ]
  pyModList = [ "common", "scheduler" ]
  
@@ -28,7 +36,7 @@
  # Distutils doesn't currently work with recent 1.3.x
  # versions of swig.
  def run_swig(swigList):
-@@ -116,7 +130,8 @@
+@@ -116,7 +131,8 @@
      check_swig_version()
      run_swig(swigList)
  
@@ -38,7 +46,7 @@
        description="Python UCL Common Library wrapper",
        author="Ivan R. Judson", author_email="judson@mcs.anl.gov",
        url="http://www.mcs.anl.gov/fl/research/common",
-@@ -127,10 +142,7 @@
+@@ -127,10 +143,7 @@
                               extra_link_args=ldFlagsList,
                               libraries=libList)],
        package_dir = { "common" : "common" }, packages = ["common"],
