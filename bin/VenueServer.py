e18419c08ddaf03982b7ba0923c1e86ae0deeedc
Chris Willing <c.willing@uq.edu.au>
2007-10-24 22:55:42 +0000

    Remove unnecessary -v option to specify a configuration file (already provided by Toolkit's -c option.

--- bin/VenueServer.py
+++ bin/VenueServer.py
@@ -4,14 +4,14 @@
 # Purpose:     This serves Venues.
 # Author:      Ivan R. Judson
 # Created:     2002/12/12
-# RCS-ID:      $Id: VenueServer.py,v 1.77 2007-05-04 20:12:55 eolson Exp $
+# RCS-ID:      $Id: VenueServer.py,v 1.78 2007-10-24 22:55:42 willing Exp $
 # Copyright:   (c) 2002-2003
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 """
 This is the venue server program. This will run a venue server.
 """
-__revision__ = "$Id: VenueServer.py,v 1.77 2007-05-04 20:12:55 eolson Exp $"
+__revision__ = "$Id: VenueServer.py,v 1.78 2007-10-24 22:55:42 willing Exp $"
 __docformat__ = "restructuredtext en"
 
 # The standard imports
@@ -73,12 +73,8 @@ def main():
     portOption = Option("-p", "--port", type="int", dest="port",
                         default=8000, metavar="PORT",
                         help="Set the port the service manager should run on.")
-    configOption = Option("-v", "--venueconfig", type="string", dest="config",
-                        default="VenueServer.cfg", metavar="CONFIG",
-                        help="Set the config file to use for initialization.")
 
     app.AddCmdLineOption(portOption)
-    app.AddCmdLineOption(configOption)
     
     # set default options
     app.SetCmdLineDefault('secure',1)
@@ -98,7 +94,6 @@ def main():
     # Get the Log
     log = app.GetLog()
     port = app.GetOption("port")
-    configfile = app.GetOption("config")
     
     # Second thing we do is create a hosting environment
     hostname = app.GetHostname()
@@ -114,7 +109,7 @@ def main():
         server = InsecureServer((hostname, port))
     
     # Then we create a VenueServer, giving it the hosting environment
-    venueServer = VenueServer(server, app.GetOption('config'))
+    venueServer = VenueServer(server, app.GetOption('configfilename'))
 
     # We register signal handlers for the VenueServer. In the event of
     # a signal we just try to shut down cleanly.n
