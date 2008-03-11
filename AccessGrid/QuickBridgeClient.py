2da5f6283878154dcaaa9890eb64b8c4ac3f9f45
Tom Uram <turam@mcs.anl.gov>
2008-03-11 19:04:52 +0000

    Add SetHostPort and PingBridge methods

--- AccessGrid/QuickBridgeClient.py
+++ AccessGrid/QuickBridgeClient.py
@@ -7,6 +7,7 @@
 # Licence:     See COPYING.TXT
 #-----------------------------------------------------------------------------
 
+import time
 import select
 import socket
 import xmlrpclib
@@ -15,26 +16,34 @@ from GUID import GUID
 from BridgeClient import BridgeClient
 from AccessGrid.UrllibTransport import UrllibTransport, TimeoutTransport
 from AccessGrid import Utilities
+from AccessGrid.Descriptions import BridgeDescription
 from AccessGrid import Log
 log = Log.GetLogger("QuickBridgeClient")
 
 class ConnectionError(Exception): pass
 
 class QuickBridgeClient(BridgeClient):
-    def __init__(self, host, port,timeout=1):
+    def __init__(self, host=None, port=None,timeout=1):
         BridgeClient.__init__(self, host, port)
         
         transport = None
-        proxyURL = Utilities.BuildPreferencesProxyURL()
-        if proxyURL:
-            transport = UrllibTransport(proxyURL,timeout)
+        self.proxyURL = Utilities.BuildPreferencesProxyURL()
+        print 'QuickBridgeClient using proxyUrl: ', self.proxyURL
+        if self.proxyURL:
+            self.transport = UrllibTransport(self.proxyURL,timeout)
         else:
-            transport = TimeoutTransport(timeout)
+            self.transport = TimeoutTransport(timeout)
         url = "http://%s:%s" % (host,str(port))
-        self.serverProxy = xmlrpclib.ServerProxy(url, transport = transport, verbose = 0)
+        self.serverProxy = xmlrpclib.ServerProxy(url, transport = self.transport, verbose = 0)
         self.host = host
         self.port = port
-
+        
+    def SetHostPort(self,host,port):
+        self.host = host
+        self.port = port
+        url = "http://%s:%s" % (host,str(port))
+        self.serverProxy = xmlrpclib.ServerProxy(url, transport = self.transport, verbose = 0)
+    
     def JoinBridge(self, multicastNetworkLocation):
         try:
             l = self.serverProxy.JoinBridge(multicastNetworkLocation)
@@ -79,18 +88,40 @@ class QuickBridgeClient(BridgeClient):
             
         return ret
             
-
+    def PingBridge(self):
+        ret = -1
+        try:
+            startTime = self.serverProxy.Ping(time.time())
+            roundTripTime = time.time() - startTime
+            if roundTripTime >= 0.0:
+                ret = roundTripTime
+            else:
+                ret = BridgeDescription.UNREACHABLE
+        except socket.timeout:
+            log.exception('Timeout pinging bridge: %s', self.host)
+            ret =  BridgeDescription.UNREACHABLE
+        except socket.error,e:
+            log.info('Socket error pinging bridge: %s %s', self.host, str(e.args))
+            ret =  BridgeDescription.UNREACHABLE
+        except Exception,e:
+            if 'method "Ping" is not supported' in str(e):
+                log.info('Ping not supported, old bridge, regard as unreachable: %s', self.host)
+            else:
+                log.exception('Exception pinging bridge: %s', self.host )
+            ret =  BridgeDescription.UNREACHABLE
+        return ret
 
 if __name__=="__main__":
-    # Example below.  You should start Bridge.py first.
     bc = QuickBridgeClient(host="milton.mcs.anl.gov", port=8030)
-    multicastNetworkLocation = MulticastNetworkLocation(host="224.2.2.2", port=9700, ttl=1)
+    multicastNetworkLocation = MulticastNetworkLocation(host="233.4.200.18", port=10002, ttl=1)
     multicastNetworkLocation.id = GUID()
     multicastNetworkLocation.privateId = GUID()
     multicastNetworkLocation.profile=("","")
     print dir(multicastNetworkLocation)
     print multicastNetworkLocation.__dict__
     desc = bc.JoinBridge(multicastNetworkLocation)
-    print desc
-    print bc.TestBridge("233.4.200.18", 10002)
+    print "Multicast location: ", multicastNetworkLocation
+    print "Unicast location: ", desc
+    print "Ping: ", bc.PingBridge()
+    print "Test: ", bc.TestBridge(multicastNetworkLocation)
 
