a837ebf124463f159f10954c49d5fc664ae7db32
Tom Uram <turam@mcs.anl.gov>
2008-04-07 21:57:49 +0000

    Initial commit

--- tests/unittest_RssReader.py
+++ tests/unittest_RssReader.py
@@ -6,6 +6,7 @@
 from AccessGrid.RssReader import RssReader
 
 expected_out ="""title: AccessGrid Dev
+modified: 1 Jan 2007 09:00:00 UT
 Multicast Network Overview13 Aug 2007 15:00:00 UThttps://vv3.mcs.anl.gov:8000/Venues/000000ff6f312bfe008c00dd0022000749c
 Access Grid Test Meeting16 Aug 2007 15:00:00 UThttps://vv3.mcs.anl.gov:8000/Venues/000000ff6f312bfe008c00dd0022000749c
 Access Grid Test Meeting23 Aug 2007 15:00:00 UThttps://vv3.mcs.anl.gov:8000/Venues/000000ff6f312bfe008c00dd0022000749c
@@ -24,6 +25,7 @@ entries = reader.entries[rssUrl]
 d = entries[0]
 out = ""
 out +=  'title: ' + d['feed']['title'] + "\n"
+out +=  'modified: ' + d['feed']['modified'] + "\n"
 for e in d.entries:
     out +=  e.title + e['modified'] + e.enclosures[0].url + "\n"
 assert out == expected_out
