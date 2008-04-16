bf3596f1a45e817ebe64151b966170c0c88ea172
Tom Uram <turam@mcs.anl.gov>
2008-04-16 20:35:36 +0000

    Catch failures in OpenSSL build; Use 098e (later versions do not build on Windows)

--- packaging/BuildOpenSSL.py
+++ packaging/BuildOpenSSL.py
@@ -3,43 +3,64 @@ import sys, os
 DEST=sys.argv[1]
 
 def BuildLinux():
-    os.system("./config")
-    os.system("make")
-    os.system("make test")
+    ret = os.system("./config")
+    if ret:
+        return ret
+    ret = os.system("make")
+    if ret:
+        return ret
+    ret = os.system("make test")
+    
+    return ret
 
 def BuildWindows():
-    
-    os.system("perl Configure VC-WIN32")
+    opensslinstalldir = os.path.abspath(os.path.join(os.getcwd(),'opensslinstall'))
+    ret = os.system("perl Configure VC-WIN32 --prefix=%s" % opensslinstalldir)
+    if ret:
+        return ret
 
     masmPath = os.path.join(os.getcwd(), 'ms', 'do_masm.bat')
-    os.system(masmPath)
+    ret = os.system(masmPath)
+    if ret:
+        return ret
 
     ntPath = os.path.join(os.getcwd(), 'ms', 'ntdll.mak')
-    os.system('nmake -f %s' %ntPath)
+    ret = os.system('nmake -f %s install' %ntPath)
+    if ret:
+        return ret
 
     out32dllPath = os.path.join(os.getcwd(), "out32dll")
 
     cmd = "copy %s %s" % (os.path.join(out32dllPath, "ssleay32.dll"),
                         os.path.join(DEST,'install'))
-    os.system(cmd)
+    ret = os.system(cmd)
+    if ret:
+        return ret
     
     cmd = "copy %s %s" % (os.path.join(out32dllPath, "libeay32.dll"),
                         os.path.join(DEST,'install'))
-    os.system(cmd)
+    ret = os.system(cmd)
+    
+    return ret
     
 def BuildDarwin():
-    os.system("./config --openssldir=%s" % DEST)
-    os.system("make")
+    ret = os.system("./config --openssldir=%s" % DEST)
+    if ret:
+        return ret
+    ret = os.system("make")
+
+    return ret
 
 os.chdir(os.path.abspath(os.path.join(os.environ['AGBUILDROOT'],
-                                      "openssl-0.9.8g")))
+                                      "openssl-0.9.8e")))
 
 if sys.platform == 'linux2':
-    BuildLinux()
+    ret = BuildLinux()
 elif sys.platform == 'win32':
-    BuildWindows()
+    ret = BuildWindows()
 elif sys.platform == 'darwin':
-    BuildDarwin()
+    ret = BuildDarwin()
 else:
     print "Can't build for you!"
     
+sys.exit(ret)
