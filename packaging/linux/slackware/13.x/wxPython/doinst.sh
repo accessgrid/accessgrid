f29c712fa7021568743c30aefe415baab84dc439
Chris Willing <c.willing@uq.edu.au>
2011-08-05 02:26:45 +0000

    Update packaging for Slackware

--- packaging/linux/slackware/13.x/wxPython/doinst.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-LDSOCONF=/etc/ld.so.conf
-PATHFOUND=`grep "^include[ ]*/etc/ld.so.conf.d/\*.conf" ${LDSOCONF}` >/dev/null
-if [ $? -eq 1 ]; then
-  echo "include /etc/ld.so.conf.d/*.conf" >>${LDSOCONF}
-fi
-/sbin/ldconfig
-
